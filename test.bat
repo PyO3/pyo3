a60037e27fa0532cd7dfa7a0e512fa1a02fcc08f
Daniel Grunwald <daniel@danielgrunwald.de>
2015-04-19 05:21:45 +0200

    Remove old test scripts (replaced by extensions/Makefile)

--- test.bat
+++
@@ -1,6 +0,0 @@
-@c:\cygwin\bin\touch src/lib.rs
-cargo build
-@if errorlevel 1 exit /b 1
-rustc testmodule.rs --extern rust-cpython=target\librust-cpython-211b85e007ec6e28.rlib -L "c:\Program Files\Python27\libs" -o testmodule.pyd
-@if errorlevel 1 exit /b 1
-python -c "import testmodule"
--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -xeu
-cargo build
-rustc testmodule.rs -L target/debug -L target/debug/deps -o testmodule.so
-python -c "import testmodule; print(repr(testmodule.__author__))"
-
