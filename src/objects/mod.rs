d77f93056e9fae5a2d1fdc19503088febd9256bf
Samuel Cormier-Iijima <sciyoshi@surveymonkey.com>
2016-06-02 01:02:43 -0700

    Add py_exception! macro for defining custom exception types

--- src/objects/mod.rs
+++ src/objects/mod.rs
@@ -38,6 +38,7 @@ pub use self::num::PyLong as PyInt;
 pub use self::num::{PyLong, PyFloat};
 pub use self::sequence::PySequence;
 
+#[macro_export]
 macro_rules! pyobject_newtype(
     ($name: ident) => (
         py_impl_to_py_object_for_python_object!($name);
