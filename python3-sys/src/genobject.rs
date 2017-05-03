ac1a3acaa446cadec94905d8ca2698b871ea422f
Nikolay Kim <fafhrd91@gmail.com>
2017-05-03 12:26:49 -0700

    py3.5 compat

--- python3-sys/src/genobject.rs
+++ python3-sys/src/genobject.rs
@@ -69,17 +69,17 @@ pub unsafe fn PyCoroWrapper_Check(op: *mut PyObject) -> c_int {
     0
 }
 
-#[cfg(Py_3_5)]
+#[cfg(Py_3_6)]
 #[cfg_attr(windows, link(name="pythonXY"))] extern "C" {
     pub static mut PyAsyncGen_Type: PyTypeObject;
 }
 
-#[cfg(Py_3_5)]
+#[cfg(Py_3_6)]
 #[inline(always)]
 pub unsafe fn PyAsyncGen_Check(op: *mut PyObject) -> c_int {
     PyObject_TypeCheck(op, &mut PyAsyncGen_Type)
 }
-#[cfg(not(Py_3_5))]
+#[cfg(not(Py_3_6))]
 #[inline(always)]
 pub unsafe fn PyAsyncGen_Check(op: *mut PyObject) -> c_int {
     0
