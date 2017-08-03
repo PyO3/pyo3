130c33ec09652d1887c8c8fc3b765a389a8dacb7
Nikolay Kim <fafhrd91@gmail.com>
2017-08-03 15:55:23 -0700

    export IntoPyDictPointer

--- src/lib.rs
+++ src/lib.rs
@@ -155,7 +155,7 @@ pub use objects::*;
 pub use objectprotocol::ObjectProtocol;
 pub use object::PyObject;
 pub use noargs::NoArgs;
-pub use python::{Python, ToPyPointer, IntoPyPointer};
+pub use python::{Python, ToPyPointer, IntoPyPointer, IntoPyDictPointer};
 pub use pythonrun::{GILGuard, GILPool, prepare_freethreaded_python, prepare_pyo3_library};
 pub use instance::{PyToken, PyObjectWithToken, AsPyRef, Py, PyNativeType};
 pub use conversion::{FromPyObject, RefFromPyObject, PyTryFrom, PyTryInto,
