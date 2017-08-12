0abc39ef4f56c137e0e803bca13b1e0ccd3f8287
Nikolay Kim <fafhrd91@gmail.com>
2017-08-12 09:31:14 -0700

    prepare release

--- tests/compile-fail/modinit-used-in-non-fn.rs
+++
@@ -1,9 +0,0 @@
-#![feature(proc_macro)]
-extern crate pyo3;
-
-use pyo3::{py, PyResult, Python, PyModule};
-
-#[py::modinit(rust2py)] 
-//~^ ERROR: custom attribute panicked
-//~^^ HELP: #[modinit] can only be used with fn block
-struct Rust2Py;
