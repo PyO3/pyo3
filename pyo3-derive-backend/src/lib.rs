e570c6b9b9345ff9ab1337a3d10ad03931223f3b
konstin <konstin@mailbox.org>
2018-04-01 15:04:45 +0200

    Move the codegen into its own crate
    This makes the code generatioin reusable from third party crates.

---
+++ pyo3-derive-backend/src/lib.rs
@@ -0,0 +1,20 @@
+// Copyright (c) 2017-present PyO3 Project and Contributors
+
+#![recursion_limit = "1024"]
+
+#[macro_use]
+extern crate log;
+#[macro_use]
+extern crate quote;
+extern crate syn;
+
+pub mod py_class;
+pub mod py_impl;
+pub mod py_proto;
+pub mod py_method;
+pub mod args;
+pub mod defs;
+pub mod func;
+pub mod method;
+pub mod module;
+pub mod utils;
