57de066a6baa5512bffd3e63cb3bf7c2bd54f3c2
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-04-25 15:04:51 +0200

    Use `docmatic` with additional arguments

--- tests/test_doc.rs
+++ tests/test_doc.rs
@@ -7,17 +7,15 @@ fn pypath() -> Option<PathBuf> {
     option_env!("PYTHON").map(|py| PathBuf::from(py).join("libs"))
 }
 
-
 fn test_file<P: AsRef<Path>>(path: P) {
     let args = if cfg!(windows) {
-        vec![("--library-path", pypath().unwrap())]
+        let py = pypath().unwrap().to_str().unwrap().to_string();
+        vec![String::from("--library-path"), py]
     } else {
         Vec::new()
     };
     docmatic::assert_file_with(path, &args);
 }
-
-
 
 #[test]
 fn test_guide() {
