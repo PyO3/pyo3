80b3ce780117c72d73af2f182aa624f7f09abdc8
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-04-25 21:55:10 +0200

    Use new `docmatic` API

--- tests/test_doc.rs
+++ tests/test_doc.rs
@@ -1,31 +1,25 @@
 extern crate docmatic;
 
 use std::path::{Path, PathBuf};
-use docmatic::assert_file_with;
+use std::default::Default;
 
-fn pypath() -> Option<PathBuf> {
-    option_env!("PYTHON").map(|py| PathBuf::from(py).join("libs"))
-}
-
-fn test_file<P: AsRef<Path>>(path: P) {
-    let args = if cfg!(windows) {
-        let py = pypath().unwrap().to_str().unwrap().to_string();
-        vec![String::from("--library-path"), py]
-    } else {
-        Vec::new()
-    };
-    docmatic::assert_file_with(path, &args);
+fn assert_file<P: AsRef<Path>>(path: P) {
+    let mut doc = docmatic::Assert::default();
+    if cfg!(windows) {
+        doc.library_path(option_env!("PYTHON").map(|py| PathBuf::from(py).join("libs")).unwrap());
+    }
+    doc.test_file(path.as_ref())
 }
 
 #[test]
 fn test_guide() {
     let guide_path = PathBuf::from("guide").join("src");
     for entry in guide_path.read_dir().unwrap() {
-        test_file(entry.unwrap().path())
+        assert_file(entry.unwrap().path())
     }
 }
 
 #[test]
 fn test_readme() {
-    test_file(PathBuf::from("README.md"));
+    assert_file("README.md")
 }
