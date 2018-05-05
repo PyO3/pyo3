da3e9a90ed287a6fb32d970ff3bd0c48b95dd8e0
konstin <konstin@mailbox.org>
2018-05-05 16:14:26 +0200

    Add missing closing bracket

--- ci/travis/guide.sh
+++ ci/travis/guide.sh
@@ -15,7 +15,7 @@ fi
 ### Build API reference ########################################################
 
 cargo doc --no-deps -p pyo3 -p pyo3cls
-echo "<meta http-equiv=refresh content=0;url=guide/" > target/doc/index.html
+echo "<meta http-equiv=refresh content=0;url='guide/'>" > target/doc/index.html
 
 
 ### Build guide ################################################################
