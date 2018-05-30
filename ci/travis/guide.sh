b4be009e38e282237906eea1b40f7ac1d95e5112
konstin <konstin@mailbox.org>
2018-05-30 14:32:41 +0200

    Deploy different versions of the guide to github pages

--- ci/travis/guide.sh
+++ ci/travis/guide.sh
@@ -5,24 +5,32 @@ set -ex
 ### Setup latest mdbook version ################################################
 
 INSTALLED=$(echo $(mdbook --version 2>/dev/null || echo "mdbook none") | cut -d' ' -f1)
-LATEST=0.1.5
+PINNED=0.1.5
 
-if [ "$LATEST" != "$INSTALLED" ]; then
-    URL=https://github.com/rust-lang-nursery/mdBook/releases/download/v${LATEST}/mdbook-v${LATEST}-x86_64-unknown-linux-gnu.tar.gz
+if [ "$PINNED" != "$INSTALLED" ]; then
+    URL=https://github.com/rust-lang-nursery/mdBook/releases/download/v${PINNED}/mdbook-v${PINNED}-x86_64-unknown-linux-gnu.tar.gz
     curl -SsL $URL | tar xvz -C $HOME/.cargo/bin
 fi
 
-### Build API reference ########################################################
-
-cargo doc --no-deps -p pyo3 -p pyo3cls -p pyo3-derive-backend
-echo "<meta http-equiv=refresh content=0;url='guide/'>" > target/doc/index.html
-
-
-### Build guide ################################################################
+### Build the guide ################################################################
+# Build and then upload the guide to a specific folder on the gh-pages branch. This way we can have multiple versions
+# of the guide at the same time (See #165)
 
 # This builds the book in target/doc/guide. See https://github.com/rust-lang-nursery/mdBook/issues/698
 mdbook build -d ../target/doc/guide guide
 
-git clone https://github.com/davisp/ghp-import.git
-./ghp-import/ghp_import.py -n -p -f -m "Documentation upload" -r https://"$GH_TOKEN"@github.com/"$TRAVIS_REPO_SLUG.git" target/doc
-echo "Uploaded documentation"
+LASTEST_TAG=$(git tag -l "v*" | tail -n1)
+
+git clone -b gh-pages https://$GH_TOKEN@github.com/$TRAVIS_REPO_SLUG.git gh_pages
+cd gh_pages
+
+echo "<meta http-equiv=refresh content=0;url='${LASTEST_TAG}/'>" > index.html
+echo "pyo3.rs" > CNAME
+
+# For builds triggered by a tag, $TRAVIS_BRANCH will be set to the tag
+rm -rf "$TRAVIS_BRANCH"
+cp -r ../target/doc/guide "$TRAVIS_BRANCH"
+git add --all
+git commit -m "Upload documentation for $TRAVIS_BRANCH"
+
+git push -f
