823f1117d2599ef9d507e3215454205b0791aaff
Antonio Yang <antonio.yang@siliconmotion.com.tw>
2018-06-10 23:19:24 +0800

    Write the test for clone_get macro

--- examples/my-class/setup.py
+++ examples/my-class/setup.py
@@ -14,8 +14,22 @@ except ImportError:
     else:
         from setuptools_rust import RustExtension
 
+class PyTest(TestCommand):
+    user_options = []
+    
+    def run(self):
+        self.run_command("test_rust")
+
+        import subprocess
+        import sys
+        errno = subprocess.call([sys.executable, '-m', 'pytest', 'tests'])
+        raise SystemExit(errno)
+
+
 setup_requires = ['setuptools-rust>=0.6.1']
 install_requires = []
+tests_require = install_requires + ['pytest']
+
 
 setup(
     name='my-class',
@@ -27,4 +41,5 @@ setup(
     setup_requires=setup_requires,
     include_package_data=True,
     zip_safe=False,
+    cmdclass=dict(test=PyTest)
 )
