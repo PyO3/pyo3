517fd3d4a1e9651f8c7c9865d718633342407938
Florian Doyon <florian.doyon@uk.mlp.com>
2018-06-28 15:44:33 +0100

    add sentinel debugger

--- examples/test_dict/test_dict.py
+++ examples/test_dict/test_dict.py
@@ -1,16 +1,45 @@
 from _test_dict import DictSize
 
+class Foo:
+
+    def __init__(self, x):
+        self.x = x
+        print("Foo:", self.x)
+
+    def __repr__(self):
+        return "Foo({})".format(self.x)
+
+    def __del__(self):
+        print("~Foo:", self.x)
+
+
+def fake(to_call):
+    def monkey(x):
+        print(">>Monkey:", x)
+        try:
+            breakpoint()
+        except NameError:
+            print("No breakpoint method")
+        to_call(x)
+        print("<<Monkey:", x)
+
+    return monkey
+
+
+
 
 def test_size(size):
     print ("-------------------")
     print ("size:{}",size)
     d = {}
+
     for i in range(0,size):
-        d[i]=i
-    
-    print("Python says");
-    print(d)
-    print("Rust says");
+        d[i]=Foo(i)
+
+    print("Injecting destructor to call breakpoint when the dict is dropped")
+    Foo.__del__=fake(Foo.__del__)
+
+    print("Pass to rust");
     DictSize(len(d)).iter_dict(d)
     print("Look ma, no segfault!")
 
