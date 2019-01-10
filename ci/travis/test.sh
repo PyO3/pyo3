42557beb5089fbd069c5b4034d3964b53305dc07
Joar Wandborg <joar@wandborg.se>
2019-01-10 08:06:58 +0100

    Use -e instead of || exit

--- ci/travis/test.sh
+++ ci/travis/test.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-# Note: Avoid using "-e" globally, its behaviour may sometimes be convoluted.
-# For more details, see http://mywiki.wooledge.org/BashFAQ/105
-set -x
+set -ex
 
-cargo fmt --all -- --check || exit 1
-cargo test --features "$FEATURES" || exit 1
-cargo clippy --features "$FEATURES" || exit 1
+cargo fmt --all -- --check
+cargo test --features "$FEATURES"
+cargo clippy --features "$FEATURES"
 
 for example_dir in examples/*; do
-    tox -c "$example_dir/tox.ini" -e py || exit 1
+    tox -c "$example_dir/tox.ini" -e py
 done
