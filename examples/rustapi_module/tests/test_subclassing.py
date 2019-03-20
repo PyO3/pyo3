f7d208cdb2d87f592acfd7e38042849fcf54c12b
Omer Ben-Amram <omerbenamram@gmail.com>
2019-03-20 15:45:22 +0200

    revert changes that cause an additional GC bug

--- examples/rustapi_module/tests/test_subclassing.py
+++ examples/rustapi_module/tests/test_subclassing.py
@@ -1,10 +1,9 @@
 from rustapi_module.subclassing import Subclassable
 
 
-# should not raise
-def test_subclassing_works():
-    class SomeSubClass(Subclassable):
-        pass
+class SomeSubClass(Subclassable):
+    pass
 
-    a = SomeSubClass()
-    _b = str(a) + repr(a)
+
+a = SomeSubClass()
+_b = str(a) + repr(a)
