fc787e7a395487c8ac9a323bb65eda6728779d63
Omer Ben-Amram <omerbenamram@gmail.com>
2019-03-21 10:12:12 +0200

    only setup PyPy when testing against PyPy

--- ci/travis/setup.sh
+++ ci/travis/setup.sh
@@ -37,11 +37,13 @@ fi
 
 ### Setup PyPy ################################################################
 
-wget --quiet https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh && \
-/bin/bash Miniconda3-latest-Linux-x86_64.sh -f -b -p /opt/anaconda && \
-/opt/anaconda/bin/conda install --quiet --yes conda && \
-/opt/anaconda/bin/conda config --system --add channels conda-forge && \
-/opt/anaconda/bin/conda init bash && \
-/opt/anaconda/bin/conda create -n pypy3 pypy3.5 -y && \
-/opt/anaconda/envs/pypy3/bin/pypy3 -m ensurepip && \
-/opt/anaconda/envs/pypy3/bin/pypy3 -m pip install setuptools-rust pytest pytest-benchmark tox
+if ! [[ $FEATURES == *"pypy"* ]]; then
+    wget --quiet https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh && \
+    /bin/bash Miniconda3-latest-Linux-x86_64.sh -f -b -p /opt/anaconda && \
+    /opt/anaconda/bin/conda install --quiet --yes conda && \
+    /opt/anaconda/bin/conda config --system --add channels conda-forge && \
+    /opt/anaconda/bin/conda init bash && \
+    /opt/anaconda/bin/conda create -n pypy3 pypy3.5 -y && \
+    /opt/anaconda/envs/pypy3/bin/pypy3 -m ensurepip && \
+    /opt/anaconda/envs/pypy3/bin/pypy3 -m pip install setuptools-rust pytest pytest-benchmark tox
+fi
