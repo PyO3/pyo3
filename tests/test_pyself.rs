a7736dd51b7acc4cc173f07cd897f08a2ee34d34
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-04-03 18:01:28 +0900

    Add test of slf: PyRefMut<_>

--- tests/test_pyself.rs
+++ tests/test_pyself.rs
@@ -1,5 +1,4 @@
-//! Rust value -> Python Iterator
-//! Inspired by https://github.com/jothan/cordoba, thanks.
+//! Test slf: PyRef/PyMutRef<Self>(especially, slf.into::<Py>) works
 use pyo3;
 use pyo3::prelude::*;
 use pyo3::types::{PyBytes, PyString};
@@ -10,20 +9,32 @@ use std::collections::HashMap;
 mod common;
 
 /// Assumes it's a file reader or so.
+/// Inspired by https://github.com/jothan/cordoba, thanks.
 #[pyclass]
+#[derive(Clone)]
 struct Reader {
     inner: HashMap<u8, String>,
 }
 
 #[pymethods]
 impl Reader {
-    fn get_optional(&self, test: Option<i32>) -> PyResult<i32> {
-        Ok(test.unwrap_or(10))
-    }
     fn get_iter(slf: PyRef<Self>, keys: Py<PyBytes>) -> PyResult<Iter> {
         Ok(Iter {
             reader: slf.into(),
-            keys: keys,
+            keys,
+            idx: 0,
+        })
+    }
+    fn get_iter_and_reset(
+        mut slf: PyRefMut<Self>,
+        keys: Py<PyBytes>,
+        py: Python,
+    ) -> PyResult<Iter> {
+        let reader = Py::new(py, slf.clone())?;
+        slf.inner.clear();
+        Ok(Iter {
+            reader,
+            keys,
             idx: 0,
         })
     }
@@ -42,7 +53,6 @@ impl PyIterProtocol for Iter {
         let py = unsafe { Python::assume_gil_acquired() };
         Ok(slf.to_object(py))
     }
-
     fn __next__(mut slf: PyRefMut<Self>) -> PyResult<Option<PyObject>> {
         let py = unsafe { Python::assume_gil_acquired() };
         match slf.keys.as_ref(py).as_bytes().get(slf.idx) {
@@ -75,4 +85,25 @@ fn test_nested_iter() {
         reader,
         "list(reader.get_iter(bytes([3, 5, 2]))) == ['c', 'e', 'b']"
     );
+}
+
+#[test]
+fn test_nested_iter_reset() {
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+    let reader = [(1, "a"), (2, "b"), (3, "c"), (4, "d"), (5, "e")];
+    let reader = PyRef::new(
+        py,
+        Reader {
+            inner: reader.iter().map(|(k, v)| (*k, v.to_string())).collect(),
+        },
+    )
+    .unwrap();
+    let obj = reader.into_object(py);
+    py_assert!(
+        py,
+        obj,
+        "list(obj.get_iter_and_reset(bytes([3, 5, 2]))) == ['c', 'e', 'b']"
+    );
+    assert!(reader.inner.is_empty());
 }
