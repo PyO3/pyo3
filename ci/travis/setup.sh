d971e6237d78c8e169f7529507e99d5e4d1ad83d
Omer Ben-Amram <omerbenamram@gmail.com>
2019-04-13 14:15:19 +0300

    setup pypy before using “python"

--- ci/travis/setup.sh
+++ ci/travis/setup.sh
@@ -11,9 +11,28 @@ if [ "$TRAVIS_JOB_NAME" = "Minimum nightly" ]; then
     rustup component add rustfmt
 fi
 
+### Setup PyPy ################################################################
+
+if [[ $FEATURES == *"pypy"* ]]; then
+    wget --quiet https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh && \
+    /bin/bash Miniconda3-latest-Linux-x86_64.sh -f -b -p /opt/anaconda && \
+    /opt/anaconda/bin/conda install --quiet --yes conda && \
+    /opt/anaconda/bin/conda config --system --add channels conda-forge && \
+    /opt/anaconda/bin/conda init bash && \
+    /opt/anaconda/bin/conda create -n pypy3 pypy3.5 -y && \
+    /opt/anaconda/envs/pypy3/bin/pypy3 -m ensurepip && \
+    /opt/anaconda/envs/pypy3/bin/pypy3 -m pip install setuptools-rust pytest pytest-benchmark tox
+fi
+
 ### Setup python linker flags ##################################################
 
-PYTHON_LIB=$(python -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
+if [[ $FEATURES == *"pypy"* ]]; then
+    PYTHON_BINARY="pypy3"
+else
+    PYTHON_BINARY="python"
+fi
+
+PYTHON_LIB=$($PYTHON_BINARY -c "import sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
 
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$PYTHON_LIB:$HOME/rust/lib"
 
@@ -32,18 +51,4 @@ if [ ! -f "$HOME/.cargo/bin/kcov" ]; then
     make
     install src/kcov $HOME/.cargo/bin/kcov
     cd $TRAVIS_BUILD_DIR
-fi
-
-
-### Setup PyPy ################################################################
-
-if [[ $FEATURES == *"pypy"* ]]; then
-    wget --quiet https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh && \
-    /bin/bash Miniconda3-latest-Linux-x86_64.sh -f -b -p /opt/anaconda && \
-    /opt/anaconda/bin/conda install --quiet --yes conda && \
-    /opt/anaconda/bin/conda config --system --add channels conda-forge && \
-    /opt/anaconda/bin/conda init bash && \
-    /opt/anaconda/bin/conda create -n pypy3 pypy3.5 -y && \
-    /opt/anaconda/envs/pypy3/bin/pypy3 -m ensurepip && \
-    /opt/anaconda/envs/pypy3/bin/pypy3 -m pip install setuptools-rust pytest pytest-benchmark tox
 fi
