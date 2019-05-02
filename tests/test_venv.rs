75f5081c3195629e89c6bf5789e3ad8ac3776d57
Ibrakov Azat <azatibrakov@gmail.com>
2019-05-03 00:24:55 +0300

    Fix commands execution

--- tests/test_venv.rs
+++ tests/test_venv.rs
@@ -4,27 +4,34 @@ use std::process::Command;
 #[test]
 fn test_building_example_using_venv() {
     fn is_example_built_using_venv(venv_name: &str, example_name: &str) -> bool {
-        let commands_chain = if cfg!(target_os = "windows") {
-            format!(
-                "python -m venv {0} && \
-                 {0}\\Scripts\\activate.bat && \
-                 cd examples\\{1} && \
-                 python setup.py build",
-                venv_name, example_name
-            );
+        if cfg!(target_os = "windows") {
+            Command::new("cmd")
+                .args(&[
+                    "/C",
+                    format!(
+                        "python -m venv {0} && \
+                         {0}\\Scripts\\activate.bat && \
+                         cd examples\\{1} && \
+                         python setup.py build",
+                        venv_name, example_name
+                    )
+                    .as_str(),
+                ])
+                .status()
+                .expect("failed to execute commands chain")
+                .success()
         } else {
-            format!(
+            Command::new(format!(
                 "python -m venv {0} && \
                  source {0}/bin/activate && \
                  cd examples/{1} && \
                  python setup.py build",
                 venv_name, example_name
-            );
-        };
-        Command::new(commands_chain)
+            ))
             .status()
             .expect("failed to execute commands chain")
             .success()
+        }
     }
 
     let virtual_environment_name = "venv";
