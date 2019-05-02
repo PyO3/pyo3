b58d9e75254f8833747939091fbce7a7573db097
Ibrakov Azat <azatibrakov@gmail.com>
2019-05-03 01:41:54 +0300

    Fix commands execution for Linux

--- tests/test_venv.rs
+++ tests/test_venv.rs
@@ -21,16 +21,21 @@ fn test_building_example_using_venv() {
                 .expect("failed to execute commands chain")
                 .success()
         } else {
-            Command::new(format!(
-                "python -m venv {0} && \
-                 source {0}/bin/activate && \
-                 cd examples/{1} && \
-                 python setup.py build",
-                venv_name, example_name
-            ))
-            .status()
-            .expect("failed to execute commands chain")
-            .success()
+            Command::new("sh")
+                .args(&[
+                    "-c",
+                    format!(
+                        "python -m venv {0} && \
+                         . {0}/bin/activate && \
+                         cd examples/{1} && \
+                         python setup.py build",
+                        venv_name, example_name
+                    )
+                    .as_str(),
+                ])
+                .status()
+                .expect("failed to execute commands chain")
+                .success()
         }
     }
 
