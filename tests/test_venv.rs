58ade09d5c905d4bbc725fa9332308998c395913
Ibrakov Azat <azatibrakov@gmail.com>
2019-05-03 02:48:22 +0300

    Extract virtual environment creation/removing into separate functions

--- tests/test_venv.rs
+++ tests/test_venv.rs
@@ -3,14 +3,25 @@ use std::process::Command;
 
 #[test]
 fn test_building_example_using_venv() {
+    fn create_venv(name: &str) {
+        Command::new("python")
+            .args(&["-m", "venv", name])
+            .status()
+            .expect("failed to create virtual environment")
+            .success();
+    }
+
+    fn remove_venv(name: &str) {
+        remove_dir_all(name).expect("failed to remove virtual environment");
+    }
+
     fn is_example_built_using_venv(venv_name: &str, example_name: &str) -> bool {
         if cfg!(target_os = "windows") {
             Command::new("cmd")
                 .args(&[
                     "/C",
                     format!(
-                        "python -m venv {0} && \
-                         {0}\\Scripts\\activate.bat && \
+                        "{0}\\Scripts\\activate.bat && \
                          cd examples\\{1} && \
                          python setup.py build",
                         venv_name, example_name
@@ -25,8 +36,7 @@ fn test_building_example_using_venv() {
                 .args(&[
                     "-c",
                     format!(
-                        "python -m venv {0} && \
-                         . {0}/bin/activate && \
+                        ". {0}/bin/activate && \
                          cd examples/{1} && \
                          python setup.py build",
                         venv_name, example_name
@@ -40,10 +50,10 @@ fn test_building_example_using_venv() {
     }
 
     let virtual_environment_name = "venv";
+    create_venv(virtual_environment_name);
     assert!(is_example_built_using_venv(
         virtual_environment_name,
         "word-count"
     ));
-    remove_dir_all(virtual_environment_name)
-        .expect("failed to remove virtual environment directory");
+    remove_venv(virtual_environment_name);
 }
