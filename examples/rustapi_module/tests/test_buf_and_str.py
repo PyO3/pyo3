f1bb043b704efb55f8e95b1e66b0a889536c5292
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-09-22 02:06:20 +0900

    Temporaly skip test_buf_and_str for PyPY

--- examples/rustapi_module/tests/test_buf_and_str.py
+++ examples/rustapi_module/tests/test_buf_and_str.py
@@ -1,8 +1,17 @@
 import os
+import platform
 import psutil
+import pytest
 from rustapi_module.buf_and_str import BytesExtractor
 
+PYPY = platform.python_implementation() == "PyPy"
 
+
+@pytest.mark.skipif(
+    PYPY,
+    reason="PyPy has a segfault bug around this area."
+    "See https://github.com/PyO3/pyo3/issues/589 for detail.",
+)
 def test_pybuffer_doesnot_leak_memory():
     N = 1000
     extractor = BytesExtractor()
