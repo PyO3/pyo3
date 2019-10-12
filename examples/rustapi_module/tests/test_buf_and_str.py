bbb8d8a2e34fb02f7343bdba80e3365696210fac
kngwyu <yuji.kngw.80s.revive@gmail.com>
2019-10-12 15:14:07 +0900

    Simplify test_buf_and_str

--- examples/rustapi_module/tests/test_buf_and_str.py
+++ examples/rustapi_module/tests/test_buf_and_str.py
@@ -1,4 +1,3 @@
-import gc
 import os
 import platform
 
@@ -21,10 +20,8 @@ def test_pybuffer_doesnot_leak_memory():
 
     def memory_diff(f):
         before = process.memory_info().rss
-        gc.collect()  # Trigger Garbage collection
         for _ in range(N):
             f()
-        gc.collect()  # Trigger Garbage collection
         after = process.memory_info().rss
         return after - before
 
@@ -36,10 +33,6 @@ def test_pybuffer_doesnot_leak_memory():
 
     def from_str():
         extractor.from_str(message_s)
-
-    # Running the memory_diff to warm-up the garbage collector
-    memory_diff(from_bytes)
-    memory_diff(from_str)
 
     assert memory_diff(from_bytes) == 0
     assert memory_diff(from_str) == 0
