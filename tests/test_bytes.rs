69cbf1a494e68bbd3420e9317941525f0215f442
Alexander Niederbühl <a.niederbuehl@gmail.com>
2019-10-20 15:12:17 +0200

    Remove test_get_pybytes

--- tests/test_bytes.rs
+++ tests/test_bytes.rs
@@ -1,29 +1,11 @@
 use pyo3::prelude::*;
-use pyo3::types::PyBytes;
 use pyo3::wrap_pyfunction;
 
 mod common;
 
 #[pyfunction]
-fn get_pybytes() -> PyObject {
-    let gil = Python::acquire_gil();
-    let py = gil.python();
-
-    PyBytes::new(py, b"Hello World").into()
-}
-
-#[pyfunction]
 fn bytes_pybytes_conversion(bytes: &[u8]) -> &[u8] {
     bytes
-}
-
-#[test]
-fn test_get_pybytes() {
-    let gil = Python::acquire_gil();
-    let py = gil.python();
-
-    let get_bytes = wrap_pyfunction!(get_pybytes)(py);
-    py_assert!(py, get_bytes, "get_bytes() == b'Hello World'");
 }
 
 #[test]
