89e5a654ad03b3a697d683927a7b75ef42174fc5
kngwyu <yuji.kngw.80s.revive@gmail.com>
2020-03-19 00:29:27 +0900

    Bump version to 0.9.0

--- src/lib.rs
+++ src/lib.rs
@@ -52,7 +52,7 @@
 //! crate-type = ["cdylib"]
 //!
 //! [dependencies.pyo3]
-//! version = "0.9.0-alpha.1"
+//! version = "0.9.0"
 //! features = ["extension-module"]
 //! ```
 //!
@@ -109,7 +109,7 @@
 //!
 //! ```toml
 //! [dependencies]
-//! pyo3 = "0.9.0-alpha.1"
+//! pyo3 = "0.9.0"
 //! ```
 //!
 //! Example program displaying the value of `sys.version`:
