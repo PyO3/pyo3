4796a13b7633ee3c4be6828e6e41770062c9eb07
kngwyu <yuji.kngw.80s.revive@gmail.com>
2020-03-24 14:59:03 +0900

    Enable mac x64 and Win x86 tests on github Actions

---
+++ ci/actions/test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cargo test --features "$FEATURES num-bigint num-complex"
+(cd pyo3-derive-backend; cargo test)
+
+for example_dir in examples/*; do
+    cd $example_dir
+    tox -c "tox.ini" -e py
+    cd -
+done
