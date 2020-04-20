f067ff0074fa2311ddea39d32b5f0d304aa6b1d8
Mara Bos <m-ou.se@m-ou.se>
2020-04-20 17:53:54 +0200

    Add test for AsPyRef's lifetimes.

--- tests/test_compile_error.rs
+++ tests/test_compile_error.rs
@@ -7,6 +7,7 @@ fn test_compile_errors() {
     t.compile_fail("tests/ui/invalid_pymethod_names.rs");
     t.compile_fail("tests/ui/missing_clone.rs");
     t.compile_fail("tests/ui/reject_generics.rs");
+    t.compile_fail("tests/ui/wrong_aspyref_lifetimes.rs");
     // Since the current minimum nightly(2020-01-20) has a different error message,
     // we skip this test.
     // TODO(kngwyu): Remove this `if` when we update minimum nightly.
