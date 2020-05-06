11c1c5e6a1ae085371c503430294308e589d19a0
kngwyu <yuji.kngw.80s.revive@gmail.com>
2020-05-06 17:43:11 +0900

    Use -Cpanic=abort for correct coverage

--- ci/travis/cover.sh
+++ ci/travis/cover.sh
@@ -9,6 +9,11 @@ if [[ $FEATURES == *"pypy"* ]]; then
 fi
 
 ### Run grcov ##################################################################
+# export env vars and re-run tests
+export CARGO_INCREMENTAL=0
+export RUSTFLAGS="-Zpanic_abort_tests -Zprofile -Cpanic=abort -Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Coverflow-checks=off"
+export RUSTDOCFLAGS="-Cpanic=abort"
+cargo test --features "$FEATURES num-bigint num-complex"
 
 zip -0 ccov.zip `find . \( -name "pyo3*.gc*" \) -print`;
 ./grcov ccov.zip -s . -t lcov --llvm --branch --ignore-not-existing --ignore "/*" -o lcov.info;
