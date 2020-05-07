d3d68eafb4370e338403fba4b4bcf7d036d3008c
scalexm <alexandre@scalexm.fr>
2020-05-07 20:13:10 +0200

    Add a test with class attrs returning `PyClass` instances

--- tests/test_class_attributes.rs
+++ tests/test_class_attributes.rs
@@ -3,7 +3,16 @@ use pyo3::prelude::*;
 mod common;
 
 #[pyclass]
-struct Foo {}
+struct Foo {
+    #[pyo3(get)]
+    x: i32,
+}
+
+#[pyclass]
+struct Bar {
+    #[pyo3(get)]
+    x: i32,
+}
 
 #[pymethods]
 impl Foo {
@@ -16,6 +25,24 @@ impl Foo {
     #[name = "B"]
     fn b() -> String {
         "bar".to_string()
+    }
+
+    #[classattr]
+    fn foo() -> Foo {
+        Foo { x: 1 }
+    }
+
+    #[classattr]
+    fn bar() -> Bar {
+        Bar { x: 2 }
+    }
+}
+
+#[pymethods]
+impl Bar {
+    #[classattr]
+    fn foo() -> Foo {
+        Foo { x: 3 }
     }
 }
 
@@ -34,4 +61,15 @@ fn class_attributes_are_immutable() {
     let py = gil.python();
     let foo_obj = py.get_type::<Foo>();
     py_expect_exception!(py, foo_obj, "foo_obj.a = 6", TypeError);
+}
+
+#[test]
+fn recursive_class_attributes() {
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+    let foo_obj = py.get_type::<Foo>();
+    let bar_obj = py.get_type::<Bar>();
+    py_assert!(py, foo_obj, "foo_obj.foo.x == 1");
+    py_assert!(py, foo_obj, "foo_obj.bar.x == 2");
+    py_assert!(py, bar_obj, "bar_obj.foo.x == 3");
 }
