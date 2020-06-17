6798fe9da74ca986e9cf2687cc5db2afa5157aac
konstin <konstin@mailbox.org>
2020-06-17 10:41:51 +0200

    Fix tests on stable

--- tests/test_compile_error.rs
+++ tests/test_compile_error.rs
@@ -4,7 +4,6 @@ fn test_compile_errors() {
     t.compile_fail("tests/ui/invalid_macro_args.rs");
     t.compile_fail("tests/ui/invalid_property_args.rs");
     t.compile_fail("tests/ui/invalid_pyclass_args.rs");
-    t.compile_fail("tests/ui/invalid_pymethod_names.rs");
     t.compile_fail("tests/ui/missing_clone.rs");
     t.compile_fail("tests/ui/reject_generics.rs");
     t.compile_fail("tests/ui/wrong_aspyref_lifetimes.rs");
@@ -12,6 +11,7 @@ fn test_compile_errors() {
     // we skip this test.
     // TODO(kngwyu): Remove this `if` when we update minimum nightly.
     if option_env!("TRAVIS_JOB_NAME") != Some("Minimum nightly") {
+        t.compile_fail("tests/ui/invalid_pymethod_names.rs");
         t.compile_fail("tests/ui/static_ref.rs");
     }
 }
