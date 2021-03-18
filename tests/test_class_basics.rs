b27ee3fd64d6dd45193c931a5a7372db5deb2733
scalexm <alexandre@scalexm.fr>
2021-03-18 19:35:17 +0100

    Allow `#[name]` with `#[getter]` and `#[setter]`

--- tests/test_class_basics.rs
+++ tests/test_class_basics.rs
@@ -78,6 +78,12 @@ impl EmptyClass2 {
     #[staticmethod]
     #[name = "custom_static"]
     fn bar_static() {}
+
+    #[getter]
+    #[name = "custom_getter"]
+    fn foo(&self) -> i32 {
+        5
+    }
 }
 
 #[test]
@@ -92,8 +98,14 @@ fn custom_names() {
         typeobj,
         "typeobj.custom_static.__name__ == 'custom_static'"
     );
+    py_assert!(
+        py,
+        typeobj,
+        "typeobj.custom_getter.__name__ == 'custom_getter'"
+    );
     py_assert!(py, typeobj, "not hasattr(typeobj, 'bar')");
     py_assert!(py, typeobj, "not hasattr(typeobj, 'bar_static')");
+    py_assert!(py, typeobj, "not hasattr(typeobj, 'foo')");
 }
 
 #[pyclass]
