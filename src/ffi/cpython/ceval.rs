121808ef69c98a90110ebf438045d5c52e7a3249
mejrs <brunokolenbrander@hotmail.com>
2021-06-23 01:58:11 +0200

    reorder to header file order

--- src/ffi/cpython/ceval.rs
+++ src/ffi/cpython/ceval.rs
@@ -1,13 +1,16 @@
 use crate::ffi::object::{freefunc, PyObject};
-use crate::ffi::pystate::Py_tracefunc;
+use crate::ffi::Py_tracefunc;
+
 use std::os::raw::c_int;
 
 extern "C" {
+    pub fn PyEval_SetProfile(trace_func: Py_tracefunc, arg1: *mut PyObject);
+
     pub fn _PyEval_EvalFrameDefault(
         arg1: *mut crate::ffi::PyFrameObject,
         exc: c_int,
     ) -> *mut PyObject;
     pub fn _PyEval_RequestCodeExtraIndex(func: freefunc) -> c_int;
-    pub fn PyEval_SetProfile(trace_func: Py_tracefunc, arg1: *mut PyObject);
+
     pub fn PyEval_SetTrace(trace_func: Py_tracefunc, arg1: *mut PyObject);
 }
