d46d3265cbd17851acb3b1d59f25d70b7563b6c5
David Hewitt <1939362+davidhewitt@users.noreply.github.com>
2021-07-22 09:18:12 +0100

    pypy: fix FFI definition of Py_Buffer

--- examples/pyo3-pytests/tests/test_buf_and_str.py
+++ examples/pyo3-pytests/tests/test_buf_and_str.py
@@ -18,3 +18,12 @@ def test_extract_str_lossy():
     message = '\\(-"-;) A message written with a trailing surrogate \ud800'
     rust_surrogate_len = extractor.from_str_lossy("\ud800")
     assert extractor.from_str_lossy(message) == len(message) - 1 + rust_surrogate_len
+
+
+def test_extract_buffer():
+    extractor = BytesExtractor()
+    message = b'\\(-"-;) A message written in bytes'
+    assert extractor.from_buffer(message) == len(message)
+
+    arr = bytearray(b'\\(-"-;) A message written in bytes')
+    assert extractor.from_buffer(arr) == len(arr)
