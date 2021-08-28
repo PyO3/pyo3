585487c3c1085c37ad4c1f6d0b23b8212b5c79c2
Georg Brandl <georg@python.org>
2021-08-28 16:02:43 +0200

    more macro hygiene cleanup: test the FromPyObject derive macro

--- tests/test_proc_macro_hygiene.rs
+++ tests/test_proc_macro_hygiene.rs
@@ -1,5 +1,26 @@
 #![no_implicit_prelude]
 
+#[derive(::pyo3::prelude::FromPyObject)]
+struct Derive1(i32); // newtype case
+
+#[derive(::pyo3::prelude::FromPyObject)]
+#[allow(dead_code)]
+struct Derive2(i32, i32); // tuple case
+
+#[derive(::pyo3::prelude::FromPyObject)]
+#[allow(dead_code)]
+struct Derive3 {
+    f: i32,
+    g: i32,
+} // struct case
+
+#[derive(::pyo3::prelude::FromPyObject)]
+#[allow(dead_code)]
+enum Derive4 {
+    A(i32),
+    B { f: i32 },
+} // enum case
+
 #[::pyo3::proc_macro::pyclass]
 #[derive(::std::clone::Clone)]
 pub struct Foo;
