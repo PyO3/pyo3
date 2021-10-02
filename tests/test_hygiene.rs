31589f09e33ea9c90fd87632473582810100b99e
mejrs <brunokolenbrander@hotmail.com>
2021-10-03 01:41:22 +0200

    Skip pymethods hygiene on `multiple-pymethods`

--- tests/test_hygiene.rs
+++ tests/test_hygiene.rs
@@ -2,6 +2,8 @@ mod hygiene {
     mod misc;
     mod pyclass;
     mod pyfunction;
+    // cannot implement protos in #[pymethods] using `multiple-pymethods` feature
+    #[cfg(not(feature = "multiple-pymethods"))]
     mod pymethods;
     mod pymodule;
     mod pyproto;
