54ddd5aad73e2f09df22776f6c9e434f19133f7d
David Hewitt <1939362+davidhewitt@users.noreply.github.com>
2021-10-22 23:16:39 +0100

    tests: move ui tests updated for rust 1.56 to new section

--- tests/test_compile_error.rs
+++ tests/test_compile_error.rs
@@ -23,13 +23,13 @@ fn _test_compile_errors() {
     t.compile_fail("tests/ui/invalid_pymethods.rs");
     t.compile_fail("tests/ui/invalid_pymethod_names.rs");
     t.compile_fail("tests/ui/invalid_argument_attributes.rs");
-    t.compile_fail("tests/ui/invalid_closure.rs");
     t.compile_fail("tests/ui/reject_generics.rs");
 
     tests_rust_1_48(&t);
     tests_rust_1_49(&t);
     tests_rust_1_54(&t);
     tests_rust_1_55(&t);
+    tests_rust_1_56(&t);
 
     #[rustversion::since(1.48)]
     fn tests_rust_1_48(t: &trybuild::TestCases) {
@@ -49,21 +49,29 @@ fn _test_compile_errors() {
     #[rustversion::since(1.54)]
     fn tests_rust_1_54(t: &trybuild::TestCases) {
         t.compile_fail("tests/ui/invalid_frompy_derive.rs");
-        t.compile_fail("tests/ui/invalid_result_conversion.rs");
-        t.compile_fail("tests/ui/pyclass_send.rs");
         t.compile_fail("tests/ui/static_ref.rs");
     }
     #[rustversion::before(1.54)]
     fn tests_rust_1_54(_t: &trybuild::TestCases) {}
 
-    #[rustversion::before(1.55)]
-    fn tests_rust_1_55(_t: &trybuild::TestCases) {}
-
     #[rustversion::since(1.55)]
     fn tests_rust_1_55(t: &trybuild::TestCases) {
         t.compile_fail("tests/ui/invalid_pymethod_receiver.rs");
+    }
+
+    #[rustversion::before(1.55)]
+    fn tests_rust_1_55(_t: &trybuild::TestCases) {}
+
+    #[rustversion::since(1.56)]
+    fn tests_rust_1_56(t: &trybuild::TestCases) {
+        t.compile_fail("tests/ui/invalid_closure.rs");
+        t.compile_fail("tests/ui/invalid_result_conversion.rs");
+        t.compile_fail("tests/ui/pyclass_send.rs");
 
         #[cfg(Py_LIMITED_API)]
         t.compile_fail("tests/ui/abi3_nativetype_inheritance.rs");
     }
+
+    #[rustversion::before(1.56)]
+    fn tests_rust_1_56(_t: &trybuild::TestCases) {}
 }
