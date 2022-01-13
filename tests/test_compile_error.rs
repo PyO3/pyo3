a8a10ac33d28b3a43301db2ef64e7d0ca0d83390
mejrs <brunokolenbrander@hotmail.com>
2022-01-13 22:43:51 +0100

    gate the changed errors

--- tests/test_compile_error.rs
+++ tests/test_compile_error.rs
@@ -22,23 +22,19 @@ fn _test_compile_errors() {
     t.compile_fail("tests/ui/invalid_property_args.rs");
     t.compile_fail("tests/ui/invalid_pyclass_args.rs");
     t.compile_fail("tests/ui/invalid_pyclass_enum.rs");
-    t.compile_fail("tests/ui/invalid_pyclass_item.rs");
-    t.compile_fail("tests/ui/invalid_pyfunctions.rs");
-    t.compile_fail("tests/ui/invalid_pymethods.rs");
+    t.compile_fail("tests/ui/invalid_pyclass_item.rs");    
     #[cfg(not(Py_LIMITED_API))]
     t.compile_fail("tests/ui/invalid_pymethods_buffer.rs");
     t.compile_fail("tests/ui/invalid_pymethod_names.rs");
     t.compile_fail("tests/ui/invalid_pymodule_args.rs");
-    t.compile_fail("tests/ui/missing_clone.rs");
     t.compile_fail("tests/ui/reject_generics.rs");
-    t.compile_fail("tests/ui/not_send.rs");
     t.compile_fail("tests/ui/invalid_pymethod_proto_args.rs");
     t.compile_fail("tests/ui/invalid_pymethod_proto_args_py.rs");
 
     tests_rust_1_49(&t);
-    tests_rust_1_55(&t);
     tests_rust_1_56(&t);
     tests_rust_1_57(&t);
+    tests_rust_1_58(&t);
 
     #[rustversion::since(1.49)]
     fn tests_rust_1_49(t: &trybuild::TestCases) {
@@ -46,14 +42,6 @@ fn _test_compile_errors() {
     }
     #[rustversion::before(1.49)]
     fn tests_rust_1_49(_t: &trybuild::TestCases) {}
-
-    #[rustversion::since(1.55)]
-    fn tests_rust_1_55(t: &trybuild::TestCases) {
-        t.compile_fail("tests/ui/invalid_pymethod_receiver.rs");
-    }
-
-    #[rustversion::before(1.55)]
-    fn tests_rust_1_55(_t: &trybuild::TestCases) {}
 
     #[rustversion::since(1.56)]
     fn tests_rust_1_56(t: &trybuild::TestCases) {
@@ -78,4 +66,16 @@ fn _test_compile_errors() {
 
     #[rustversion::before(1.57)]
     fn tests_rust_1_57(_t: &trybuild::TestCases) {}
+
+    #[rustversion::since(1.58)]
+    fn tests_rust_1_58(t: &trybuild::TestCases) {
+        t.compile_fail("tests/ui/invalid_pyfunctions.rs");
+        t.compile_fail("tests/ui/invalid_pymethod_receiver.rs");
+        t.compile_fail("tests/ui/invalid_pymethods.rs");
+        t.compile_fail("tests/ui/missing_clone.rs");
+        t.compile_fail("tests/ui/not_send.rs");
+    }
+
+    #[rustversion::before(1.58)]
+    fn tests_rust_1_58(_t: &trybuild::TestCases) {}
 }
