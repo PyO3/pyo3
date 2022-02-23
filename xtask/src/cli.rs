180515233afa566b5b46bb42eadc0ddaf90ed1ea
mejrs <brunokolenbrander@hotmail.com>
2022-02-23 09:02:33 +0100

    Try to work around missing installs

--- xtask/src/cli.rs
+++ xtask/src/cli.rs
@@ -1,5 +1,6 @@
 use crate::utils::*;
 use anyhow::{ensure, Result};
+use std::io;
 use std::process::Command;
 use structopt::StructOpt;
 
@@ -59,18 +60,38 @@ impl Default for DocOpts {
 
 impl Subcommand {
     pub fn execute(self) -> Result<()> {
+        let installed = Installed::new()?;
+
         match self {
             Subcommand::All => {
-                crate::fmt::run()?;
+                crate::fmt::rust::run()?;
+                if installed.black {
+                    crate::fmt::python::run()?;
+                } else {
+                    Installed::warn_black()
+                };
                 crate::clippy::run()?;
                 crate::test::run()?;
                 crate::doc::run(DocOpts::default())?;
-                crate::pytests::run(None)?;
-                crate::llvm_cov::run(CoverageOpts::default())?;
+                if installed.nox {
+                    crate::pytests::run(None)?;
+                } else {
+                    Installed::warn_nox()
+                };
+                if installed.llvm_cov {
+                    crate::llvm_cov::run(CoverageOpts::default())?
+                } else {
+                    Installed::warn_llvm_cov()
+                };
+
+                installed.assert()?
             }
 
             Subcommand::Doc(opts) => crate::doc::run(opts)?,
-            Subcommand::Fmt => crate::fmt::run()?,
+            Subcommand::Fmt => {
+                crate::fmt::rust::run()?;
+                crate::fmt::python::run()?;
+            }
             Subcommand::Clippy => crate::clippy::run()?,
             Subcommand::Coverage(opts) => crate::llvm_cov::run(opts)?,
             Subcommand::TestPy => crate::pytests::run(None)?,
@@ -94,4 +115,85 @@ pub fn run(command: &mut Command) -> Result<()> {
         command = format_command(command),
     };
     Ok(())
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Installed {
+    pub nox: bool,
+    pub black: bool,
+    pub llvm_cov: bool,
+}
+
+impl Installed {
+    pub fn new() -> anyhow::Result<Self> {
+        Ok(Self {
+            nox: Self::nox()?,
+            black: Self::black()?,
+            llvm_cov: Self::llvm_cov()?,
+        })
+    }
+
+    pub fn nox() -> anyhow::Result<bool> {
+        let output = std::process::Command::new("nox").arg("--version").output();
+        match output {
+            Ok(_) => Ok(true),
+            Err(e) if e.kind() == io::ErrorKind::NotFound => Ok(false),
+            Err(other) => Err(other)?,
+        }
+    }
+
+    pub fn warn_nox() {
+        eprintln!("Skipping: formatting Python code, because `nox` was not found");
+    }
+
+    pub fn black() -> anyhow::Result<bool> {
+        let output = std::process::Command::new("black")
+            .arg("--version")
+            .output();
+        match output {
+            Ok(_) => Ok(true),
+            Err(e) if e.kind() == io::ErrorKind::NotFound => Ok(false),
+            Err(other) => Err(other)?,
+        }
+    }
+
+    pub fn warn_black() {
+        eprintln!("Skipping: Python code formatting, because `black` was not found.");
+    }
+
+    pub fn llvm_cov() -> anyhow::Result<bool> {
+        let output = std::process::Command::new("cargo")
+            .arg("llvm-cov")
+            .arg("--version")
+            .output();
+
+        match output {
+            Ok(_) => Ok(true),
+            Err(e) if e.kind() == io::ErrorKind::NotFound => Ok(false),
+            Err(other) => Err(other)?,
+        }
+    }
+    pub fn warn_llvm_cov() {
+        eprintln!("Skipping: code coverage, because `llvm-cov` was not found");
+    }
+
+    pub fn assert(&self) -> anyhow::Result<()> {
+        if self.nox && self.black && self.llvm_cov {
+            Ok(())
+        } else {
+            let mut err =
+                String::from("\n\nxtask was unable to run all tests due to some missing programs:");
+            if !self.black {
+                err.push_str("\n`black` was not installed. (`pip install black`)");
+            }
+            if !self.nox {
+                err.push_str("\n`nox` was not installed. (`pip install nox`)");
+            }
+            if !self.llvm_cov {
+                err.push_str("\n`llvm-cov` was not installed. (`cargo install llvm-cov`)");
+            }
+
+            Err(anyhow::anyhow!(err))
+        }
+    }
 }
--- xtask/src/fmt.rs
+++ xtask/src/fmt.rs
@@ -1,16 +1,23 @@
-use crate::cli;
-use std::process::Command;
+pub mod rust {
+    use crate::cli;
+    use std::process::Command;
+    pub fn run() -> anyhow::Result<()> {
+        cli::run(
+            Command::new("cargo")
+                .arg("fmt")
+                .arg("--all")
+                .arg("--")
+                .arg("--check"),
+        )?;
+        Ok(())
+    }
+}
 
-pub fn run() -> anyhow::Result<()> {
-    cli::run(
-        Command::new("cargo")
-            .arg("fmt")
-            .arg("--all")
-            .arg("--")
-            .arg("--check"),
-    )?;
-
-    cli::run(Command::new("black").arg(".").arg("--check"))?;
-
-    Ok(())
+pub mod python {
+    use crate::cli;
+    use std::process::Command;
+    pub fn run() -> anyhow::Result<()> {
+        cli::run(Command::new("black").arg(".").arg("--check"))?;
+        Ok(())
+    }
 }
