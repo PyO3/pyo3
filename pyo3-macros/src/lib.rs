49c1d22606be647d651d6e908953569d6fa65a12
David Hewitt <1939362+davidhewitt@users.noreply.github.com>
2022-03-22 10:38:36 +0000

    docs: for #2234

--- pyo3-macros/src/lib.rs
+++ pyo3-macros/src/lib.rs
@@ -81,47 +81,18 @@ pub fn pyproto(_: TokenStream, input: TokenStream) -> TokenStream {
 
 /// A proc macro used to expose Rust structs and fieldless enums as Python objects.
 ///
-/// `#[pyclass]` can be used the following [parameters][2]:
-///
-/// |  Parameter  |  Description |
-/// | :-  | :- |
-/// | <span style="white-space: pre">`crate = "some::path"`</span>  | Path to import the `pyo3` crate, if it's not accessible at `::pyo3`. |
-/// | `dict` | Gives instances of this class an empty `__dict__` to store custom attributes. |
-/// | <span style="white-space: pre">`extends = BaseType`</span>  | Use a custom baseclass. Defaults to [`PyAny`][4] |
-/// | <span style="white-space: pre">`freelist = N`</span> |  Implements a [free list][9] of size N. This can improve performance for types that are often created and deleted in quick succession. Profile your code to see whether `freelist` is right for you.  |
-/// | <span style="white-space: pre">`module = "module_name"`</span> |  Python code will see the class as being defined in this module. Defaults to `builtins`. |
-/// | <span style="white-space: pre">`name = "python_name"`</span> | Sets the name that Python sees this class as. Defaults to the name of the Rust struct. |
-/// | <span style="white-space: pre">`text_signature = "(arg1, arg2, ...)"`</span> |  Sets the text signature for the Python class' `__new__` method. |
-/// | `subclass` | Allows other Python classes and `#[pyclass]` to inherit from this class. Enums cannot be subclassed. |
-/// | `unsendable` | Required if your struct is not [`Send`][3]. Rather than using `unsendable`, consider implementing your struct in a threadsafe way by e.g. substituting [`Rc`][7] with [`Arc`][8]. By using `unsendable`, your class will panic when accessed by another thread.|
-/// | `weakref` | Allows this class to be [weakly referenceable][6]. |
-///
-/// All of these parameters can either be passed directly on the `#[pyclass(...)]` annotation, or as one or
-/// more accompanying `#[pyo3(...)]` annotations, e.g.:
-///
-/// ```rust,ignore
-/// // Argument supplied directly to the `#[pyclass]` annotation.
-/// #[pyclass(name = "SomeName", subclass)]
-/// struct MyClass { }
-///
-/// // Argument supplied as a separate annotation.
-/// #[pyclass]
-/// #[pyo3(name = "SomeName", subclass)]
-/// struct MyClass { }
-/// ```
+#[cfg_attr(docsrs, cfg_attr(docsrs, doc = include_str!("../docs/pyclass_parameters.md")))]
 ///
 /// For more on creating Python classes,
 /// see the [class section of the guide][1].
 ///
 /// [1]: https://pyo3.rs/latest/class.html
-/// [2]: https://pyo3.rs/latest/class.html#customizing-the-class
-/// [3]: std::marker::Send
-/// [4]: ../prelude/struct.PyAny.html
-/// [5]: https://pyo3.rs/latest/class/protocols.html#garbage-collector-integration
-/// [6]: https://docs.python.org/3/library/weakref.html
-/// [7]: std::rc::Rc
-/// [8]: std::sync::Arc
-/// [9]: https://en.wikipedia.org/wiki/Free_list
+/// [params-1]: ../prelude/struct.PyAny.html
+/// [params-2]: https://en.wikipedia.org/wiki/Free_list
+/// [params-3]: std::marker::Send
+/// [params-4]: std::rc::Rc
+/// [params-5]: std::sync::Arc
+/// [params-6]: https://docs.python.org/3/library/weakref.html
 #[proc_macro_attribute]
 pub fn pyclass(attr: TokenStream, input: TokenStream) -> TokenStream {
     use syn::Item;
