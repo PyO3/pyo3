f6cf4c362d58f65fdd7269df8cd688831ec493fa
messense <messense@icloud.com>
2022-03-31 19:28:44 +0800

    Remove link_name PyPyUnicode_FromFormatV

--- src/ffi/unicodeobject.rs
+++ src/ffi/unicodeobject.rs
@@ -78,9 +78,6 @@ extern "C" {
     #[cfg_attr(PyPy, link_name = "PyPyUnicode_FromObject")]
     pub fn PyUnicode_FromObject(obj: *mut PyObject) -> *mut PyObject;
     #[cfg_attr(PyPy, link_name = "PyPyUnicode_FromFormat")]
-    #[cfg_attr(PyPy, link_name = "PyPyUnicode_FromFormatV")]
-    //pub fn PyUnicode_FromFormatV(format: *const c_char,
-    //                             vargs: va_list) -> *mut PyObject;
     pub fn PyUnicode_FromFormat(format: *const c_char, ...) -> *mut PyObject;
     #[cfg_attr(PyPy, link_name = "PyPyUnicode_InternInPlace")]
     pub fn PyUnicode_InternInPlace(arg1: *mut *mut PyObject);
