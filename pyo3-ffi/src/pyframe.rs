7a7926c0ffaaf11542a88ca80db25a3cac179c17
Alex Gaynor <alex.gaynor@gmail.com>
2022-05-28 22:45:50 -0400

    Update pyframe.rs

--- pyo3-ffi/src/pyframe.rs
+++ pyo3-ffi/src/pyframe.rs
@@ -1,4 +1,4 @@
-#[cfg(Py_3_9)]
+#[cfg(any(Py_3_10, all(Py_3_9, not(Py_LIMITED_API))))]
 use crate::PyCodeObject;
 #[cfg(not(Py_LIMITED_API))]
 use crate::PyFrameObject;
@@ -9,6 +9,6 @@ opaque_struct!(PyFrameObject);
 
 extern "C" {
     pub fn PyFrame_GetLineNumber(f: *mut PyFrameObject) -> c_int;
-    #[cfg(Py_3_9)]
+    #[cfg(any(Py_3_10, all(Py_3_9, not(Py_LIMITED_API))))]
     pub fn PyFrame_GetCode(f: *mut PyFrameObject) -> *mut PyCodeObject;
 }
