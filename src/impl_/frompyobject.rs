e4ec720d51145b12759363ee13dad66d329e1b54
David Hewitt <1939362+davidhewitt@users.noreply.github.com>
2022-06-01 08:51:48 +0100

    frompyobject: tidy up generated code

--- src/impl_/frompyobject.rs
+++ src/impl_/frompyobject.rs
@@ -1,4 +1,4 @@
-use crate::{exceptions::PyTypeError, PyErr, Python};
+use crate::{exceptions::PyTypeError, FromPyObject, PyAny, PyErr, PyResult, Python};
 
 #[cold]
 pub fn failed_to_extract_enum(
@@ -23,4 +23,104 @@ pub fn failed_to_extract_enum(
         ));
     }
     PyTypeError::new_err(err_msg)
+}
+
+pub fn extract_struct_field<'py, T>(
+    obj: &'py PyAny,
+    struct_name: &str,
+    field_name: &str,
+) -> PyResult<T>
+where
+    T: FromPyObject<'py>,
+{
+    match obj.extract() {
+        ok @ Ok(_) => ok,
+        Err(err) => Err(failed_to_extract_struct_field(
+            obj.py(),
+            err,
+            struct_name,
+            field_name,
+        )),
+    }
+}
+
+pub fn extract_struct_field_with<'py, T>(
+    extractor: impl FnOnce(&'py PyAny) -> PyResult<T>,
+    obj: &'py PyAny,
+    struct_name: &str,
+    field_name: &str,
+) -> PyResult<T> {
+    match extractor(obj) {
+        ok @ Ok(_) => ok,
+        Err(err) => Err(failed_to_extract_struct_field(
+            obj.py(),
+            err,
+            struct_name,
+            field_name,
+        )),
+    }
+}
+
+#[cold]
+fn failed_to_extract_struct_field(
+    py: Python<'_>,
+    inner_err: PyErr,
+    struct_name: &str,
+    field_name: &str,
+) -> PyErr {
+    let new_err = PyTypeError::new_err(format!(
+        "failed to extract field {}.{}",
+        struct_name, field_name
+    ));
+    new_err.set_cause(py, ::std::option::Option::Some(inner_err));
+    new_err
+}
+
+pub fn extract_tuple_struct_field<'py, T>(
+    obj: &'py PyAny,
+    struct_name: &str,
+    index: usize,
+) -> PyResult<T>
+where
+    T: FromPyObject<'py>,
+{
+    match obj.extract() {
+        ok @ Ok(_) => ok,
+        Err(err) => Err(failed_to_extract_tuple_struct_field(
+            obj.py(),
+            err,
+            struct_name,
+            index,
+        )),
+    }
+}
+
+pub fn extract_tuple_struct_field_with<'py, T>(
+    extractor: impl FnOnce(&'py PyAny) -> PyResult<T>,
+    obj: &'py PyAny,
+    struct_name: &str,
+    index: usize,
+) -> PyResult<T> {
+    match extractor(obj) {
+        ok @ Ok(_) => ok,
+        Err(err) => Err(failed_to_extract_tuple_struct_field(
+            obj.py(),
+            err,
+            struct_name,
+            index,
+        )),
+    }
+}
+
+#[cold]
+fn failed_to_extract_tuple_struct_field(
+    py: Python<'_>,
+    inner_err: PyErr,
+    struct_name: &str,
+    index: usize,
+) -> PyErr {
+    let new_err =
+        PyTypeError::new_err(format!("failed to extract field {}.{}", struct_name, index));
+    new_err.set_cause(py, ::std::option::Option::Some(inner_err));
+    new_err
 }
