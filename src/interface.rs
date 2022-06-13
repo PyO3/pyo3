7acd791dd4610cef0bcb5c6e96a1c2656207d477
Ivan “CLOVIS” Canet <ivan.canet@gmail.com>
2022-06-13 14:43:24 +0200

    Expand type information into a string representation

--- src/interface.rs
+++ src/interface.rs
@@ -3,6 +3,8 @@
 //! This module creates three data structures, [`ModuleInfo`], [`ClassInfo`] and [`FieldInfo`],
 //! which are responsible for generating parts of the interface files.
 
+use std::fmt::{Display, Formatter};
+
 /// Python Interface information for a module.
 #[derive(Debug)]
 pub struct ModuleInfo {}
@@ -118,10 +120,129 @@ pub enum TypeInfo<'a> {
     Builtin(&'a str),
 
     /// Any type that doesn't receive special treatment from PyO3.
-    Other {
+    Class {
         module: &'a str,
         name: &'a str,
     },
+}
+
+impl<'a> TypeInfo<'a> {
+    pub fn module_name(&self) -> Option<&'a str> {
+        match self {
+            TypeInfo::Class { module, .. } => { Some(module) }
+            TypeInfo::Builtin(_) => None,
+            _ => Some("typing"),
+        }
+    }
+}
+
+impl<'a> Display for TypeInfo<'a> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TypeInfo::Any => write!(f, "Any"),
+            TypeInfo::None => write!(f, "None"),
+            TypeInfo::Callable(args, output) => {
+                if let Some(args) = args {
+                    write!(f, "Callable[[")?;
+                    let mut is_first = true;
+                    for arg in *args {
+                        if !is_first {
+                            write!(f, ", ")?;
+                        }
+                        write!(f, "{}", arg)?;
+                        is_first = false
+                    }
+                    write!(f, "], {}]", output)
+                } else {
+                    write!(f, "Callable[..., {}]", output)
+                }
+            }
+            TypeInfo::NoReturn => write!(f, "NoReturn"),
+            TypeInfo::AnyTuple => write!(f, "Tuple[...]"),
+            TypeInfo::Tuple(args) => {
+                write!(f, "Tuple[")?;
+                let mut is_first = true;
+                for arg in *args {
+                    if !is_first {
+                        write!(f, ", ")?;
+                    }
+                    write!(f, "{}", arg)?;
+                    is_first = false;
+                }
+                if args.is_empty() {
+                    write!(f, "()")?;
+                }
+                write!(f, "]")
+            }
+            TypeInfo::UnsizedTuple(arg) => write!(f, "Tuple[{arg}, ...]"),
+            TypeInfo::Union(args) => {
+                write!(f, "Union[")?;
+                let mut is_first = true;
+                for arg in *args {
+                    if !is_first {
+                        write!(f, ", ")?;
+                    }
+                    write!(f, "{}", arg)?;
+                    is_first = false;
+                }
+                write!(f, "]")
+            }
+            TypeInfo::Optional(arg) => write!(f, "Optional[{arg}]"),
+            TypeInfo::Dict(key, value) => write!(f, "Dict[{key}, {value}]"),
+            TypeInfo::Mapping(key, value) => write!(f, "Mapping[{key}, {value}]"),
+            TypeInfo::List(arg) => write!(f, "List[{arg}]"),
+            TypeInfo::Set(arg) => write!(f, "Set[{arg}]"),
+            TypeInfo::FrozenSet(arg) => write!(f, "FrozenSet[{arg}]"),
+            TypeInfo::Sequence(arg) => write!(f, "Sequence[{arg}]"),
+            TypeInfo::Iterator(arg) => write!(f, "Iterator[{arg}]"),
+            TypeInfo::Iterable(arg) => write!(f, "Iterable[{arg}]"),
+            TypeInfo::Builtin(name) => write!(f, "{}", name),
+            TypeInfo::Class { name, .. } => write!(f, "{}", name),
+        }
+    }
+}
+
+#[cfg(test)]
+#[test]
+fn type_info_display() {
+    // Ensure the type infos follow the syntax defined in PEP484
+    assert_eq!("Any", format!("{}", TypeInfo::Any));
+    assert_eq!("None", format!("{}", TypeInfo::None));
+    assert_eq!("int", format!("{}", TypeInfo::Builtin("int")));
+    assert_eq!("Callable[..., int]", format!("{}", TypeInfo::Callable(None, &TypeInfo::Builtin("int"))));
+    assert_eq!("Callable[[int, bool], int]", format!("{}", TypeInfo::Callable(Some(&[&TypeInfo::Builtin("int"), &TypeInfo::Builtin("bool")]), &TypeInfo::Builtin("int"))));
+    assert_eq!("NoReturn", format!("{}", TypeInfo::NoReturn));
+    assert_eq!("Tuple[...]", format!("{}", TypeInfo::AnyTuple));
+    assert_eq!("Tuple[int, bool, int]", format!("{}", TypeInfo::Tuple(&[&TypeInfo::Builtin("int"), &TypeInfo::Builtin("bool"), &TypeInfo::Builtin("int")])));
+    assert_eq!("Tuple[()]", format!("{}", TypeInfo::Tuple(&[])));
+    assert_eq!("Tuple[int, ...]", format!("{}", TypeInfo::UnsizedTuple(&TypeInfo::Builtin("int"))));
+    assert_eq!("Union[int, bool]", format!("{}", TypeInfo::Union(&[&TypeInfo::Builtin("int"), &TypeInfo::Builtin("bool")])));
+    assert_eq!("Optional[int]", format!("{}", TypeInfo::Optional(&TypeInfo::Builtin("int"))));
+    assert_eq!("Optional[Any]", format!("{}", TypeInfo::Optional(&TypeInfo::Any)));
+    assert_eq!("Dict[str, int]", format!("{}", TypeInfo::Dict(&TypeInfo::Builtin("str"), &TypeInfo::Builtin("int"))));
+    assert_eq!("Mapping[str, int]", format!("{}", TypeInfo::Mapping(&TypeInfo::Builtin("str"), &TypeInfo::Builtin("int"))));
+    assert_eq!("List[str]", format!("{}", TypeInfo::List(&TypeInfo::Builtin("str"))));
+    assert_eq!("Set[str]", format!("{}", TypeInfo::Set(&TypeInfo::Builtin("str"))));
+    assert_eq!("FrozenSet[str]", format!("{}", TypeInfo::FrozenSet(&TypeInfo::Builtin("str"))));
+    assert_eq!("Sequence[str]", format!("{}", TypeInfo::Sequence(&TypeInfo::Builtin("str"))));
+    assert_eq!("Iterable[str]", format!("{}", TypeInfo::Iterable(&TypeInfo::Builtin("str"))));
+    assert_eq!("Iterator[str]", format!("{}", TypeInfo::Iterator(&TypeInfo::Builtin("str"))));
+    assert_eq!("MyClass", format!("{}", TypeInfo::Class { module: "whatever", name: "MyClass" }));
+
+    // Just to be sure, a complicated (real life!) example
+    assert_eq!(
+        "List[Callable[[Common, Common], List[List[Tuple[Common, Common]]]]]",
+        format!("{}", TypeInfo::List(
+            &TypeInfo::Callable(
+                Some(&[&TypeInfo::Class { module: "foo", name: "Common" }, &TypeInfo::Class { module: "foo", name: "Common" }]),
+                &TypeInfo::List(
+                    &TypeInfo::List(
+                        &TypeInfo::Tuple(&[&TypeInfo::Class { module: "foo", name: "Common" }, &TypeInfo::Class { module: "foo", name: "Common" }])
+                    )
+                ),
+            ),
+        )),
+    );
 }
 
 pub trait GetClassInfo {
