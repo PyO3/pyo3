3fd0c0e1421c71b0e5755427eaaa886cc67cd9c1
Andrew Burkett <burkett.andrew@gmail.com>
2022-06-23 13:31:44 -0700

    Fix PyObject_CallNoArgs python version cfg (#2476)
    * Fix PyObject_CallNoArgs python version cfg
    
    PyObject_CallNoArgs was added to python 3.9 but not to limited api until 3.10 per https://docs.python.org/3/c-api/call.html#c.PyObject_CallNoArgs
    
    * Update change log
    
    * Fix uses of PyObject_CallNoArgs
    
    Co-authored-by: Andrew Burkett <andrew.burkett@crowdstrike.com>

--- src/types/any.rs
+++ src/types/any.rs
@@ -484,7 +484,10 @@ impl PyAny {
     /// This is equivalent to the Python expression `help()`.
     pub fn call0(&self) -> PyResult<&PyAny> {
         cfg_if::cfg_if! {
-            if #[cfg(all(Py_3_9, not(PyPy)))] {
+            if #[cfg(all(
+                not(PyPy),
+                any(Py_3_10, all(not(Py_LIMITED_API), Py_3_9)) // PyObject_CallNoArgs was added to python in 3.9 but to limited API in 3.10
+            ))] {
                 // Optimized path on python 3.9+
                 unsafe {
                     self.py().from_owned_ptr_or_err(ffi::PyObject_CallNoArgs(self.as_ptr()))
