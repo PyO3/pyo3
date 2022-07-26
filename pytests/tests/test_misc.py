78ba70d2b4cdae1228561700bab62da793801d18
David Hewitt <1939362+davidhewitt@users.noreply.github.com>
2022-07-26 19:28:03 +0100

    pymodule: only allow initializing once per process

--- pytests/tests/test_misc.py
+++ pytests/tests/test_misc.py
@@ -1,6 +1,24 @@
+import importlib
+import platform
+
 import pyo3_pytests.misc
+import pytest
 
 
 def test_issue_219():
     # Should not deadlock
     pyo3_pytests.misc.issue_219()
+
+
+@pytest.mark.skipif(
+    platform.python_implementation() == "PyPy",
+    reason="PyPy does not reinitialize the module (appears to be some internal caching)",
+)
+def test_second_module_import_fails():
+    spec = importlib.util.find_spec("pyo3_pytests.pyo3_pytests")
+
+    with pytest.raises(
+        ImportError,
+        match="PyO3 modules may only be initialized once per interpreter process",
+    ):
+        importlib.util.module_from_spec(spec)
