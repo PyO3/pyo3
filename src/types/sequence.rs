462084243fe84120b1187e0b7cdf79c75c2a8af1
Adam Reichold <adam.reichold@t-online.de>
2022-09-13 22:05:12 +0200

    Further relax extract_sequence as neither PyObject::len nor PyObject::iter actually need the sequence protocol.

--- src/types/sequence.rs
+++ src/types/sequence.rs
@@ -299,18 +299,9 @@ fn extract_sequence<'s, T>(obj: &'s PyAny) -> PyResult<Vec<T>>
 where
     T: FromPyObject<'s>,
 {
-    // Types that pass `PySequence_Check` usually implement enough of the sequence protocol
-    // to support this function and if not, we will only fail extraction safely.
-    let seq = unsafe {
-        if ffi::PySequence_Check(obj.as_ptr()) != 0 {
-            <PySequence as PyTryFrom>::try_from_unchecked(obj)
-        } else {
-            return Err(PyDowncastError::new(obj, "Sequence").into());
-        }
-    };
-
-    let mut v = Vec::with_capacity(seq.len().unwrap_or(0));
-    for item in seq.iter()? {
+    let iter = obj.iter()?;
+    let mut v = Vec::with_capacity(obj.len().unwrap_or(0));
+    for item in iter {
         v.push(item?.extract::<T>()?);
     }
     Ok(v)
