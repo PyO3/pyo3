465fa1f2eba61160fd7b80a5489ac143e4646277
Federico Dolce <psykopear@gmail.com>
2022-09-20 08:57:23 +0200

    Applied rustfmt

--- src/conversions/chrono.rs
+++ src/conversions/chrono.rs
@@ -16,7 +16,10 @@
 //! chrono = "0.4"
 // workaround for `extended_key_value_attributes`: https://github.com/rust-lang/rust/issues/82768#issuecomment-803935643
 #![cfg_attr(docsrs, cfg_attr(docsrs, doc = concat!("pyo3 = { version = \"", env!("CARGO_PKG_VERSION"),  "\", features = [\"chrono\"] }")))]
-#![cfg_attr(not(docsrs), doc = "pyo3 = { version = \"*\", features = [\"chrono\"] }")]
+#![cfg_attr(
+    not(docsrs),
+    doc = "pyo3 = { version = \"*\", features = [\"chrono\"] }"
+)]
 //! ```
 //!
 //! Note that you must use compatible versions of chrono and PyO3.
