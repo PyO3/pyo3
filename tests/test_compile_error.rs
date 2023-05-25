08bdc32b6ed4291ad9cdf64bf64f50326480fd1b
Adam Reichold <adam.reichold@t-online.de>
2023-05-25 20:02:51 +0200

    Move traverse-bare-self UI test to version-gated section as its error output is not stable between MSRV and current stable.

--- tests/test_compile_error.rs
+++ tests/test_compile_error.rs
@@ -45,7 +45,6 @@ fn _test_compile_errors() {
     t.compile_fail("tests/ui/invalid_pymethod_names.rs");
     t.compile_fail("tests/ui/invalid_pymodule_args.rs");
     t.compile_fail("tests/ui/reject_generics.rs");
-    t.compile_fail("tests/ui/traverse_bare_self.rs");
 
     tests_rust_1_49(&t);
     tests_rust_1_56(&t);
@@ -119,6 +118,7 @@ fn _test_compile_errors() {
         t.compile_fail("tests/ui/not_send2.rs");
         t.compile_fail("tests/ui/not_send3.rs");
         t.compile_fail("tests/ui/get_set_all.rs");
+        t.compile_fail("tests/ui/traverse_bare_self.rs");
     }
 
     #[rustversion::before(1.63)]
