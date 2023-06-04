f28c280fbf1066e55cd6b77f2f81c59e801e8765
David Hewitt <1939362+davidhewitt@users.noreply.github.com>
2023-06-04 22:58:44 +0100

    run cargo check instead of clippy for MSRV in CI

--- tests/test_class_attributes.rs
+++ tests/test_class_attributes.rs
@@ -96,7 +96,7 @@ fn recursive_class_attributes() {
 }
 
 #[test]
-#[cfg(panic = "unwind")]
+#[cfg_attr(cfg_panic, cfg(panic = "unwind"))]
 fn test_fallible_class_attribute() {
     use pyo3::{exceptions::PyValueError, types::PyString};
 
