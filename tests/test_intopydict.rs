66a3442e1b464599883c97925baa42c55d18196d
Vidur Modgil <vidurmodgil@Vidurs-MacBook-Pro.local>
2023-07-28 12:11:55 -0400

    tests better?

--- tests/test_intopydict.rs
+++ tests/test_intopydict.rs
@@ -1,19 +1,34 @@
-use pyo3::types::{PyDict, IntoPyDict};
+use pyo3::{types::{PyDict, IntoPyDict}, Python};
 use pyo3_macros::IntoPyDict;
 
 pub trait TestTrait<'a> {
 
 }
 
-#[derive(IntoPyDict)]
+#[derive(IntoPyDict, PartialEq, Debug)]
 pub struct Test1 {
     x: u8,
     y: u8,
 }
 
 #[derive(IntoPyDict)]
-pub struct Test<T: IntoPyDict> {
+pub struct Test {
+    v: Vec<Vec<Test1>>,
     j: Test1,
     h: u8,
-    x: T
+}
+
+#[test]
+fn test_into_py_dict_derive() {
+    let test_struct = Test {
+        v: vec![vec![Test1 { x: 9, y: 10 }]],
+        j: Test1 { x: 10, y: 11 },
+        h: 9,
+    };
+
+    Python::with_gil(|py| {
+        let py_dict = test_struct.into_py_dict(py);
+        let h: u8 = py_dict.get_item("h").unwrap().extract().unwrap();
+        assert_eq!(h, 9);
+    });
 }
