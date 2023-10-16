10c007c2318e3af69de0be903a323ff139acc04e
Michael Huang <michaelhly@gmail.com>
2023-10-15 23:22:46 -0400

    Move tests

--- src/types/mappingproxy.rs
+++ src/types/mappingproxy.rs
@@ -155,8 +155,9 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::types::{PyInt, PyString, PyTuple};
     #[cfg(not(PyPy))]
-    use crate::{types::PyTuple, PyTypeInfo, Python};
+    use crate::{PyTypeInfo, Python};
     use std::collections::{BTreeMap, HashMap};
 
     #[test]
@@ -517,6 +518,86 @@ mod tests {
             let mappingproxy = abc_mappingproxy(py);
             let items = mappingproxy.call_method0("items").unwrap();
             assert!(items.is_instance(PyDictItems::type_object(py)).unwrap());
+        })
+    }
+
+    #[test]
+    fn get_value_from_mappingproxy_of_strings() {
+        Python::with_gil(|py: Python<'_>| {
+            let mut map = HashMap::new();
+            map.insert("first key", "first value");
+            map.insert("second key", "second value");
+            map.insert("third key", "third value");
+
+            let mappingproxy = map.iter().into_py_mappingproxy(py).unwrap();
+
+            assert_eq!(
+                map.into_iter().collect::<Vec<(&str, &str)>>(),
+                mappingproxy
+                    .iter()
+                    .map(|object| (
+                        object.0.downcast::<PyString>().unwrap().to_str().unwrap(),
+                        object.1.downcast::<PyString>().unwrap().to_str().unwrap()
+                    ))
+                    .collect::<Vec<(&str, &str)>>()
+            );
+        })
+    }
+
+    #[test]
+    fn get_value_from_mappingproxy_of_integers() {
+        Python::with_gil(|py: Python<'_>| {
+            const LEN: usize = 10_000;
+            let items: Vec<(usize, usize)> = (1..LEN).map(|i| (i, i - 1)).collect();
+            let mappingproxy = items.to_vec().into_py_mappingproxy(py).unwrap();
+            assert_eq!(
+                items,
+                mappingproxy
+                    .iter()
+                    .map(|object| (
+                        object
+                            .0
+                            .downcast::<PyInt>()
+                            .unwrap()
+                            .extract::<usize>()
+                            .unwrap(),
+                        object
+                            .1
+                            .downcast::<PyInt>()
+                            .unwrap()
+                            .extract::<usize>()
+                            .unwrap()
+                    ))
+                    .collect::<Vec<(usize, usize)>>()
+            );
+            for index in 1..LEN {
+                assert_eq!(
+                    mappingproxy
+                        .get_item(index)
+                        .unwrap()
+                        .extract::<usize>()
+                        .unwrap(),
+                    index - 1
+                );
+            }
+        })
+    }
+
+    #[test]
+    fn iter_mappingproxy_nosegv() {
+        Python::with_gil(|py| {
+            const LEN: usize = 10_000_000;
+            let mappingproxy = (0..LEN as u64)
+                .map(|i| (i, i * 2))
+                .into_py_mappingproxy(py)
+                .unwrap();
+
+            let mut sum = 0;
+            for (k, _v) in mappingproxy.iter() {
+                let i: u64 = k.extract().unwrap();
+                sum += i;
+            }
+            assert_eq!(sum, 49_999_995_000_000);
         })
     }
 }
