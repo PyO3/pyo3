5ac56b8eb03d94e5b3ec0e0a801caf4da168b29b
David Hewitt <mail@davidhewitt.dev>
2023-11-24 03:11:05 +0000

    improve error for invalid `#[classmethod]` receivers

--- tests/ui/invalid_pyfunctions.rs
+++ tests/ui/invalid_pyfunctions.rs
@@ -15,4 +15,12 @@ fn destructured_argument((a, b): (i32, i32)) {}
 #[pyfunction]
 fn function_with_required_after_option(_opt: Option<i32>, _x: i32) {}
 
+#[pyfunction(pass_module)]
+fn pass_module_but_no_arguments<'py>() {}
+
+#[pyfunction(pass_module)]
+fn first_argument_not_module<'py>(string: &str, module: &'py PyModule) -> PyResult<&'py str> {
+    module.name()
+}
+
 fn main() {}
