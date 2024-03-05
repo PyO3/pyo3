f328a202f407f946a6aeffd441dc6c53c3ddc330
goulart-paul <paul.goulart@eng.ox.ac.uk>
2024-03-05 14:27:48 +0000

    added unit test

--- src/stdio.rs
+++ src/stdio.rs
@@ -14,12 +14,12 @@
 //! writeln!(stdout, "Hello, world!").unwrap();
 //! ```
 
-use crate::types::PyString;
 use crate::intern;
 use crate::prelude::*;
+use crate::types::PyString;
 use std::io::{LineWriter, Write};
 
-/// Implements `std::io::Write` for a `PyAny` object.    The underlying 
+/// Implements `std::io::Write` for a `PyAny` object.    The underlying
 ///  Python object must provide both `write` and `flush` methods.
 pub struct PyWriter(Py<PyAny>);
 
@@ -49,7 +49,7 @@ pub fn stdout() -> PyWriter {
 /// Construct a new `PyWriter` for Python's `sys.stderr` stream.
 pub fn stderr() -> PyWriter {
     get_stdio_pywriter("stderr")
-}   
+}
 
 /// Construct a new `PyWriter` for Python's `sys.__stdout__` stream.
 pub fn __stdout__() -> PyWriter {
@@ -59,32 +59,29 @@ pub fn __stdout__() -> PyWriter {
 /// Construct a new `PyWriter` for Python's `sys.__stderr__` stream.
 pub fn __stderr__() -> PyWriter {
     get_stdio_pywriter("__stderr__")
-}   
+}
 
 impl Write for PyWriter {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        Python::with_gil(|py| {
-            PyWriterBound(self.0.bind(py)).write(buf)
-        })
+        Python::with_gil(|py| PyWriterBound(self.0.bind(py)).write(buf))
     }
     fn flush(&mut self) -> std::io::Result<()> {
-        Python::with_gil(|py| {
-            PyWriterBound(self.0.bind(py)).flush()
-        })
+        Python::with_gil(|py| PyWriterBound(self.0.bind(py)).flush())
     }
 }
 
-impl Write for PyWriterBound<'_, '_>  {
+impl Write for PyWriterBound<'_, '_> {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        let str = PyString::new_bound(self.0.py(),&String::from_utf8_lossy(buf));
-        self.0.call_method1(intern!(self.0.py(),"write"), (str,))
+        let str = PyString::new_bound(self.0.py(), &String::from_utf8_lossy(buf));
+        self.0
+            .call_method1(intern!(self.0.py(), "write"), (str,))
             .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
         Ok(buf.len())
     }
     fn flush(&mut self) -> std::io::Result<()> {
-         self.0.call_method0(intern!(self.0.py(),"flush"))
-                .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        self.0
+            .call_method0(intern!(self.0.py(), "flush"))
+            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
         Ok(())
     }
 }
-
