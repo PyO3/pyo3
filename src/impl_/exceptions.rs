5ce07c32eb35a7a6d3dc339d70ce72b145910284
David Hewitt <mail@davidhewitt.dev>
2024-03-31 20:36:17 +0100

    newsfragment and tidy up

--- src/impl_/exceptions.rs
+++ src/impl_/exceptions.rs
@@ -1,8 +1,4 @@
-use crate::{
-    sync::GILOnceCell,
-    types::{PyAnyMethods, PyTracebackMethods, PyType},
-    Bound, Py, Python,
-};
+use crate::{sync::GILOnceCell, types::PyType, Bound, Py, Python};
 
 pub struct ImportedExceptionTypeObject {
     imported_value: GILOnceCell<Py<PyType>>,
@@ -21,27 +17,12 @@ impl ImportedExceptionTypeObject {
 
     pub fn get<'py>(&self, py: Python<'py>) -> &Bound<'py, PyType> {
         self.imported_value
-            .get_or_init(py, || {
-                let imp = py.import_bound(self.module).unwrap_or_else(|err| {
-                    let traceback = err
-                        .traceback_bound(py)
-                        .map(|tb| tb.format().expect("raised exception will have a traceback"))
-                        .unwrap_or_default();
-                    panic!(
-                        "Can not import module {}: {}\n{}",
-                        self.module, err, traceback
-                    );
-                });
-                let cls = imp.getattr(self.name).unwrap_or_else(|_| {
-                    panic!(
-                        "Can not load exception class: {}.{}",
-                        self.module, self.name
-                    )
-                });
-
-                cls.extract()
-                    .expect("Imported exception should be a type object")
+            .get_or_try_init_type_ref(py, self.module, self.name)
+            .unwrap_or_else(|e| {
+                panic!(
+                    "failed to import exception {}.{}: {}",
+                    self.module, self.name, e
+                )
             })
-            .bind(py)
     }
 }
