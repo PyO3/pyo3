93a161411bffe03ef45003c3a3931cc9d7bf33f0
Mike Foster <i.am.mike.foster@gmail.com>
2024-04-12 08:08:40 +0000

    macro test is good, import_pyo3_from not adding fn header

--- pyo3-testing/src/lib.rs
+++ pyo3-testing/src/lib.rs
@@ -1,14 +1,12 @@
 use proc_macro2::{Span, TokenStream as TokenStream2};
 use quote::quote;
 use syn::{
-    parse::{Parse, ParseStream},
-    token::Colon,
-    Attribute, Ident,
+    parse::{Parse, ParseStream}, parse2, token::Colon, Attribute, Ident, ItemFn
 };
 
 /// Takes a code block which should be executed using the python gil, after importing a pyo3-wrapped
 /// function and adds the required `import` and `with_gil` statements.
-/// 
+///
 /// Technically this is the equivalent to the python statements:
 /// ```python
 /// import module
@@ -76,13 +74,20 @@ impl Parse for Pyo3Import {
 /// - `Some(Pyo3Import)` for Attributes with the path `pyo3import` e.g.:
 /// `#[pyo3import(foo: from foo import bar)]`
 /// - `None` for Attributes with other paths.
-#[allow(dead_code)] // Not yet fully implemented
-fn parsepyo3import(import: Attribute) -> Option<Pyo3Import> {
+fn parsepyo3import(import: &Attribute) -> Option<Pyo3Import> {
     if import.path().is_ident("pyo3import") {
         Some(import.parse_args().unwrap())
     } else {
         None
     }
+}
+
+#[allow(dead_code)] // Not yet fully implemented
+fn impl_pyo3test(_attr: TokenStream2, input: TokenStream2) -> TokenStream2 {
+    let input: ItemFn = parse2(input).unwrap();
+    let import = parsepyo3import(&input.attrs[0]).unwrap();
+    let input = quote!(input);
+    import_pyo3_from(import, input)
 }
 
 #[cfg(test)]
@@ -138,8 +143,41 @@ mod tests {
             functionname: "function".to_string(),
         };
 
-        let parsed = parsepyo3import(import);
+        let parsed = parsepyo3import(&import);
 
         assert_eq!(parsed.unwrap(), expected)
+    }
+
+    #[test]
+    fn test_macro() {
+        let attr = quote! {
+        };
+
+        let input = quote! {
+            #[pyo3import(foo_o3: from pyfoo import pybar)]
+            fn pytest() {
+                assert!(true)
+            }
+        };
+
+        let expected = quote! {
+            fn pytest() {
+                pyo3::append_to_inittab!(foo_o3);
+                pyo3::prepare_freethreaded_python();
+                Python::with_gil(|py| {
+                    let pyfoo = py
+                    .import_bound("pyfoo")
+                    .expect("Failed to import pyfoo");
+                    let pybar = pyfoo
+                    .getattr("pybar")
+                    .expect("Failed to get pybar function");
+                    assert!(true);
+                });
+            };
+        };
+
+        let result = impl_pyo3test(attr, input);
+
+        assert_eq!(result.to_string(), expected.to_string())
     }
 }
