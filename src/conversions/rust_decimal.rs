dc436fd03de49797a763838f2b6c790ae1fbff74
David <davidmatos06@gmail.com>
2024-04-14 21:43:36 +0200

    Modify let bindings location

--- src/conversions/rust_decimal.rs
+++ src/conversions/rust_decimal.rs
@@ -61,11 +61,11 @@ use std::str::FromStr;
 impl FromPyObject<'_> for Decimal {
     fn extract_bound(obj: &Bound<'_, PyAny>) -> PyResult<Self> {
         // use the string representation to not be lossy
-        let py_str = &obj.str()?;
-        let rs_str = &py_str.to_cow()?;
         if let Ok(val) = obj.extract() {
             Ok(Decimal::new(val, 0))
         } else {
+            let py_str = &obj.str()?;
+            let rs_str = &py_str.to_cow()?;
             Decimal::from_str(rs_str).or_else(|_| {
                 Decimal::from_scientific(rs_str).map_err(|e| PyValueError::new_err(e.to_string()))
             })
