529e2a95f34af1c1f2c1b8c8581e27f811dac4d3
Mike Foster <i.am.mike.foster@gmail.com>
2024-04-20 07:14:40 +0000

    add explanation of inability to ui test invalid (out of scope) pyo3methods or pyo3functions

--- tests/ui/invalid_pyo3imports.rs
+++ tests/ui/invalid_pyo3imports.rs
@@ -34,4 +34,22 @@ fn no_colon() {}
 #[pyo3import(py_adders: from adders import)]
 fn missing_function() {}
 
+// This will compile fine with trybuild due to the #[test] which is added to the
+// wrapped function. see https://github.com/dtolnay/trybuild/issues/231
+//
+// Only when actually trying to run the test will you get an error:
+// error[E0433]: failed to resolve: use of undeclared crate or module `py_addrs`
+// --> tests/ui/invalid_pyo3imports.rs:47:14
+//    |
+// 47 | #[pyo3import(py_addrs: from adders import addone)]
+//    |              ^^^^^^^^ use of undeclared crate or module `py_addrs`
+#[pyo3test]
+#[pyo3import(py_addrs: from adders import addone)]
+fn invalid_rust_method() {}
+
+// This passes without error ... for copy-pasting ;)
+#[pyo3test]
+#[pyo3import(py_adders: from adders import addone)]
+fn good_example() {}
+
 fn main() {}
