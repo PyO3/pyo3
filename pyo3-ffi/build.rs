388d1760b5d6545c94925dafe0d640200b9fded2
David Hewitt <mail@davidhewitt.dev>
2024-05-25 23:41:26 +0100

    ci: start testing on 3.13-dev (#4184)
    * ci: start testing on 3.13-dev
    
    * ffi fixes for 3.13 beta 1
    
    * support 3.13
    
    * move gevent to be binary-only
    
    * adjust for div_ceil
    
    * fixup pytests

--- pyo3-ffi/build.rs
+++ pyo3-ffi/build.rs
@@ -17,7 +17,7 @@ const SUPPORTED_VERSIONS_CPYTHON: SupportedVersions = SupportedVersions {
     min: PythonVersion { major: 3, minor: 7 },
     max: PythonVersion {
         major: 3,
-        minor: 12,
+        minor: 13,
     },
 };
 
