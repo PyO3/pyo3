9b89c40cbb530344a20b8b0176715a3f2bd2f4b7
Michael Gilbert <git.3mc1o@aleeas.com>
2024-06-05 17:00:52 -0700

    update: fixed test function names

--- tests/test_class_formatting.rs
+++ tests/test_class_formatting.rs
@@ -36,7 +36,7 @@ pub enum MyEnum3 {
 }
 
 #[test]
-fn test_enum_class_attr() {
+fn test_enum_class_fmt() {
     Python::with_gil(|py| {
         let var1 = Py::new(py, MyEnum::Variant).unwrap();
         let var2 = Py::new(py, MyEnum2::Variant).unwrap();
@@ -58,7 +58,7 @@ pub struct Point {
 }
 
 #[test]
-fn test_custom_str_representation() {
+fn test_custom_struct_custom_str() {
     Python::with_gil(|py| {
         let var1 = Py::new(py, Point { x: 1, y: 2, z: 3 }).unwrap();
         py_assert!(py, var1, "str(var1) == 'X: 1, Y: 2, Z: 3'");
@@ -80,7 +80,7 @@ impl Display for Point2 {
 }
 
 #[test]
-fn test_display_trait_implementation() {
+fn test_struct_str() {
     Python::with_gil(|py| {
         let var1 = Py::new(py, Point2 { x: 1, y: 2, z: 3 }).unwrap();
         py_assert!(py, var1, "str(var1) == '(1, 2, 3)'");
@@ -95,7 +95,7 @@ enum ComplexEnumWithStr {
 }
 
 #[test]
-fn test_str_representation_complex_enum() {
+fn test_custom_complex_enum_str() {
     Python::with_gil(|py| {
         let var1 = Py::new(py, ComplexEnumWithStr::A(45)).unwrap();
         let var2 = Py::new(
