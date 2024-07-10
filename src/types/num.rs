af11d4c36e605f015e921ea5af2b4ce994baa4a6
William Takeshi <williamtpereira@gmail.com>
2024-07-10 19:26:10 -0300

    remove constructors, improve partialeq implementation

--- src/types/num.rs
+++ src/types/num.rs
@@ -1,7 +1,6 @@
 use super::any::PyAnyMethods;
 
-use crate::{ffi, ffi_ptr_ext::FfiPtrExt, instance::Bound, PyAny, Python};
-use std::os::raw::c_long;
+use crate::{ffi, instance::Bound, PyAny};
 
 /// Represents a Python `int` object.
 ///
@@ -14,246 +13,47 @@ pub struct PyLong(PyAny);
 
 pyobject_native_type_core!(PyLong, pyobject_native_static_type_object!(ffi::PyLong_Type), #checkfunction=ffi::PyLong_Check);
 
-impl PyLong {
-    /// Creates a new `PyLong` from a C `long` integer.
-    /// # Returns
-    /// A [`Bound`] reference to a `PyLong` object representing the given `val`.
-    #[inline]
-    pub fn new_bound_from_c_long(py: Python<'_>, val: c_long) -> Bound<'_, Self> {
-        unsafe {
-            ffi::PyLong_FromLong(val)
-                .assume_owned(py)
-                .downcast_into_unchecked()
+macro_rules! int_compare {
+    ($rust_type: ty) => {
+        impl PartialEq<$rust_type> for Bound<'_, PyLong> {
+            #[inline]
+            fn eq(&self, other: &$rust_type) -> bool {
+                if let Ok(value) = self.extract::<$rust_type>() {
+                    value == *other
+                } else {
+                    false
+                }
+            }
         }
-    }
-}
-
-/// Implementation of functionality for [`PyLong`].
-///
-/// These methods are defined for the `Bound<'py, PyLong>` smart pointer, so to use method call
-/// syntax these methods are separated into a trait, because stable Rust does not yet support
-/// `arbitrary_self_types`.
-#[doc(alias = "PyLong")]
-pub trait PyLongMethods<'py>: crate::sealed::Sealed {
-    /// Gets the value of this int.
-    fn value(&self) -> c_long;
-}
-
-impl<'py> PyLongMethods<'py> for Bound<'py, PyLong> {
-    fn value(&self) -> c_long {
-        unsafe {
-            // Safety: self is PyLong object
-            ffi::PyLong_AsLong(self.as_ptr())
+        impl PartialEq<Bound<'_, PyLong>> for $rust_type {
+            #[inline]
+            fn eq(&self, other: &Bound<'_, PyLong>) -> bool {
+                if let Ok(value) = other.extract::<$rust_type>() {
+                    value == *self
+                } else {
+                    false
+                }
+            }
         }
-    }
+    };
 }
 
-/// Implement <Bound<'_, PyLong>> == i8 comparisons
-impl PartialEq<i8> for Bound<'_, PyLong> {
-    #[inline]
-    fn eq(&self, other: &i8) -> bool {
-        self.value() == *other as c_long
-    }
-}
-
-/// Implement i8 == <Bound<'_, PyLong>> comparisons
-impl PartialEq<Bound<'_, PyLong>> for i8 {
-    #[inline]
-    fn eq(&self, other: &Bound<'_, PyLong>) -> bool {
-        *self as c_long == other.value()
-    }
-}
-
-/// Implement <Bound<'_, PyLong>> == u8 comparisons
-impl PartialEq<u8> for Bound<'_, PyLong> {
-    #[inline]
-    fn eq(&self, other: &u8) -> bool {
-        self.value() == *other as c_long
-    }
-}
-
-/// Implement u8 == <Bound<'_, PyLong>> comparisons
-impl PartialEq<Bound<'_, PyLong>> for u8 {
-    #[inline]
-    fn eq(&self, other: &Bound<'_, PyLong>) -> bool {
-        *self as c_long == other.value()
-    }
-}
-
-/// Implement <Bound<'_, PyLong>> == i16 comparisons
-impl PartialEq<i16> for Bound<'_, PyLong> {
-    #[inline]
-    fn eq(&self, other: &i16) -> bool {
-        self.value() == *other as c_long
-    }
-}
-
-/// Implement i16 == <Bound<'_, PyLong>> comparisons
-impl PartialEq<Bound<'_, PyLong>> for i16 {
-    #[inline]
-    fn eq(&self, other: &Bound<'_, PyLong>) -> bool {
-        *self as c_long == other.value()
-    }
-}
-
-/// Implement <Bound<'_, PyLong>> == u16 comparisons
-impl PartialEq<u16> for Bound<'_, PyLong> {
-    #[inline]
-    fn eq(&self, other: &u16) -> bool {
-        self.value() == *other as c_long
-    }
-}
-
-/// Implement u16 == <Bound<'_, PyLong>> comparisons
-impl PartialEq<Bound<'_, PyLong>> for u16 {
-    #[inline]
-    fn eq(&self, other: &Bound<'_, PyLong>) -> bool {
-        *self as c_long == other.value()
-    }
-}
-
-/// Implement <Bound<'_, PyLong>> == i32 comparisons
-impl PartialEq<i32> for Bound<'_, PyLong> {
-    #[inline]
-    fn eq(&self, other: &i32) -> bool {
-        self.value() == *other as c_long
-    }
-}
-
-/// Implement i32 == <Bound<'_, PyLong>> comparisons
-impl PartialEq<Bound<'_, PyLong>> for i32 {
-    #[inline]
-    fn eq(&self, other: &Bound<'_, PyLong>) -> bool {
-        *self as c_long == other.value()
-    }
-}
-
-/// Implement <Bound<'_, PyLong>> == u32 comparisons
-impl PartialEq<u32> for Bound<'_, PyLong> {
-    #[inline]
-    fn eq(&self, other: &u32) -> bool {
-        self.value() == *other as c_long
-    }
-}
-
-/// Implement u32 == <Bound<'_, PyLong>> comparisons
-impl PartialEq<Bound<'_, PyLong>> for u32 {
-    #[inline]
-    fn eq(&self, other: &Bound<'_, PyLong>) -> bool {
-        *self as c_long == other.value()
-    }
-}
-
-/// Implement <Bound<'_, PyLong>> == i64 comparisons
-impl PartialEq<i64> for Bound<'_, PyLong> {
-    #[inline]
-    fn eq(&self, other: &i64) -> bool {
-        self.value() == *other as c_long
-    }
-}
-
-/// Implement i64 == <Bound<'_, PyLong>> comparisons
-impl PartialEq<Bound<'_, PyLong>> for i64 {
-    #[inline]
-    fn eq(&self, other: &Bound<'_, PyLong>) -> bool {
-        *self as c_long == other.value()
-    }
-}
-
-/// Implement <Bound<'_, PyLong>> == u64 comparisons
-impl PartialEq<u64> for Bound<'_, PyLong> {
-    #[inline]
-    fn eq(&self, other: &u64) -> bool {
-        self.value() == *other as c_long
-    }
-}
-
-/// Implement u64 == <Bound<'_, PyLong>> comparisons
-impl PartialEq<Bound<'_, PyLong>> for u64 {
-    #[inline]
-    fn eq(&self, other: &Bound<'_, PyLong>) -> bool {
-        *self as c_long == other.value()
-    }
-}
-
-/// Implement <Bound<'_, PyLong>> == i128 comparisons
-impl PartialEq<i128> for Bound<'_, PyLong> {
-    #[inline]
-    fn eq(&self, other: &i128) -> bool {
-        self.value() == *other as c_long
-    }
-}
-
-/// Implement i128 == <Bound<'_, PyLong>> comparisons
-impl PartialEq<Bound<'_, PyLong>> for i128 {
-    #[inline]
-    fn eq(&self, other: &Bound<'_, PyLong>) -> bool {
-        *self as c_long == other.value()
-    }
-}
-
-/// Implement <Bound<'_, PyLong>> == u128 comparisons
-impl PartialEq<u128> for Bound<'_, PyLong> {
-    #[inline]
-    fn eq(&self, other: &u128) -> bool {
-        self.value() == *other as c_long
-    }
-}
-
-/// Implement u128 == <Bound<'_, PyLong>> comparisons
-impl PartialEq<Bound<'_, PyLong>> for u128 {
-    #[inline]
-    fn eq(&self, other: &Bound<'_, PyLong>) -> bool {
-        *self as c_long == other.value()
-    }
-}
-
-/// Implement <Bound<'_, PyLong>> == isize comparisons
-impl PartialEq<isize> for Bound<'_, PyLong> {
-    #[inline]
-    fn eq(&self, other: &isize) -> bool {
-        self.value() == *other as c_long
-    }
-}
-
-/// Implement isize == <Bound<'_, PyLong>> comparisons
-impl PartialEq<Bound<'_, PyLong>> for isize {
-    #[inline]
-    fn eq(&self, other: &Bound<'_, PyLong>) -> bool {
-        *self as c_long == other.value()
-    }
-}
-
-/// Implement <Bound<'_, PyLong>> == usize comparisons
-impl PartialEq<usize> for Bound<'_, PyLong> {
-    #[inline]
-    fn eq(&self, other: &usize) -> bool {
-        self.value() == *other as c_long
-    }
-}
-
-/// Implement usize == <Bound<'_, PyLong>> comparisons
-impl PartialEq<Bound<'_, PyLong>> for usize {
-    #[inline]
-    fn eq(&self, other: &Bound<'_, PyLong>) -> bool {
-        *self as c_long == other.value()
-    }
-}
+int_compare!(i8);
+int_compare!(u8);
+int_compare!(i16);
+int_compare!(u16);
+int_compare!(i32);
+int_compare!(u32);
+int_compare!(i64);
+int_compare!(u64);
+int_compare!(i128);
+int_compare!(u128);
+int_compare!(isize);
+int_compare!(usize);
 
 #[cfg(test)]
 mod tests {
-    use super::PyLong;
-    use super::PyLongMethods;
-    use crate::Python;
-
-    #[test]
-    fn test_c_long_value() {
-        Python::with_gil(|py| {
-            let v = 123;
-            let obj = PyLong::new_bound_from_c_long(py, 123);
-            assert_eq!(v, obj.value());
-        });
-    }
+    use crate::{IntoPy, Python};
 
     #[test]
     fn test_partial_eq() {
@@ -270,7 +70,7 @@ mod tests {
             let v_u128 = 123u128;
             let v_isize = 123isize;
             let v_usize = 123usize;
-            let obj = PyLong::new_bound_from_c_long(py, 123);
+            let obj = 123_i64.into_py(py).downcast_bound(py).unwrap().clone();
             assert_eq!(v_i8, obj);
             assert_eq!(obj, v_i8);
 
