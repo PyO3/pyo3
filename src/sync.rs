146c39705d9a37d5a42c5bc0525ce81fb3599d7f
David Hewitt <mail@davidhewitt.dev>
2024-08-13 00:34:01 +0100

    ci: fix nightly warning about unreachable pattern (#4437)
    * ci: fix nightly warning about unreachable pattern
    
    * add comment

--- src/sync.rs
+++ src/sync.rs
@@ -126,10 +126,9 @@ impl<T> GILOnceCell<T> {
             return value;
         }
 
-        match self.init(py, || Ok::<T, std::convert::Infallible>(f())) {
-            Ok(value) => value,
-            Err(void) => match void {},
-        }
+        // .unwrap() will never panic because the result is always Ok
+        self.init(py, || Ok::<T, std::convert::Infallible>(f()))
+            .unwrap()
     }
 
     /// Like `get_or_init`, but accepts a fallible initialization function. If it fails, the cell
