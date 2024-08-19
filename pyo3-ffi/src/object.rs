6db725b05a5de4aabde05829564624ff8d0bf86c
David Hewitt <mail@davidhewitt.dev>
2024-08-19 22:55:20 +0100

    fix `Py_Is` on PyPy 3.10

--- pyo3-ffi/src/object.rs
+++ pyo3-ffi/src/object.rs
@@ -134,8 +134,17 @@ pub struct PyVarObject {
 // skipped private _PyVarObject_CAST
 
 #[inline]
+#[cfg(not(all(PyPy, Py_3_10)))]
+#[cfg_attr(docsrs, doc(cfg(all())))]
 pub unsafe fn Py_Is(x: *mut PyObject, y: *mut PyObject) -> c_int {
     (x == y).into()
+}
+
+#[cfg(all(PyPy, Py_3_10))]
+#[cfg_attr(docsrs, doc(cfg(all())))]
+extern "C" {
+    #[cfg_attr(PyPy, link_name = "PyPy_Is")]
+    pub fn Py_Is(x: *mut PyObject, y: *mut PyObject) -> c_int;
 }
 
 // skipped private _Py_GetThreadLocal_Addr
