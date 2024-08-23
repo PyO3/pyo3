ec0853dbe6b19a2f90d013149c77b872f40688ec
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2024-08-23 13:45:04 -0600

    Fix tests for cargo stress on free-threaded build (#4469)
    * make it possible to run test_bad_datetime_module_panic under 'cargo stress'
    
    * weaken assertions in tests of the pending decrefs pool on free-threaded builds
    
    * use the tempfile crate to create a temporary directory

--- tests/test_datetime_import.rs
+++ tests/test_datetime_import.rs
@@ -1,26 +1,28 @@
 #![cfg(not(Py_LIMITED_API))]
 
 use pyo3::{prelude::*, types::PyDate};
+use tempfile::Builder;
 
 #[test]
 #[should_panic(expected = "module 'datetime' has no attribute 'datetime_CAPI'")]
 fn test_bad_datetime_module_panic() {
     // Create an empty temporary directory
     // with an empty "datetime" module which we'll put on the sys.path
-    let tmpdir = std::env::temp_dir();
-    let tmpdir = tmpdir.join("pyo3_test_date_check");
-    let _ = std::fs::remove_dir_all(&tmpdir);
-    std::fs::create_dir(&tmpdir).unwrap();
-    std::fs::File::create(tmpdir.join("datetime.py")).unwrap();
+    let tmpdir = Builder::new()
+        .prefix("pyo3_test_data_check")
+        .tempdir()
+        .unwrap();
+    std::fs::File::create(tmpdir.path().join("datetime.py")).unwrap();
 
     Python::with_gil(|py: Python<'_>| {
         let sys = py.import("sys").unwrap();
         sys.getattr("path")
             .unwrap()
-            .call_method1("insert", (0, tmpdir))
+            .call_method1("insert", (0, tmpdir.path()))
             .unwrap();
 
         // This should panic because the "datetime" module is empty
         PyDate::new(py, 2018, 1, 1).unwrap();
     });
+    tmpdir.close().unwrap();
 }
