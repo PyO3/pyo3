09d6dbe78a1a319dbfcee9a9e96df4ebef3b643a
David Hewitt <mail@davidhewitt.dev>
2024-09-03 14:22:45 +0200

    fixup CI warnings

--- src/tests/hygiene/pyfunction.rs
+++ src/tests/hygiene/pyfunction.rs
@@ -7,7 +7,7 @@ fn do_something(x: i32) -> crate::PyResult<i32> {
 #[test]
 fn invoke_wrap_pyfunction() {
     crate::Python::with_gil(|py| {
-        let func = crate::wrap_pyfunction!(do_something, py).unwrap();
+        let func = crate::wrap_pyfunction_bound!(do_something, py).unwrap();
         crate::py_run!(py, func, r#"func(5)"#);
     });
 }
