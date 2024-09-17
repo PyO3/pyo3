307e8cf5c7e678806aa8195f18c334ecc7734a99
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2024-09-17 16:26:33 -0600

    add Sync impl and make lock accept an immutable reference

--- src/types/mutex.rs
+++ src/types/mutex.rs
@@ -11,14 +11,14 @@ pub struct PyMutex<T> {
 /// RAII guard to handle releasing a PyMutex lock.
 #[derive(Debug)]
 pub struct PyMutexGuard<'a, T> {
-    mutex: &'a mut PyMutex<T>,
+    mutex: &'a PyMutex<T>,
 }
 
 impl<T> PyMutex<T> {
     /// Acquire the mutex, blocking the current thread until it is able to do so.
-    pub fn lock(&mut self) -> PyMutexGuard<'_, T> {
-        unsafe { crate::ffi::PyMutex_Lock(self._mutex.get_mut()) };
-        PyMutexGuard { mutex: &mut *self }
+    pub fn lock(&self) -> PyMutexGuard<'_, T> {
+        unsafe { crate::ffi::PyMutex_Lock(UnsafeCell::raw_get(&self._mutex)) };
+        PyMutexGuard { mutex: self }
     }
 
     /// Create a new mutex in an unlocked state ready for use.
@@ -30,9 +30,11 @@ impl<T> PyMutex<T> {
     }
 }
 
+unsafe impl<T: Send> Sync for PyMutex<T> {}
+
 impl<'a, T> Drop for PyMutexGuard<'a, T> {
     fn drop(&mut self) {
-        unsafe { crate::ffi::PyMutex_Unlock(self.mutex._mutex.get_mut()) };
+        unsafe { crate::ffi::PyMutex_Unlock(UnsafeCell::raw_get(&self.mutex._mutex)) };
     }
 }
 
@@ -50,27 +52,26 @@ impl<'a, T> DerefMut for PyMutexGuard<'a, T> {
     fn deref_mut(&mut self) -> &mut T {
         // safety: cannot be null pointer because PyMutexGuard::new always
         // creates a valid PyMutex pointer
-        self.mutex.data.get_mut()
+        unsafe { &mut *self.mutex.data.get() }
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::sync::GILOnceCell;
     use crate::types::{PyAnyMethods, PyDict, PyDictMethods, PyNone};
     use crate::Py;
     use crate::Python;
 
     #[test]
     fn test_pymutex() {
-        let mut mutex = Python::with_gil(|py| -> PyMutex<Py<PyDict>> {
+        let mutex = Python::with_gil(|py| -> PyMutex<Py<PyDict>> {
             let d = PyDict::new(py);
             PyMutex::new(d.unbind())
         });
 
         Python::with_gil(|py| {
-            let mut mutex = py.allow_threads(|| -> PyMutex<Py<PyDict>> {
+            let mutex = py.allow_threads(|| -> PyMutex<Py<PyDict>> {
                 std::thread::spawn(|| {
                     let dict_guard = mutex.lock();
                     Python::with_gil(|py| {
