6834f3f139cb5f89fb1d4ae68392ffaf2aa5d5eb
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2024-10-01 16:36:08 -0600

    WIP: declare free-threaded support in pymodule macro

--- src/types/module.rs
+++ src/types/module.rs
@@ -488,6 +488,7 @@ impl<'py> PyModuleMethods<'py> for Bound<'py, PyModule> {
         T: IntoPyCallbackOutput<PyObject>,
     {
         fn inner(module: &Bound<'_, PyModule>, object: Bound<'_, PyAny>) -> PyResult<()> {
+            if object.is_instance_of::<PyModule>() {}
             let name = object.getattr(__name__(module.py()))?;
             module.add(name.downcast_into::<PyString>()?, object)
         }
