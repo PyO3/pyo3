d36b088ddb375968f2d0ce0b9e7d59e6a5846df8
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2024-10-21 15:55:51 -0600

    fix ruff

--- pytests/tests/test_free_threaded.py
+++ pytests/tests/test_free_threaded.py
@@ -4,9 +4,11 @@ import sysconfig
 
 from pyo3_pytests import free_threaded_mod  # NOQA
 
-GIL_DISABLED_BUILD = bool(sysconfig.get_config_var('Py_GIL_DISABLED'))
+GIL_DISABLED_BUILD = bool(sysconfig.get_config_var("Py_GIL_DISABLED"))
 
-@pytest.mark.skipif(not GIL_DISABLED_BUILD,
-                    reason="test is not meaningful on GIL-enabled build")
+
+@pytest.mark.skipif(
+    not GIL_DISABLED_BUILD, reason="test is not meaningful on GIL-enabled build"
+)
 def test_gil_disabled():
     assert not sys._is_gil_enabled()
