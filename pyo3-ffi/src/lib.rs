19125dcf22856fa48345238b6ca83b3e8876ca10
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2024-10-28 12:37:12 -0600

    don't depend on a file that doesn't exist

--- pyo3-ffi/src/lib.rs
+++ pyo3-ffi/src/lib.rs
@@ -123,7 +123,9 @@
 //!
 //! **`build.rs`**
 //! ```rust,ignore
-#![doc = include_str!("../../examples/string-sum/build.rs")]
+//! fn main() {
+//!     pyo3_build_config::use_pyo3_cfgs()
+//! }
 //! ```
 //!
 //! **`src/lib.rs`**
