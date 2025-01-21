b734505d88451f269d6c35fcd28ae6e9276bd266
JeanArhancet <jean.arhancetebehere@gmail.com>
2025-01-21 20:17:38 +0100

    refactor: apply code review feedback

--- src/conversions/uuid.rs
+++ src/conversions/uuid.rs
@@ -26,14 +26,22 @@
 //! use pyo3::exceptions::PyValueError;
 //! use uuid::Uuid;
 //!
+//! /// Parse a UUID from a string.
 //! #[pyfunction]
-//! fn parse_uuid(s: &str) -> PyResult<Uuid> {
+//! fn get_uuid_from_str(s: &str) -> PyResult<Uuid> {
 //!     Uuid::parse_str(s).map_err(|e| PyValueError::new_err(e.to_string()))
+//! }
+//!
+//! /// Passing a Python uuid.UUID directly to Rust.
+//! #[pyfunction]
+//! fn get_uuid(u: Uuid) -> Uuid {
+//!     u
 //! }
 //!
 //! #[pymodule]
 //! fn my_module(m: &Bound<'_, PyModule>) -> PyResult<()> {
-//!     m.add_function(wrap_pyfunction!(parse_uuid, m)?)?;
+//!     m.add_function(wrap_pyfunction!(get_uuid_from_str, m)?)?;
+//!     m.add_function(wrap_pyfunction!(get_uuid, m)?)?;
 //!     Ok(())
 //! }
 //! ```
@@ -42,29 +50,31 @@
 //!
 //!
 //! ```python
-//! from my_module import parse_uuid
+//! from my_module import get_uuid_from_str, get_uuid
 //! import uuid
 //!
 //! py_uuid = uuid.uuid4()
-//! rust_uuid = parse_uuid(str(py_uuid))
 //!
+//! # Convert string to Rust Uuid
+//! rust_uuid = get_uuid_from_str(str(py_uuid))
 //! assert py_uuid == rust_uuid
+//!
+//! # Pass Python UUID directly to Rust
+//! returned_uuid = get_uuid(py_uuid)
+//! assert py_uuid == returned_uuid
 //! ```
 use uuid::Uuid;
 
 use crate::conversion::IntoPyObject;
-use crate::exceptions::{PyTypeError, PyValueError};
+use crate::exceptions::PyTypeError;
 use crate::instance::Bound;
 use crate::sync::GILOnceCell;
 use crate::types::any::PyAnyMethods;
-use crate::types::{PyBytes, PyBytesMethods, PyInt, PyStringMethods, PyType};
-use crate::{FromPyObject, Py, PyAny, PyErr, PyObject, PyResult, Python};
-#[allow(deprecated)]
-use crate::{IntoPy, ToPyObject};
-
-static UUID_CLS: GILOnceCell<Py<PyType>> = GILOnceCell::new();
+use crate::types::PyType;
+use crate::{intern, FromPyObject, Py, PyAny, PyErr, PyResult, Python};
 
 fn get_uuid_cls(py: Python<'_>) -> PyResult<&Bound<'_, PyType>> {
+    static UUID_CLS: GILOnceCell<Py<PyType>> = GILOnceCell::new();
     UUID_CLS.import(py, "uuid", "UUID")
 }
 
@@ -73,52 +83,15 @@ impl FromPyObject<'_> for Uuid {
         let py = obj.py();
 
         if let Ok(uuid_cls) = get_uuid_cls(py) {
-            if obj.is_exact_instance(uuid_cls) {
-                let uuid_int: u128 = obj.getattr("int")?.extract()?;
-                return Ok(Uuid::from_u128(uuid_int.to_le()));
-            }
-        }
-
-        if obj.is_instance_of::<PyBytes>() {
-            let bytes = if let Ok(py_bytes) = obj.downcast::<PyBytes>() {
-                py_bytes.as_bytes()
+            if obj.is_instance(uuid_cls)? {
+                let uuid_int: u128 = obj.getattr(intern!(py, "int"))?.extract()?;
+                Ok(Uuid::from_u128(uuid_int.to_le()))
             } else {
-                return Err(PyTypeError::new_err("Expected bytes for UUID extraction."));
-            };
-
-            return Uuid::from_slice(bytes)
-                .map_err(|_| PyValueError::new_err("The given bytes value is not a valid UUID."));
+                Err(PyTypeError::new_err("Expected a `uuid.UUID` instance."))
+            }
+        } else {
+            Err(PyTypeError::new_err("Failed to import `uuid.UUID`."))
         }
-
-        if obj.is_instance_of::<PyInt>() {
-            let uuid_int: u128 = obj.extract().map_err(|_| {
-                PyTypeError::new_err(
-                    "Expected integer for UUID extraction but got an incompatible type.",
-                )
-            })?;
-            return Ok(Uuid::from_u128(uuid_int));
-        }
-
-        let py_str = &obj.str()?;
-        let rs_str = &py_str.to_cow()?;
-        Uuid::parse_str(rs_str)
-            .map_err(|e| PyValueError::new_err(format!("Invalid UUID string: {e}")))
-    }
-}
-
-#[allow(deprecated)]
-impl ToPyObject for Uuid {
-    #[inline]
-    fn to_object(&self, py: Python<'_>) -> PyObject {
-        self.into_pyobject(py).unwrap().unbind()
-    }
-}
-
-#[allow(deprecated)]
-impl IntoPy<PyObject> for Uuid {
-    #[inline]
-    fn into_py(self, py: Python<'_>) -> PyObject {
-        self.into_pyobject(py).unwrap().unbind()
     }
 }
 
@@ -130,9 +103,7 @@ impl<'py> IntoPyObject<'py> for Uuid {
     fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
         let uuid_cls = get_uuid_cls(py)?;
 
-        Ok(uuid_cls
-            .call1((py.None(), py.None(), py.None(), py.None(), self.as_u128()))?
-            .into_pyobject(py)?)
+        uuid_cls.call1((py.None(), py.None(), py.None(), py.None(), self.as_u128()))
     }
 }
 
@@ -150,7 +121,7 @@ impl<'py> IntoPyObject<'py> for &Uuid {
 mod tests {
     use super::*;
     use crate::types::dict::PyDictMethods;
-    use crate::types::{PyDict, PyString};
+    use crate::types::PyDict;
     use std::ffi::CString;
     use uuid::Uuid;
 
@@ -213,90 +184,4 @@ mod tests {
         Uuid::parse_str("a6cc5730-2261-11ee-9c43-2eb5a363657c").unwrap(),
         "a6cc5730-2261-11ee-9c43-2eb5a363657c"
     );
-
-    #[test]
-    fn test_uuid_str() {
-        Python::with_gil(|py| {
-            let s = PyString::new(py, "a6cc5730-2261-11ee-9c43-2eb5a363657c");
-            let uuid: Uuid = s.extract().unwrap();
-            assert_eq!(
-                uuid,
-                Uuid::parse_str("a6cc5730-2261-11ee-9c43-2eb5a363657c").unwrap()
-            );
-        });
-    }
-
-    #[test]
-    fn test_uuid_bytes() {
-        Python::with_gil(|py| {
-            let s = PyBytes::new(
-                py,
-                &[
-                    0xa1, 0xa2, 0xa3, 0xa4, 0xb1, 0xb2, 0xc1, 0xc2, 0xd1, 0xd2, 0xd3, 0xd4, 0xd5,
-                    0xd6, 0xd7, 0xd8,
-                ],
-            );
-            let uuid: Uuid = s.extract().unwrap();
-            assert_eq!(
-                uuid,
-                Uuid::parse_str("a1a2a3a4-b1b2-c1c2-d1d2-d3d4d5d6d7d8").unwrap()
-            );
-        });
-    }
-
-    #[test]
-    fn test_invalid_uuid_bytes() {
-        Python::with_gil(|py| {
-            let s = PyBytes::new(
-                py,
-                &[
-                    0xa1, 0xa2, 0xa3, 0xa4, 0xb1, 0xb2, 0xc1, 0xc2, 0xd1, 0xd2, 0xd3, 0xd4, 0xd5,
-                    0xd6, 0xd7,
-                ],
-            );
-            let uuid: Result<Uuid, PyErr> = s.extract();
-            assert!(uuid.is_err())
-        });
-    }
-
-    #[test]
-    fn test_uuid_int() {
-        Python::with_gil(|py| {
-            let v = 0xa1a2a3a4b1b2c1c2d1d2d3d4d5d6d7d8u128;
-            let obj: Bound<'_, PyInt> = v.into_pyobject(py).unwrap();
-            let uuid: Uuid = obj.extract().unwrap();
-            assert_eq!(
-                uuid,
-                Uuid::parse_str("a1a2a3a4-b1b2-c1c2-d1d2-d3d4d5d6d7d8").unwrap()
-            );
-        });
-    }
-
-    #[test]
-    fn test_invalid_uuid_int() {
-        Python::with_gil(|py| {
-            let v = -42;
-            let obj: Bound<'_, PyInt> = v.into_pyobject(py).unwrap();
-            let uuid: Result<Uuid, PyErr> = obj.extract();
-            assert!(uuid.is_err())
-        });
-    }
-
-    #[test]
-    fn test_uuid_incorrect_length() {
-        Python::with_gil(|py| {
-            let s = PyString::new(py, "123e4567-e89b-12d3-a456-42661417400");
-            let uuid: Result<Uuid, PyErr> = s.extract();
-            assert!(uuid.is_err())
-        });
-    }
-
-    #[test]
-    fn test_invalid_uuid_string() {
-        Python::with_gil(|py| {
-            let s = PyString::new(py, "invalid-uuid-str");
-            let uuid: Result<Uuid, PyErr> = s.extract();
-            assert!(uuid.is_err())
-        });
-    }
 }
