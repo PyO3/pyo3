5d81a6ee4b2bc123a49a8cf4871d72c99f86f7ff
Ivan Carvalho <8753214+IvanIsCoding@users.noreply.github.com>
2025-02-19 11:25:38 -0500

    Use PyPy_GenericAlias

--- pyo3-ffi/src/genericaliasobject.rs
+++ pyo3-ffi/src/genericaliasobject.rs
@@ -4,6 +4,7 @@ use crate::object::{PyObject, PyTypeObject};
 #[cfg_attr(windows, link(name = "pythonXY"))]
 extern "C" {
     #[cfg(Py_3_9)]
+    #[cfg_attr(PyPy, link_name = "PyPy_GenericAlias")]
     pub fn Py_GenericAlias(origin: *mut PyObject, args: *mut PyObject) -> *mut PyObject;
 
     #[cfg(Py_3_9)]
