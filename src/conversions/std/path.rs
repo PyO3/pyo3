fc26584e13cfeb16b53c0b05b25a65a6be08a61e
Bas Schoenmaeckers <b.schoenmaeckers@zanders.eu>
2025-02-19 16:25:32 +0100

    Convert `PathBuf` into python `pathlib.Path`

--- src/conversions/std/path.rs
+++ src/conversions/std/path.rs
@@ -1,13 +1,12 @@
 use crate::conversion::IntoPyObject;
 use crate::ffi_ptr_ext::FfiPtrExt;
 use crate::instance::Bound;
+use crate::sync::GILOnceCell;
 use crate::types::any::PyAnyMethods;
-use crate::types::PyString;
-use crate::{ffi, FromPyObject, PyAny, PyObject, PyResult, Python};
+use crate::{ffi, FromPyObject, PyAny, PyErr, PyObject, PyResult, Python};
 #[allow(deprecated)]
 use crate::{IntoPy, ToPyObject};
 use std::borrow::Cow;
-use std::convert::Infallible;
 use std::ffi::OsString;
 use std::path::{Path, PathBuf};
 
@@ -38,20 +37,23 @@ impl IntoPy<PyObject> for &Path {
 }
 
 impl<'py> IntoPyObject<'py> for &Path {
-    type Target = PyString;
+    type Target = PyAny;
     type Output = Bound<'py, Self::Target>;
-    type Error = Infallible;
+    type Error = PyErr;
 
     #[inline]
     fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
-        self.as_os_str().into_pyobject(py)
+        static PY_PATH: GILOnceCell<PyObject> = GILOnceCell::new();
+        PY_PATH
+            .import(py, "pathlib", "Path")?
+            .call((self.as_os_str(),), None)
     }
 }
 
 impl<'py> IntoPyObject<'py> for &&Path {
-    type Target = PyString;
+    type Target = PyAny;
     type Output = Bound<'py, Self::Target>;
-    type Error = Infallible;
+    type Error = PyErr;
 
     #[inline]
     fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
@@ -76,24 +78,24 @@ impl IntoPy<PyObject> for Cow<'_, Path> {
 }
 
 impl<'py> IntoPyObject<'py> for Cow<'_, Path> {
-    type Target = PyString;
+    type Target = PyAny;
     type Output = Bound<'py, Self::Target>;
-    type Error = Infallible;
+    type Error = PyErr;
 
     #[inline]
     fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
-        self.as_os_str().into_pyobject(py)
+        (*self).into_pyobject(py)
     }
 }
 
 impl<'py> IntoPyObject<'py> for &Cow<'_, Path> {
-    type Target = PyString;
+    type Target = PyAny;
     type Output = Bound<'py, Self::Target>;
-    type Error = Infallible;
+    type Error = PyErr;
 
     #[inline]
     fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
-        self.as_os_str().into_pyobject(py)
+        (&**self).into_pyobject(py)
     }
 }
 
@@ -114,13 +116,13 @@ impl IntoPy<PyObject> for PathBuf {
 }
 
 impl<'py> IntoPyObject<'py> for PathBuf {
-    type Target = PyString;
+    type Target = PyAny;
     type Output = Bound<'py, Self::Target>;
-    type Error = Infallible;
+    type Error = PyErr;
 
     #[inline]
     fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
-        self.as_os_str().into_pyobject(py)
+        (&self).into_pyobject(py)
     }
 }
 
@@ -133,20 +135,21 @@ impl IntoPy<PyObject> for &PathBuf {
 }
 
 impl<'py> IntoPyObject<'py> for &PathBuf {
-    type Target = PyString;
+    type Target = PyAny;
     type Output = Bound<'py, Self::Target>;
-    type Error = Infallible;
+    type Error = PyErr;
 
     #[inline]
     fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
-        self.as_os_str().into_pyobject(py)
+        (&**self).into_pyobject(py)
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::ffi_ptr_ext::FfiPtrExt;
     use crate::types::{PyAnyMethods, PyString, PyStringMethods};
-    use crate::{BoundObject, IntoPyObject, Python};
+    use crate::{ffi, IntoPyObject, IntoPyObjectExt, Python};
     use std::borrow::Cow;
     use std::fmt::Debug;
     use std::path::{Path, PathBuf};
@@ -180,10 +183,16 @@ mod tests {
                 T: IntoPyObject<'py> + AsRef<Path> + Debug + Clone,
                 T::Error: Debug,
             {
-                let pyobject = obj.clone().into_pyobject(py).unwrap().into_any();
-                let pystring = pyobject.as_borrowed().downcast::<PyString>().unwrap();
+                let pyobject = obj.clone().into_bound_py_any(py).unwrap();
+                let pystring = unsafe {
+                    ffi::PyOS_FSPath(pyobject.as_ptr())
+                        .assume_owned_or_err(py)
+                        .unwrap()
+                        .downcast_into::<PyString>()
+                        .unwrap()
+                };
                 assert_eq!(pystring.to_string_lossy(), obj.as_ref().to_string_lossy());
-                let roundtripped_obj: PathBuf = pystring.extract().unwrap();
+                let roundtripped_obj: PathBuf = pyobject.extract().unwrap();
                 assert_eq!(obj.as_ref(), roundtripped_obj.as_path());
             }
             let path = Path::new("Hello\0\n🐍");
