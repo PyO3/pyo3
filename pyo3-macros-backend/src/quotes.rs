c23b0aff3ca6755a0c0c36e1ea3a1de1df116315
Lily Foote <code@lilyf.org>
2025-02-27 15:06:11 +0000

    Update quoting in ok_wrap to improve error messages

--- pyo3-macros-backend/src/quotes.rs
+++ pyo3-macros-backend/src/quotes.rs
@@ -15,13 +15,11 @@ pub(crate) fn ok_wrap(obj: TokenStream, ctx: &Ctx) -> TokenStream {
         output_span,
     } = ctx;
     let pyo3_path = pyo3_path.to_tokens_spanned(*output_span);
-    let converter = quote_spanned! { *output_span => {
+    let into = quote!(::core::convert::Into::<#pyo3_path::PyErr>::into);
+    quote_spanned! { *output_span => {
         let obj = #obj;
-        #pyo3_path::impl_::wrap::converter(&obj).wrap(obj)
-    }};
-    quote! {
-        #converter.map_err(::core::convert::Into::<#pyo3_path::PyErr>::into)
-    }
+        #pyo3_path::impl_::wrap::converter(&obj).wrap(obj).map_err(#into)
+    }}
 }
 
 pub(crate) fn map_result_into_ptr(result: TokenStream, ctx: &Ctx) -> TokenStream {
