f08cc603d2cce20e9fa1bdf76bd5cd93f2f20bec
Pontus Andersson <epontan@gmail.com>
2025-03-17 22:11:57 +0100

    Fix `Probe` class naming issue with `#[pymethods]` (#4988)
    Commit 603a55f204e3 ("fix `#[pyclass]` could not be named `Probe`
    (#4794)") fixed the issue where it was not possible to define a class
    named `Probe`. However, similar issue exists when trying to add
    `#[pymethods]` implementations to the `Probe` class. It generates
    similar confusing errors as the original issue (#4792), due to the
    internal `Probe` trait being in scope at the user code:
    
      error[E0782]: expected a type, found a trait
      help: you can add the `dyn` keyword if you want a trait object
      # | impl dyn Probe {
    
    Fix the issue by avoiding pollution of the imports from the macro
    expansion and instead use qualified path to the internal types.

--- tests/ui/pyclass_probe.rs
+++ tests/ui/pyclass_probe.rs
@@ -3,4 +3,18 @@ use pyo3::prelude::*;
 #[pyclass]
 pub struct Probe {}
 
+#[pymethods]
+impl Probe {
+    #[new]
+    fn new() -> Self {
+        Self {}
+    }
+}
+
+#[pymodule]
+fn probe(_py: Python<'_>, m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<Probe>()?;
+    Ok(())
+}
+
 fn main() {}
