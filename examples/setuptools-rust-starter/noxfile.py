2e50f3d2ff7c54416b4ecd861a91a17c1b6439ff
David Hewitt <mail@davidhewitt.dev>
2025-04-04 18:20:06 +0100

    fixup, again

--- examples/setuptools-rust-starter/noxfile.py
+++ examples/setuptools-rust-starter/noxfile.py
@@ -3,6 +3,6 @@ import nox
 
 @nox.session
 def python(session: nox.Session):
-    session.env["SETUPTOOL_RUST_CARGO_PROFILE"] = "dev"
+    session.env["SETUPTOOLS_RUST_CARGO_PROFILE"] = "dev"
     session.install(".[dev]")
     session.run("pytest")
