4327a5dd6246fd5aa2869fea8d4bbd09ffaf76ed
David Hewitt <mail@davidhewitt.dev>
2025-04-06 12:43:07 +0100

    clean up setuptools starter example to use `pyproject.toml` configuration (#5042)
    * clean up setuptools starter example to use `pyproject.toml` configuration
    
    * fixup
    
    * fixup, again

--- examples/setuptools-rust-starter/noxfile.py
+++ examples/setuptools-rust-starter/noxfile.py
@@ -3,8 +3,6 @@ import nox
 
 @nox.session
 def python(session: nox.Session):
-    session.install("-rrequirements-dev.txt")
-    session.run_always(
-        "pip", "install", "-e", ".", "--no-build-isolation", env={"BUILD_DEBUG": "1"}
-    )
+    session.env["SETUPTOOLS_RUST_CARGO_PROFILE"] = "dev"
+    session.install(".[dev]")
     session.run("pytest")
