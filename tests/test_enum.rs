bc26a06346b2ce2d9686a71de3ca4cc0fb452a4d
Michael Gaver-Holmes <706183+hgmich@users.noreply.github.com>
2025-04-07 17:42:01 -0400

    Unraw complex struct enum field python names (#5050)
    * Unraw complex struct enum field python names
    
    Stray case related to #2395 (probably unnoticed because the functionality is
    newer and a bit of an edge case).
    
    The `python_name` of struct-type enum variant fields (and the `__match_args__`
    of the variant) do not unraw the names of these fields, which effectively
    makes them unusable as the resulting identifiers contain '#' which the Python
    lexer interprets as comments. The only reason you'd probably want to do this
    anyway is to use reserved identifiers (`type` in my case).
    
    * Add newsfragment for fix
    
    * Fix clippy lints in impl_complex_enum_variant_match_args and callers
    
    No need to pass mutable vec ref anymore
    
    * don't overeagerly collect field_names iterator
    
    * add test case for complex enums containing raw identifiers
    
    * only test raw ident pattern matching on Python 3.10+

--- tests/test_enum.rs
+++ tests/test_enum.rs
@@ -369,3 +369,38 @@ fn custom_eq() {
         assert!(b.as_any().ne("A").unwrap());
     })
 }
+
+#[pyclass]
+#[derive(Clone, Copy)]
+pub enum ComplexEnumWithRaw {
+    Raw { r#type: i32 },
+}
+
+// Cover simple field lookups with raw identifiers
+#[test]
+fn complex_enum_with_raw() {
+    Python::with_gil(|py| {
+        let complex = ComplexEnumWithRaw::Raw { r#type: 314159 };
+
+        py_assert!(py, complex, "complex.type == 314159");
+    });
+}
+
+// Cover pattern matching with raw identifiers
+#[test]
+#[cfg(Py_3_10)]
+fn complex_enum_with_raw_pattern_match() {
+    Python::with_gil(|py| {
+        let complex = ComplexEnumWithRaw::Raw { r#type: 314159 };
+        let cls = py.get_type::<ComplexEnumWithRaw>();
+
+        // Cover destructuring by pattern matching
+        py_run!(py, cls complex, r#"
+        match complex:
+            case cls.Raw(type=ty):
+                assert ty == 314159
+            case _:
+                assert False, "no matching variant found"
+        "#);
+    });
+}
