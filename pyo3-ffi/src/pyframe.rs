95442873607fe285080e39b1d129180aa22d364f
Code-Apprentice <codeguru42@gmail.com>
2025-05-19 15:05:57 -0600

    Match `use` conditional compilation with usage

--- pyo3-ffi/src/pyframe.rs
+++ pyo3-ffi/src/pyframe.rs
@@ -3,7 +3,6 @@ use crate::object::PyObject;
 #[cfg(not(GraalPy))]
 #[cfg(any(Py_3_10, all(Py_3_9, not(Py_LIMITED_API))))]
 use crate::PyCodeObject;
-#[cfg(not(Py_LIMITED_API))]
 use crate::PyFrameObject;
 #[allow(unused_imports)]
 use std::os::raw::{c_char, c_int};
