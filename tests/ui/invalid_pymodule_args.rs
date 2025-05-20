7f20fc26e0376aa4b56b3dfaa3b47c80554f7790
Jake Marchewitz <jjmarchewitz@gmail.com>
2025-05-20 14:32:33 -0400

    add more tests

--- tests/ui/invalid_pymodule_args.rs
+++ tests/ui/invalid_pymodule_args.rs
@@ -5,4 +5,12 @@ fn module(m: &Bound<'_, PyModule>) -> PyResult<()> {
     Ok(())
 }
 
-fn main(){}
+#[pyo3::pymodule(gil_used = false, gil_used = true, name = "foo", name = "bar")]
+fn module_fn_multiple_errors(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    Ok(())
+}
+
+#[pyo3::pymodule(gil_used = false, gil_used = true, name = "foo", name = "bar")]
+mod pyo3_module_multiple_errors {}
+
+fn main() {}
