9f7b449739ea84e896e825ab12ffd829640ce78e
Cheukting <cheukting.ho@gmail.com>
2025-05-20 15:03:02 -0400

    make sure no experimental-async feature

--- pytests/tests/test_awaitable.py
+++ pytests/tests/test_awaitable.py
@@ -1,7 +1,7 @@
 import pytest
 import sys
 
-from pyo3_pytests.awaitable import IterAwaitable, FutureAwaitable
+from pyo3_pytests.awaitable import IterAwaitable, FutureAwaitable, AsyncRange
 
 
 @pytest.mark.skipif(
@@ -20,3 +20,38 @@ async def test_iter_awaitable():
 @pytest.mark.asyncio
 async def test_future_awaitable():
     assert await FutureAwaitable(5) == 5
+
+class PyAsyncRange:
+    def __init__(self, n):
+        self.n = n
+
+    def __aiter__(self):
+        self.i = 0
+        return self
+
+    async def __anext__(self):
+        if self.i < self.n:
+            i = self.i
+            self.i += 1
+            return i
+        raise StopAsyncIteration
+
+
+@pytest.mark.parametrize(
+    "ty",
+    (
+            AsyncRange,
+            PyAsyncRange,
+    ),
+    ids=(
+            "rust",
+            "python",
+    ),
+)
+@pytest.mark.asyncio
+async def test_async_iterator(ty: int):
+    x = 0
+    async for i in ty(5):
+        assert i == x
+        x += 1
+    assert x == 5
