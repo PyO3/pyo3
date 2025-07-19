64cedf46a6db6e8f079a3cc70b87241b03033b81
David Hewitt <mail@davidhewitt.dev>
2025-07-19 21:03:24 +0100

    simplify / update implementation

--- src/stdio.rs
+++ src/stdio.rs
@@ -1,12 +1,12 @@
 //! Enables direct write access to I/O streams in Python's `sys` module.
-
+//!
 //! In some cases printing to Rust's `std::io::stdout` or `std::io::stderr` will not appear
 //! in the Python interpreter, e.g. in Jupyter notebooks. This module provides a way to write
 //! directly to Python's I/O streams from Rust in such cases.
-
+//!
 //! ```rust
 //! let mut stdout = pyo3::stdio::stdout();
-//!   
+//!
 //! // This may not appear in Jupyter notebooks...
 //! println!("Hello, world!");
 //!
@@ -14,74 +14,80 @@
 //! writeln!(stdout, "Hello, world!").unwrap();
 //! ```
 
+use crate::ffi;
+use crate::ffi_ptr_ext::FfiPtrExt;
 use crate::intern;
 use crate::prelude::*;
 use crate::types::PyString;
-use std::io::{LineWriter, Write};
+use std::ffi::CStr;
+use std::io::Write;
 
-/// Implements `std::io::Write` for a `PyAny` object.    The underlying
-///  Python object must provide both `write` and `flush` methods.
-pub struct PyWriter(Py<PyAny>);
+/// Implements `std::io::Write` for a `PyAny` object. The underlying
+/// Python object must provide both `write` and `flush` methods.
+///
+/// Because Python IO streams use UTF-8 encoding, this will convert the input bytes to a UTF-8 string before writing
+/// using `String::from_utf8_lossy`.
+pub struct PyStreamWriter(Py<PyAny>);
 
-impl PyWriter {
-    /// Construct a new `PyWriter` from a `PyAny` object.
-    pub fn buffered(self) -> LineWriter<PyWriter> {
-        LineWriter::new(self)
-    }
-}
-
-/// A GIL-attached equivalent to PyWriter.
-pub struct PyWriterBound<'a, 'py>(&'a Bound<'py, PyAny>);
-
-fn get_stdio_pywriter(stream: &str) -> PyWriter {
-    Python::with_gil(|py| {
-        let module = PyModule::import_bound(py, "sys").unwrap();
-        let stream = module.getattr(stream).unwrap();
-        PyWriter(stream.into())
+fn get_stdio_stream(stream: &CStr) -> PyStreamWriter {
+    Python::attach(|py| {
+        // SAFETY: `PySys_GetObject` returns a borrowed reference to a Python object.
+        // FIXME: use `PySys_GetAttrString` on 3.14+ once it's released, which returns owned references.
+        let stream = unsafe { ffi::PySys_GetObject(stream.as_ptr()).assume_borrowed_or_err(py) };
+        PyStreamWriter(
+            stream
+                .expect("failed to get Python stream")
+                .to_owned()
+                .unbind(),
+        )
     })
 }
 
-/// Construct a new `PyWriter` for Python's `sys.stdout` stream.
-pub fn stdout() -> PyWriter {
-    get_stdio_pywriter("stdout")
+/// Construct a new [`PyStreamWriter`] for Python's `sys.stdout` stream.
+pub fn stdout() -> PyStreamWriter {
+    get_stdio_stream(ffi::c_str!("stdout"))
 }
 
-/// Construct a new `PyWriter` for Python's `sys.stderr` stream.
-pub fn stderr() -> PyWriter {
-    get_stdio_pywriter("stderr")
+/// Construct a new [`PyStreamWriter`] for Python's `sys.stderr` stream.
+pub fn stderr() -> PyStreamWriter {
+    get_stdio_stream(ffi::c_str!("stderr"))
 }
 
-/// Construct a new `PyWriter` for Python's `sys.__stdout__` stream.
-pub fn __stdout__() -> PyWriter {
-    get_stdio_pywriter("__stdout__")
+/// Construct a new [`PyStreamWriter`] for Python's `sys.__stdout__` stream.
+pub fn __stdout__() -> PyStreamWriter {
+    get_stdio_stream(ffi::c_str!("__stdout__"))
 }
 
-/// Construct a new `PyWriter` for Python's `sys.__stderr__` stream.
-pub fn __stderr__() -> PyWriter {
-    get_stdio_pywriter("__stderr__")
+/// Construct a new [`PyStreamWriter`] for Python's `sys.__stderr__` stream.
+pub fn __stderr__() -> PyStreamWriter {
+    get_stdio_stream(ffi::c_str!("__stderr__"))
 }
 
-impl Write for PyWriter {
+impl Write for PyStreamWriter {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        Python::with_gil(|py| PyWriterBound(self.0.bind(py)).write(buf))
+        Python::attach(|py| PyStreamWriterBound(self.0.bind(py)).write(buf))
+    }
+    fn write_all(&mut self, buf: &[u8]) -> std::io::Result<()> {
+        // override `write_all` to avoid needing to attach for each write in the default
+        // `write_all` loop
+        Python::attach(|py| PyStreamWriterBound(self.0.bind(py)).write_all(buf))
     }
     fn flush(&mut self) -> std::io::Result<()> {
-        Python::with_gil(|py| PyWriterBound(self.0.bind(py)).flush())
+        Python::attach(|py| PyStreamWriterBound(self.0.bind(py)).flush())
     }
 }
 
-impl Write for PyWriterBound<'_, '_> {
+/// Thread-attached version of `PyStreamWriter` used internally to implement `Write`.
+struct PyStreamWriterBound<'a, 'py>(&'a Bound<'py, PyAny>);
+
+impl Write for PyStreamWriterBound<'_, '_> {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
-        let str = PyString::new_bound(self.0.py(), &String::from_utf8_lossy(buf));
-        self.0
-            .call_method1(intern!(self.0.py(), "write"), (str,))
-            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        let str = PyString::new(self.0.py(), &String::from_utf8_lossy(buf));
+        self.0.call_method1(intern!(self.0.py(), "write"), (str,))?;
         Ok(buf.len())
     }
     fn flush(&mut self) -> std::io::Result<()> {
-        self.0
-            .call_method0(intern!(self.0.py(), "flush"))
-            .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+        self.0.call_method0(intern!(self.0.py(), "flush"))?;
         Ok(())
     }
 }
--- tests/test_stdio.rs
+++ tests/test_stdio.rs
@@ -1,6 +1,14 @@
-#![cfg(feature = "macros")]
+//! Tests for the `pyo3::stdio` module.
+//!
+//! These redirect Python's `sys.stdout` and `sys.stderr` to a `StringIO` object,
+//! so they run in a separate process to avoid interfering with other tests.
 
+use pyo3::ffi;
 use pyo3::prelude::*;
+use pyo3::stdio::*;
+use pyo3::types::IntoPyDict;
+use std::ffi::CString;
+use std::io::Write;
 
 #[macro_use]
 #[path = "../src/tests/common.rs"]
@@ -8,22 +16,18 @@ mod common;
 
 #[test]
 fn test_stdio() {
-    use pyo3::stdio::*;
-    use pyo3::types::IntoPyDict;
-    use std::io::Write;
-
     let stream_fcns = [stdout, stderr, __stdout__, __stderr__];
     let stream_names = ["stdout", "stderr", "__stdout__", "__stderr__"];
 
     for (stream_fcn, stream_name) in stream_fcns.iter().zip(stream_names.iter()) {
-        Python::with_gil(|py| {
-            py.run_bound("import sys, io", None, None).unwrap();
+        Python::attach(|py| {
+            py.run(ffi::c_str!("import sys, io"), None, None).unwrap();
 
             // redirect stdout or stderr output to a StringIO object
-            let target = py.eval_bound("io.StringIO()", None, None).unwrap();
-            let locals = [("target", &target)].into_py_dict_bound(py);
-            py.run_bound(
-                &format!("sys.{} = target", stream_name),
+            let target = py.eval(ffi::c_str!("io.StringIO()"), None, None).unwrap();
+            let locals = [("target", &target)].into_py_dict(py).unwrap();
+            py.run(
+                &CString::new(format!("sys.{} = target", stream_name)).unwrap(),
                 None,
                 Some(&locals),
             )
@@ -32,12 +36,12 @@ fn test_stdio() {
             let mut stream = stream_fcn();
             assert!(writeln!(stream, "writing to {}", stream_name).is_ok());
 
-            Python::run_bound(
-                py,
-                &format!(
+            py.run(
+                &CString::new(format!(
                     "assert target.getvalue() == 'writing to {}\\n'",
                     stream_name
-                ),
+                ))
+                .unwrap(),
                 Some(&locals),
                 None,
             )
