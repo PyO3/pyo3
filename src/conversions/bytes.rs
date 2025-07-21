945fc49bfd9a2aaaf90b8a9c2063318d5666df23
Bas Schoenmaeckers <b.schoenmaeckers@zanders.eu>
2025-07-21 21:31:30 +0200

    apply review comments

--- src/conversions/bytes.rs
+++ src/conversions/bytes.rs
@@ -52,31 +52,18 @@
 //! size = num_bytes(message)
 //! assert size == 13
 //! ```
-use bytes::{Bytes, BytesMut};
+use bytes::Bytes;
 
 use crate::conversion::IntoPyObject;
-use crate::exceptions::PyTypeError;
 use crate::instance::Bound;
+use crate::pybacked::PyBackedBytes;
 use crate::types::any::PyAnyMethods;
-use crate::types::{PyByteArray, PyByteArrayMethods, PyBytes, PyBytesMethods};
+use crate::types::PyBytes;
 use crate::{FromPyObject, PyAny, PyErr, PyResult, Python};
 
 impl FromPyObject<'_> for Bytes {
     fn extract_bound(ob: &Bound<'_, PyAny>) -> PyResult<Self> {
-        if let Ok(bytes) = ob.downcast::<PyBytes>() {
-            Ok(Bytes::from((*bytes).as_bytes().to_vec()))
-        } else if let Ok(bytearray) = ob.downcast::<PyByteArray>() {
-            Ok(Bytes::from((*bytearray).to_vec()))
-        } else {
-            Err(PyTypeError::new_err("expected bytes or bytearray"))
-        }
-    }
-}
-
-impl FromPyObject<'_> for BytesMut {
-    fn extract_bound(ob: &Bound<'_, PyAny>) -> PyResult<Self> {
-        let bytes = ob.extract::<Bytes>()?;
-        Ok(BytesMut::from(bytes))
+        Ok(Bytes::from_owner(ob.extract::<PyBackedBytes>()?))
     }
 }
 
@@ -90,30 +77,14 @@ impl<'py> IntoPyObject<'py> for Bytes {
     }
 }
 
-impl<'py> IntoPyObject<'py> for BytesMut {
-    type Target = PyBytes;
-    type Output = Bound<'py, Self::Target>;
-    type Error = PyErr;
-
-    fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
-        Ok(PyBytes::new(py, &self))
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use bytes::{Bytes, BytesMut};
-
-    use crate::{
-        conversion::IntoPyObject,
-        ffi,
-        types::{any::PyAnyMethods, PyBytes},
-        Python,
-    };
+    use super::*;
+    use crate::{ffi, Python};
 
     #[test]
     fn test_bytes() {
-        Python::with_gil(|py| {
+        Python::attach(|py| {
             let py_bytes = py.eval(ffi::c_str!("b'foobar'"), None, None).unwrap();
             let bytes: Bytes = py_bytes.extract().unwrap();
             assert_eq!(bytes, Bytes::from(b"foobar".to_vec()));
@@ -125,37 +96,12 @@ mod tests {
 
     #[test]
     fn test_bytearray() {
-        Python::with_gil(|py| {
+        Python::attach(|py| {
             let py_bytearray = py
                 .eval(ffi::c_str!("bytearray(b'foobar')"), None, None)
                 .unwrap();
             let bytes: Bytes = py_bytearray.extract().unwrap();
             assert_eq!(bytes, Bytes::from(b"foobar".to_vec()));
-        });
-    }
-
-    #[test]
-    fn test_bytes_mut() {
-        Python::with_gil(|py| {
-            let py_bytearray = py
-                .eval(ffi::c_str!("bytearray(b'foobar')"), None, None)
-                .unwrap();
-            let bytes: BytesMut = py_bytearray.extract().unwrap();
-            assert_eq!(bytes, BytesMut::from(&b"foobar"[..]));
-
-            let bytesmut = BytesMut::from(&b"foobar"[..]).into_pyobject(py).unwrap();
-            assert!(bytesmut.is_instance_of::<PyBytes>());
-        });
-    }
-
-    #[test]
-    fn test_bytearray_mut() {
-        Python::with_gil(|py| {
-            let py_bytearray = py
-                .eval(ffi::c_str!("bytearray(b'foobar')"), None, None)
-                .unwrap();
-            let bytes: BytesMut = py_bytearray.extract().unwrap();
-            assert_eq!(bytes, BytesMut::from(&b"foobar"[..]));
         });
     }
 }
