0c2eb387fb105ef6c90f54ac452479eec2475aef
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2025-08-04 15:42:54 -0600

    add poisoning to PyMutex

--- src/types/mutex.rs
+++ src/types/mutex.rs
@@ -1,5 +1,88 @@
 use std::cell::UnsafeCell;
 use std::ops::{Deref, DerefMut};
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    LockResult, PoisonError,
+};
+use std::thread;
+
+// See std::sync::poison in the rust standard library.
+// This is more-or-less copied from there since it is not public.
+// this type detects a panic and poisons the wrapping mutex
+pub(crate) struct Flag {
+    #[cfg(panic = "unwind")]
+    failed: AtomicBool,
+}
+
+impl Flag {
+    #[inline]
+    const fn new() -> Flag {
+        Flag {
+            #[cfg(panic = "unwind")]
+            failed: AtomicBool::new(false),
+        }
+    }
+
+    /// Checks the flag for an unguarded borrow, where we only care about existing poison.
+    #[inline]
+    fn borrow(&self) -> LockResult<()> {
+        if self.get() {
+            Err(PoisonError::new(()))
+        } else {
+            Ok(())
+        }
+    }
+
+    /// Checks the flag for a guarded borrow, where we may also set poison when `done`.
+    #[inline]
+    fn guard(&self) -> LockResult<Guard> {
+        let ret = Guard {
+            #[cfg(panic = "unwind")]
+            panicking: thread::panicking(),
+        };
+        if self.get() {
+            Err(PoisonError::new(ret))
+        } else {
+            Ok(ret)
+        }
+    }
+
+    #[inline]
+    #[cfg(panic = "unwind")]
+    fn done(&self, guard: &Guard) {
+        if !guard.panicking && thread::panicking() {
+            self.failed.store(true, Ordering::Relaxed);
+        }
+    }
+
+    #[inline]
+    #[cfg(not(panic = "unwind"))]
+    pub fn done(&self, _guard: &Guard) {}
+
+    #[inline]
+    #[cfg(panic = "unwind")]
+    fn get(&self) -> bool {
+        self.failed.load(Ordering::Relaxed)
+    }
+
+    #[inline(always)]
+    #[cfg(not(panic = "unwind"))]
+    pub fn get(&self) -> bool {
+        false
+    }
+
+    #[inline]
+    fn clear(&self) {
+        #[cfg(panic = "unwind")]
+        self.failed.store(false, Ordering::Relaxed)
+    }
+}
+
+#[derive(Clone)]
+pub(crate) struct Guard {
+    #[cfg(panic = "unwind")]
+    panicking: bool,
+}
 
 /// Wrapper for [`PyMutex`](https://docs.python.org/3/c-api/init.html#c.PyMutex), exposing an RAII guard interface.
 ///
@@ -12,23 +95,70 @@ use std::ops::{Deref, DerefMut};
 /// thread is blocked on acquiring the lock. On the free-threaded build, threads
 /// blocked on acquiring a PyMutex will not prevent the garbage collector from
 /// running.
+///
+/// ## Poisoning
+///
+/// Like `std::sync::Mutex`, `PyMutex` implements poisoning. A mutex
+/// is considered poisoned whenever a thread panics while holding the mtex. Once
+/// a mutex is poisoned, all other threads are unable to access the data by
+/// default as it is likely to be tainted (some invariant is not being held).
+///
+/// This means that the `lock` method returns a `Result` which indicated whether
+/// the mutex has been poisoned or not. Must usage will simple `unwrap()` these
+/// results, propagating panics among threads to ensure a possible invalid
+/// invariant is not being observed.
+///
+/// A poisoned mutex, however, does not prevent all access to the underlying
+/// data. The `PoisonError` type has an `into_inner` method which will return
+/// the guard that would have otherwise been returned on a successful lock. This
+/// allows access to the data, despite the lock being poisoned.
 pub struct PyMutex<T: ?Sized> {
     mutex: UnsafeCell<crate::ffi::PyMutex>,
+    poison: Flag,
     data: UnsafeCell<T>,
 }
 
 /// RAII guard to handle releasing a PyMutex lock.
 ///
 /// The lock is released when `PyMutexGuard` is dropped.
-pub struct PyMutexGuard<'a, T> {
+pub struct PyMutexGuard<'a, T: ?Sized> {
     inner: &'a PyMutex<T>,
+    poison: Guard,
 }
+
+/// `T` must be `Sync` for a [`PyMutexGuard<T>`] to be `Sync`
+/// because it is possible to get a `&T` from `&MutexGuard` (via `Deref`).
+unsafe impl<T: ?Sized + Sync> Sync for PyMutexGuard<'_, T> {}
+
+/// `T` must be `Send` for a [`Mutex`] to be `Send` because it is possible to acquire
+/// the owned `T` from the `Mutex` via [`into_inner`].
+///
+/// [`into_inner`]: Mutex::into_inner
+unsafe impl<T: ?Sized + Send> Send for PyMutex<T> {}
+
+/// `T` must be `Send` for [`PyMutex`] to be `Sync`.
+/// This ensures that the protected data can be accessed safely from multiple threads
+/// without causing data races or other unsafe behavior.
+///
+/// [`PyMutex<T>`] provides mutable access to `T` to one thread at a time. However, it's essential
+/// for `T` to be `Send` because it's not safe for non-`Send` structures to be accessed in
+/// this manner. For instance, consider [`Rc`], a non-atomic reference counted smart pointer,
+/// which is not `Send`. With `Rc`, we can have multiple copies pointing to the same heap
+/// allocation with a non-atomic reference count. If we were to use `Mutex<Rc<_>>`, it would
+/// only protect one instance of `Rc` from shared access, leaving other copies vulnerable
+/// to potential data races.
+///
+/// Also note that it is not necessary for `T` to be `Sync` as `&T` is only made available
+/// to one thread at a time if `T` is not `Sync`.
+///
+/// [`Rc`]: crate::rc::Rc
+unsafe impl<T: ?Sized + Send> Sync for PyMutex<T> {}
 
 impl<T> PyMutex<T> {
     /// Acquire the mutex, blocking the current thread until it is able to do so.
-    pub fn lock(&self) -> PyMutexGuard<'_, T> {
+    pub fn lock(&self) -> LockResult<PyMutexGuard<'_, T>> {
         unsafe { crate::ffi::PyMutex_Lock(UnsafeCell::raw_get(&self.mutex)) };
-        PyMutexGuard { inner: self }
+        PyMutexGuard::new(self)
     }
 
     /// Create a new mutex in an unlocked state ready for use.
@@ -36,16 +166,63 @@ impl<T> PyMutex<T> {
         Self {
             mutex: UnsafeCell::new(crate::ffi::PyMutex::new()),
             data: UnsafeCell::new(value),
+            poison: Flag::new(),
         }
+    }
+
+    /// Consumes this mutex, returning the underlying data.
+    ///
+    /// # Errors
+    ///
+    /// If another user of this mutex panicked while holding the mutex, then
+    /// this call will return an error containing the underlying data
+    /// instead.
+    pub fn into_inner(self) -> LockResult<T>
+    where
+        T: Sized,
+    {
+        let data = self.data.into_inner();
+        map_result(self.poison.borrow(), |()| data)
+    }
+
+    /// Clear the poisoned state from a mutex.
+    ///
+    /// If the mutex is poisoned, it will remain poisoned until this function is called. This
+    /// allows recovering from a poisoned state and marking that it has recovered. For example, if
+    /// the value is overwritten by a known-good value, then the mutex can be marked as
+    /// un-poisoned. Or possibly, the value could be inspected to determine if it is in a
+    /// consistent state, and if so the poison is removed.
+    pub fn clear_poison(&self) {
+        self.poison.clear();
     }
 }
 
-// safety: PyMutex serializes access
-unsafe impl<T: Send> Sync for PyMutex<T> {}
+fn map_result<T, U, F>(result: LockResult<T>, f: F) -> LockResult<U>
+where
+    F: FnOnce(T) -> U,
+{
+    match result {
+        Ok(t) => Ok(f(t)),
+        #[cfg(panic = "unwind")]
+        Err(e) => Err(PoisonError::new(f(e.into_inner()))),
+    }
+}
 
-impl<'a, T> Drop for PyMutexGuard<'a, T> {
+impl<'mutex, T: ?Sized> PyMutexGuard<'mutex, T> {
+    fn new(lock: &'mutex PyMutex<T>) -> LockResult<PyMutexGuard<'mutex, T>> {
+        map_result(lock.poison.guard(), |guard| PyMutexGuard {
+            inner: lock,
+            poison: guard,
+        })
+    }
+}
+
+impl<'a, T: ?Sized> Drop for PyMutexGuard<'a, T> {
     fn drop(&mut self) {
-        unsafe { crate::ffi::PyMutex_Unlock(UnsafeCell::raw_get(&self.inner.mutex)) };
+        unsafe {
+            self.inner.poison.done(&self.poison);
+            crate::ffi::PyMutex_Unlock(UnsafeCell::raw_get(&self.inner.mutex))
+        };
     }
 }
 
@@ -71,7 +248,7 @@ impl<'a, T> DerefMut for PyMutexGuard<'a, T> {
 mod tests {
     use std::sync::{
         atomic::{AtomicBool, Ordering},
-        Barrier,
+        Arc, Barrier,
     };
 
     use super::*;
@@ -81,16 +258,16 @@ mod tests {
 
     #[test]
     fn test_pymutex() {
-        let mutex = Python::with_gil(|py| -> PyMutex<Py<PyDict>> {
+        let mutex = Python::attach(|py| -> PyMutex<Py<PyDict>> {
             let d = PyDict::new(py);
             PyMutex::new(d.unbind())
         });
 
-        Python::with_gil(|py| {
-            let mutex = py.allow_threads(|| -> PyMutex<Py<PyDict>> {
+        Python::attach(|py| {
+            let mutex = py.detach(|| -> PyMutex<Py<PyDict>> {
                 std::thread::spawn(|| {
-                    let dict_guard = mutex.lock();
-                    Python::with_gil(|py| {
+                    let dict_guard = mutex.lock().unwrap();
+                    Python::attach(|py| {
                         let dict = dict_guard.bind(py);
                         dict.set_item(PyNone::get(py), PyNone::get(py)).unwrap();
                     });
@@ -101,7 +278,7 @@ mod tests {
                 .unwrap()
             });
 
-            let dict_guard = mutex.lock();
+            let dict_guard = mutex.lock().unwrap();
             let d = dict_guard.bind(py);
 
             assert!(d
@@ -151,6 +328,48 @@ mod tests {
             });
 
             barrier.wait();
+        });
+    }
+
+    #[test]
+    fn test_recover_poison() {
+        let mutex = Python::attach(|py| -> PyMutex<Py<PyDict>> {
+            let d = PyDict::new(py);
+            d.set_item("hello", "world").unwrap();
+            PyMutex::new(d.unbind())
+        });
+
+        let lock = Arc::new(mutex);
+        let lock2 = Arc::clone(&lock);
+
+        let _ = thread::spawn(move || -> () {
+            let _guard = lock2.lock().unwrap();
+
+            // poison the mutex
+            panic!();
+        })
+        .join();
+
+        // by now the lock is poisoned, use into_inner to recover the value despite that
+        let guard = match lock.lock() {
+            Ok(guard) => {
+                assert!(false);
+                guard
+            }
+            Err(poisoned) => poisoned.into_inner(),
+        };
+
+        Python::attach(|py| {
+            assert!(
+                (*guard)
+                    .bind(py)
+                    .get_item("hello")
+                    .unwrap()
+                    .unwrap()
+                    .extract::<&str>()
+                    .unwrap()
+                    == "world"
+            );
         });
     }
 }
