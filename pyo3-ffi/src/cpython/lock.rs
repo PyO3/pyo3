3a402db2267e756e8ab17db4f779c727fa7420c0
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2025-08-06 14:14:03 -0600

    add missing conditional compilation for older pythons in FFI

--- pyo3-ffi/src/cpython/lock.rs
+++ pyo3-ffi/src/cpython/lock.rs
@@ -1,19 +1,25 @@
+use std::marker::PhantomPinned;
 use std::sync::atomic::AtomicU8;
 
 #[repr(transparent)]
 #[derive(Debug)]
+#[cfg(all(Py_3_13, not(Py_LIMITED_API)))]
 pub struct PyMutex {
     pub(crate) _bits: AtomicU8,
+    pub(crate) _pin: PhantomPinned,
 }
 
+#[cfg(all(Py_3_13, not(Py_LIMITED_API)))]
 impl PyMutex {
     pub const fn new() -> PyMutex {
         PyMutex {
             _bits: AtomicU8::new(0),
+            _pin: PhantomPinned,
         }
     }
 }
 
+#[cfg(all(Py_3_13, not(Py_LIMITED_API)))]
 impl Default for PyMutex {
     fn default() -> Self {
         Self::new()
@@ -21,6 +27,8 @@ impl Default for PyMutex {
 }
 
 extern "C" {
+    #[cfg(all(Py_3_13, not(Py_LIMITED_API)))]
     pub fn PyMutex_Lock(m: *mut PyMutex);
+    #[cfg(all(Py_3_13, not(Py_LIMITED_API)))]
     pub fn PyMutex_Unlock(m: *mut PyMutex);
 }
