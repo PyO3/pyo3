5ad0723fdaf1e2b43f868fb88b8ceb3756366061
David Hewitt <mail@davidhewitt.dev>
2025-09-19 20:21:00 +0100

    add benchmark for function which takes many keyword arguments (#5452)
    * add benchmark for function which takes many keyword arguments
    
    * clippy

--- pytests/stubs/pyfunctions.pyi
+++ pytests/stubs/pyfunctions.pyi
@@ -1,6 +1,25 @@
 import typing
 
 def args_kwargs(*args, **kwargs) -> typing.Any: ...
+def many_keyword_arguments(
+    *,
+    ant: typing.Any | None = None,
+    bear: typing.Any | None = None,
+    cat: typing.Any | None = None,
+    dog: typing.Any | None = None,
+    elephant: typing.Any | None = None,
+    fox: typing.Any | None = None,
+    goat: typing.Any | None = None,
+    horse: typing.Any | None = None,
+    iguana: typing.Any | None = None,
+    jaguar: typing.Any | None = None,
+    koala: typing.Any | None = None,
+    lion: typing.Any | None = None,
+    monkey: typing.Any | None = None,
+    newt: typing.Any | None = None,
+    owl: typing.Any | None = None,
+    penguin: typing.Any | None = None,
+) -> typing.Any: ...
 def none() -> None: ...
 def positional_only(a: typing.Any, /, b: typing.Any) -> typing.Any: ...
 def simple(
