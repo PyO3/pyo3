09c40e2aaa9af61c9dd94ec5b2cc9c01c74879b1
Bas Schoenmaeckers <b.schoenmaeckers@zanders.eu>
2025-09-19 10:41:34 +0200

    Bench using new macro

--- pyo3-benches/benches/bench_pystring_from_fmt.rs
+++ pyo3-benches/benches/bench_pystring_from_fmt.rs
@@ -1,13 +1,12 @@
 use codspeed_criterion_compat::{criterion_group, criterion_main, Bencher, Criterion};
-use pyo3::types::PyString;
-use pyo3::Python;
+use pyo3::{py_format, Python};
 use std::hint::black_box;
 
 fn format_simple(b: &mut Bencher<'_>) {
     Python::attach(|py| {
         b.iter(|| {
-            let format_args = format_args!("Hello {}!", "world");
-            PyString::new(py, &format!("{format_args}"))
+            let result = py_format!(py, "Hello {}!", "world");
+            assert!(result.is_ok());
         });
     });
 }
@@ -16,8 +15,8 @@ fn format_complex(b: &mut Bencher<'_>) {
     Python::attach(|py| {
         b.iter(|| {
             let value = (black_box(42), black_box("foo"), [0; 0]);
-            let format_args = format_args!("This is some complex value: {value:?}");
-            PyString::new(py, &format!("{format_args}"))
+            let result = py_format!(py, "This is some complex value: {value:?}");
+            assert!(result.is_ok());
         });
     });
 }
