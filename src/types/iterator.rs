67ac7ab53bd04906ebece78116814c6b31a8c4c0
Peter Holloway <holloway.p.r@gmail.com>
2025-10-20 11:10:01 +0100

    Expose types::iterator::PySendResult in types module (#5539)
    * Expose types::iterator::PySendResult in types module
    
    PyIterator::send is a public method but the returned values are not
    usable while `PySendResult` is not public. Making it public allows
    callers to match on the result and access the wrapped values.
    
    * Gate export of PySendResult behind Py_3_10
    
    * Document PySendResult and add newsfragment

--- src/types/iterator.rs
+++ src/types/iterator.rs
@@ -58,10 +58,13 @@ impl PyIterator {
     }
 }
 
+/// Outcomes from sending a value into a python generator
 #[derive(Debug)]
 #[cfg(all(not(PyPy), Py_3_10))]
 pub enum PySendResult<'py> {
+    /// The generator yielded a new value
     Next(Bound<'py, PyAny>),
+    /// The generator completed, returning a (possibly None) final value
     Return(Bound<'py, PyAny>),
 }
 
