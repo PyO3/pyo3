cab3fa2f03333afd0a9f276f96434074bca636e1
Kyle Altendorf <sda@fstab.net>
2025-11-18 07:54:41 -0500

    Refactor add_submodule to simplify name extraction

--- src/types/module.rs
+++ src/types/module.rs
@@ -514,11 +514,9 @@ impl<'py> PyModuleMethods<'py> for Bound<'py, PyModule> {
     }
 
     fn add_submodule(&self, module: &Bound<'_, PyModule>) -> PyResult<()> {
-        let name = module
-            .name()?
-            .call_method1("rpartition", (".",))?
-            .get_item(2)?
-            .cast_into::<PyString>()?;
+        let name = module.name()?;
+        let name = name.to_cow()?;
+        let name = name.rsplit_once(b'.').map_or(&name, |(_, last_part)| last_part);
         self.add(name, module)
     }
 
