db764d6a83a1db60ae72adba4d41cdbf7136d900
David Hewitt <mail@davidhewitt.dev>
2025-12-12 16:30:39 +0000

    use `Borrowed` in `extract_argument` functions

--- src/pyclass/guard.rs
+++ src/pyclass/guard.rs
@@ -95,6 +95,12 @@ impl<'a, T: PyClass> PyClassGuard<'a, T> {
         Self::try_from_class_object(obj.get_class_object())
     }
 
+    pub(crate) fn try_borrow_from_borrowed(
+        obj: Borrowed<'a, '_, T>,
+    ) -> Result<Self, PyBorrowError> {
+        Self::try_from_class_object(obj.get_class_object())
+    }
+
     fn try_from_class_object(obj: &'a PyClassObject<T>) -> Result<Self, PyBorrowError> {
         obj.ensure_threadsafe();
         obj.borrow_checker().try_borrow().map(|_| Self {
@@ -588,6 +594,12 @@ pub struct PyClassGuardMut<'a, T: PyClass<Frozen = False>> {
 
 impl<'a, T: PyClass<Frozen = False>> PyClassGuardMut<'a, T> {
     pub(crate) fn try_borrow_mut(obj: &'a Py<T>) -> Result<Self, PyBorrowMutError> {
+        Self::try_from_class_object(obj.get_class_object())
+    }
+
+    pub(crate) fn try_borrow_mut_from_borrowed(
+        obj: Borrowed<'a, '_, T>,
+    ) -> Result<Self, PyBorrowMutError> {
         Self::try_from_class_object(obj.get_class_object())
     }
 
