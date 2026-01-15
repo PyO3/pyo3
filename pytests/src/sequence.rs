5a0f355bbe5495640fc055db6f67b9b9181e42cc
Thomas Tanon <thomas.pellissier-tanon@helsing.ai>
2026-01-15 23:38:56 +0100

    Migrate all pytests modules to declarative modules and adds stubs (#5735)

--- pytests/src/sequence.rs
+++ pytests/src/sequence.rs
@@ -1,25 +1,22 @@
 use pyo3::prelude::*;
-use pyo3::types::PyString;
-
-#[pyfunction]
-fn vec_to_vec_i32(vec: Vec<i32>) -> Vec<i32> {
-    vec
-}
-
-#[pyfunction]
-fn array_to_array_i32(arr: [i32; 3]) -> [i32; 3] {
-    arr
-}
-
-#[pyfunction]
-fn vec_to_vec_pystring(vec: Vec<Bound<'_, PyString>>) -> Vec<Bound<'_, PyString>> {
-    vec
-}
 
 #[pymodule]
-pub fn sequence(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(vec_to_vec_i32, m)?)?;
-    m.add_function(wrap_pyfunction!(array_to_array_i32, m)?)?;
-    m.add_function(wrap_pyfunction!(vec_to_vec_pystring, m)?)?;
-    Ok(())
+pub mod sequence {
+    use pyo3::prelude::*;
+    use pyo3::types::PyString;
+
+    #[pyfunction]
+    fn vec_to_vec_i32(vec: Vec<i32>) -> Vec<i32> {
+        vec
+    }
+
+    #[pyfunction]
+    fn array_to_array_i32(arr: [i32; 3]) -> [i32; 3] {
+        arr
+    }
+
+    #[pyfunction]
+    fn vec_to_vec_pystring(vec: Vec<Bound<'_, PyString>>) -> Vec<Bound<'_, PyString>> {
+        vec
+    }
 }
