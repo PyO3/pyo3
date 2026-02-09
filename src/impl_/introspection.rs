d08f5730c98dea197e91d39f199ddb870f7eac5c
Luiz Eduardo Amaral <luizamaral306@gmail.com>
2026-02-09 11:51:03 -0300

    add Sealed to introspection traits

--- src/impl_/introspection.rs
+++ src/impl_/introspection.rs
@@ -1,10 +1,20 @@
 use crate::conversion::IntoPyObject;
 use crate::inspect::PyStaticExpr;
 
+/// Seals `PyReturnType` so that types outside PyO3 cannot implement it.
+mod return_type {
+    use crate::{impl_::introspection::PyReturnType, IntoPyObject};
+
+    pub trait Sealed {}
+
+    impl<'a, T: IntoPyObject<'a>> Sealed for T {}
+    impl<T: PyReturnType, E> Sealed for Result<T, E> {}
+}
+
 /// Trait to guess a function Python return type
 ///
 /// It is useful to properly get the return type `T` when the Rust implementation returns e.g. `PyResult<T>`
-pub trait PyReturnType {
+pub trait PyReturnType: return_type::Sealed {
     /// The function return type
     const OUTPUT_TYPE: PyStaticExpr;
 }
