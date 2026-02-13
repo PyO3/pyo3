c1bd2c781ab3327ca67a485bf67fdab6005eef30
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2026-02-13 14:58:20 -0700

    skip test that depend on struct layout on opaque pyobject builds

--- src/impl_/pyclass.rs
+++ src/impl_/pyclass.rs
@@ -1458,6 +1458,7 @@ pub trait ExtractPyClassWithClone {}
 #[cfg(test)]
 #[cfg(feature = "macros")]
 mod tests {
+    #[cfg(not(_Py_OPAQUE_PYOBJECT))]
     use crate::pycell::impl_::PyClassObjectContents;
 
     use super::*;
@@ -1486,11 +1487,13 @@ mod tests {
             Some(PyMethodDefType::StructMember(member)) => {
                 assert_eq!(unsafe { CStr::from_ptr(member.name) }, c"value");
                 assert_eq!(member.type_code, ffi::Py_T_OBJECT_EX);
+                #[cfg(not(_Py_OPAQUE_PYOBJECT))]
                 #[repr(C)]
                 struct ExpectedLayout {
                     ob_base: ffi::PyObject,
                     contents: PyClassObjectContents<FrozenClass>,
                 }
+                #[cfg(not(_Py_OPAQUE_PYOBJECT))]
                 assert_eq!(
                     member.offset,
                     (offset_of!(ExpectedLayout, contents) + offset_of!(FrozenClass, value))
