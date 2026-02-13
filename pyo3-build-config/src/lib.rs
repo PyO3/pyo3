2f6f5f84c634fc73b34635cf18c40a3966442e55
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2026-02-13 14:50:35 -0700

    Document Py_GIL_DISABLED in pyo3-build-config docs (#5810)

--- pyo3-build-config/src/lib.rs
+++ pyo3-build-config/src/lib.rs
@@ -35,6 +35,7 @@ use target_lexicon::OperatingSystem;
 /// | ---- | ----------- |
 /// | `#[cfg(Py_3_7)]`, `#[cfg(Py_3_8)]`, `#[cfg(Py_3_9)]`, `#[cfg(Py_3_10)]` | These attributes mark code only for a given Python version and up. For example, `#[cfg(Py_3_7)]` marks code which can run on Python 3.7 **and newer**. |
 /// | `#[cfg(Py_LIMITED_API)]` | This marks code which is run when compiling with PyO3's `abi3` feature enabled. |
+/// | `#[cfg(Py_GIL_DISABLED)]` | This marks code which is run on the free-threaded interpreter. |
 /// | `#[cfg(PyPy)]` | This marks code which is run when compiling for PyPy. |
 /// | `#[cfg(GraalPy)]` | This marks code which is run when compiling for GraalPy. |
 ///
