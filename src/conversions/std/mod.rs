887229327b4feaa3bc9c3598e35c12c15f2ba8fa
person93 <person93.person93@gmail.com>
2026-03-05 16:42:27 -0500

    unconditionally include map and set mods

--- src/conversions/std/mod.rs
+++ src/conversions/std/mod.rs
@@ -2,7 +2,6 @@ mod array;
 mod cell;
 mod cstring;
 mod ipaddr;
-#[cfg(feature = "std")]
 mod map;
 pub(crate) mod num;
 mod option;
@@ -10,7 +9,6 @@ mod option;
 mod osstr;
 #[cfg(feature = "std")]
 mod path;
-#[cfg(feature = "std")]
 mod set;
 mod slice;
 mod string;
