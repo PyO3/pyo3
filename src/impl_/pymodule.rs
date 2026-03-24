264307fe76f5d70658bd33019d67eaa2bcfe5913
Nathan Goldbaum <nathan.goldbaum@gmail.com>
2026-03-24 12:36:28 -0600

    fix merge conflict resolution error

--- src/impl_/pymodule.rs
+++ src/impl_/pymodule.rs
@@ -182,7 +182,6 @@ impl ModuleDef {
         {
             let ffi_def = self.ffi_def.get();
 
-            let name = unsafe { CStr::from_ptr((*ffi_def).m_name).to_str()? }.to_string();
             let m_name = unsafe { CStr::from_ptr((*ffi_def).m_name) };
             let name = m_name
                 .to_str()
