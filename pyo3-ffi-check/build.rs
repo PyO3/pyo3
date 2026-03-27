baadc08a9290c3f56aa90ca878bffa3a9abed1ce
David Hewitt <mail@davidhewitt.dev>
2026-03-27 19:52:26 +0000

    forward target into docs build

--- pyo3-ffi-check/build.rs
+++ pyo3-ffi-check/build.rs
@@ -2,6 +2,7 @@ fn main() {
     let status = std::process::Command::new("cargo")
         .args(["doc", "-p", "pyo3-ffi-check-definitions", "--no-deps"])
         .env("CARGO_TARGET_DIR", std::env::var("OUT_DIR").unwrap())
+        .env("CARGO_BUILD_TARGET", std::env::var("TARGET").unwrap())
         .status()
         .expect("failed to build definitions");
 
