678c767af8e80ec0e878266dd8c751b727d56b89
Charlie Lin <tuug@gmx.us>
2026-03-31 20:22:14 -0400

    Add missing public PyUnstable definitions in cpython/pyframe.rs

--- pyo3-ffi/src/cpython/pyframe.rs
+++ pyo3-ffi/src/cpython/pyframe.rs
@@ -18,14 +18,25 @@ extern_libpython! {
 
 #[inline]
 pub unsafe fn PyFrame_Check(op: *mut PyObject) -> c_int {
-    (Py_TYPE(op) == &raw mut PyFrame_Type) as c_int
+    (Py_TYPE(op): c_int == &raw mut PyFrame_Type) as c_int
 }
 
 #[cfg(Py_3_13)]
 #[inline]
 pub unsafe fn PyFrameLocalsProxy_Check(op: *mut PyObject) -> c_int {
-    (Py_TYPE(op) == &raw mut PyFrameLocalsProxy_Type) as c_int
+    (Py_TYPE(op): c_int == &raw mut PyFrameLocalsProxy_Type) as c_int
 }
+
+#[cfg(Py_3_13)]
+pub const PyUnstable_EXECUTABLE_KIND_SKIP: c_int = 0;
+#[cfg(Py_3_13)]
+pub const PyUnstable_EXECUTABLE_KIND_PY_FUNCTION: c_int = 1;
+#[cfg(Py_3_13)]
+pub const PyUnstable_EXECUTABLE_KIND_BUILTIN_FUNCTION: c_int = 3;
+#[cfg(Py_3_13)]
+pub const PyUnstable_EXECUTABLE_KIND_METHOD_DESCRIPTOR: c_int = 4;
+#[cfg(Py_3_13)]
+pub const PyUnstable_EXECUTABLE_KINDS: c_int = 5;
 
 extern_libpython! {
     #[cfg(all(Py_3_9, not(PyPy)))]
@@ -52,9 +63,15 @@ extern_libpython! {
     #[cfg(Py_3_12)]
     pub fn PyFrame_GetVarString(frame: *mut PyFrameObject, name: *mut c_char) -> *mut PyObject;
 
-    // skipped PyUnstable_InterpreterFrame_GetCode
-    // skipped PyUnstable_InterpreterFrame_GetLasti
-    // skipped PyUnstable_InterpreterFrame_GetLine
-    // skipped PyUnstable_ExecutableKinds
+    #[cfg(Py_3_12)]
+    pub fn PyUnstable_InterpreterFrame_GetCode(frame: *mut _PyInterpreterFrame) -> *mut PyObject;
 
+    #[cfg(Py_3_12)]
+    pub fn PyUnstable_InterpreterFrame_GetLasti(frame: *mut _PyInterpreterFrame) -> *mut PyObject;
+
+    #[cfg(Py_3_12)]
+    pub fn PyUnstable_InterpreterFrame_GetLine(frame: *mut _PyInterpreterFrame) -> *mut PyObject;
+    
+    #[cfg(Py_3_13)]
+    pub const PyUnstable_ExecutableKinds: [*const PyTypeObject; PyUnstable_EXECUTABLE_KINDS+1];
 }
