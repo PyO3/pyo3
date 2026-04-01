2022c487caea7eb703c17e2c61a979073c8a7e0e
Charlie Lin <tuug@gmx.us>
2026-03-31 22:45:51 -0400

    Change c_uint to c_int for executable kinds

--- pyo3-ffi/src/cpython/pyframe.rs
+++ pyo3-ffi/src/cpython/pyframe.rs
@@ -4,22 +4,21 @@ use crate::{PyObject, PyTypeObject, Py_TYPE};
 #[cfg(Py_3_12)]
 use std::ffi::c_char;
 use std::ffi::c_int;
-use std::ffi::c_uint;
 
 // NB used in `_PyEval_EvalFrameDefault`, maybe we remove this too.
 #[cfg(all(Py_3_11, not(PyPy)))]
 opaque_struct!(pub _PyInterpreterFrame);
 
 #[cfg(Py_3_13)]
-pub const PyUnstable_EXECUTABLE_KIND_SKIP: c_uint = 0;
+pub const PyUnstable_EXECUTABLE_KIND_SKIP: c_int = 0;
 #[cfg(Py_3_13)]
-pub const PyUnstable_EXECUTABLE_KIND_PY_FUNCTION: c_uint = 1;
+pub const PyUnstable_EXECUTABLE_KIND_PY_FUNCTION: c_int = 1;
 #[cfg(Py_3_13)]
-pub const PyUnstable_EXECUTABLE_KIND_BUILTIN_FUNCTION: c_uint = 3;
+pub const PyUnstable_EXECUTABLE_KIND_BUILTIN_FUNCTION: c_int = 3;
 #[cfg(Py_3_13)]
-pub const PyUnstable_EXECUTABLE_KIND_METHOD_DESCRIPTOR: c_uint = 4;
+pub const PyUnstable_EXECUTABLE_KIND_METHOD_DESCRIPTOR: c_int = 4;
 #[cfg(Py_3_13)]
-pub const PyUnstable_EXECUTABLE_KINDS: c_uint = 5;
+pub const PyUnstable_EXECUTABLE_KINDS: c_int = 5;
 
 extern_libpython! {
     pub static mut PyFrame_Type: PyTypeObject;
