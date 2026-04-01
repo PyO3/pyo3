55169f9f62fe05bee1bcc03ba90ea2bccdf00fe3
Charlie Lin <tuug@gmx.us>
2026-03-31 21:23:40 -0400

    Move ExecutableKind declarations

--- pyo3-ffi/src/cpython/pyframe.rs
+++ pyo3-ffi/src/cpython/pyframe.rs
@@ -9,11 +9,25 @@ use std::ffi::c_int;
 #[cfg(all(Py_3_11, not(PyPy)))]
 opaque_struct!(pub _PyInterpreterFrame);
 
+#[cfg(Py_3_13)]
+pub const PyUnstable_EXECUTABLE_KIND_SKIP: c_int = 0;
+#[cfg(Py_3_13)]
+pub const PyUnstable_EXECUTABLE_KIND_PY_FUNCTION: c_int = 1;
+#[cfg(Py_3_13)]
+pub const PyUnstable_EXECUTABLE_KIND_BUILTIN_FUNCTION: c_int = 3;
+#[cfg(Py_3_13)]
+pub const PyUnstable_EXECUTABLE_KIND_METHOD_DESCRIPTOR: c_int = 4;
+#[cfg(Py_3_13)]
+pub const PyUnstable_EXECUTABLE_KINDS: c_int = 5;
+
 extern_libpython! {
     pub static mut PyFrame_Type: PyTypeObject;
 
     #[cfg(Py_3_13)]
     pub static mut PyFrameLocalsProxy_Type: PyTypeObject;
+
+    #[cfg(Py_3_13)]
+    pub static PyUnstable_ExecutableKinds: [*const PyTypeObject; PyUnstable_EXECUTABLE_KINDS+1];
 }
 
 #[inline]
@@ -26,17 +40,6 @@ pub unsafe fn PyFrame_Check(op: *mut PyObject) -> c_int {
 pub unsafe fn PyFrameLocalsProxy_Check(op: *mut PyObject) -> c_int {
     (Py_TYPE(op) == &raw mut PyFrameLocalsProxy_Type) as c_int
 }
-
-#[cfg(Py_3_13)]
-pub const PyUnstable_EXECUTABLE_KIND_SKIP: c_int = 0;
-#[cfg(Py_3_13)]
-pub const PyUnstable_EXECUTABLE_KIND_PY_FUNCTION: c_int = 1;
-#[cfg(Py_3_13)]
-pub const PyUnstable_EXECUTABLE_KIND_BUILTIN_FUNCTION: c_int = 3;
-#[cfg(Py_3_13)]
-pub const PyUnstable_EXECUTABLE_KIND_METHOD_DESCRIPTOR: c_int = 4;
-#[cfg(Py_3_13)]
-pub const PyUnstable_EXECUTABLE_KINDS: c_int = 5;
 
 extern_libpython! {
     #[cfg(all(Py_3_9, not(PyPy)))]
@@ -71,7 +74,4 @@ extern_libpython! {
 
     #[cfg(Py_3_12)]
     pub fn PyUnstable_InterpreterFrame_GetLine(frame: *mut _PyInterpreterFrame) -> *mut PyObject;
-
-    #[cfg(Py_3_13)]
-    pub static PyUnstable_ExecutableKinds: [*const PyTypeObject; PyUnstable_EXECUTABLE_KINDS+1];
 }
