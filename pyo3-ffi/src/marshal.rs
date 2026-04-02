143f0351e1294dc40cd9a6662d8352a17a0a5f95
Charlie Lin <tuug@gmx.us>
2026-04-02 08:36:44 -0400

    All members in marshal.rs aren't accessible when using the Limited API

--- pyo3-ffi/src/marshal.rs
+++ pyo3-ffi/src/marshal.rs
@@ -2,28 +2,35 @@ use super::{PyObject, Py_ssize_t};
 use libc::FILE;
 use std::ffi::{c_char, c_int, c_long};
 
-#[cfg(Py_3_15)]
+#[cfg(all(Py_3_15, not(Py_LIMITED_API)))]
 pub const Py_MARSHAL_VERSION: c_int = 6;
 
-#[cfg(not(Py_3_15))]
+#[cfg(all(not(Py_3_15), not(Py_LIMITED_API)))]
 pub const Py_MARSHAL_VERSION: c_int = 5;
 
 extern_libpython! {
     #[cfg_attr(PyPy, link_name = "PyPyMarshal_WriteObjectToString")]
+    #[cfg(not(Py_LIMITED_API))]
     pub fn PyMarshal_WriteObjectToString(object: *mut PyObject, version: c_int) -> *mut PyObject;
 
     #[cfg_attr(PyPy, link_name = "PyPyMarshal_ReadObjectFromString")]
     pub fn PyMarshal_ReadObjectFromString(data: *const c_char, len: Py_ssize_t) -> *mut PyObject;
 
+    #[cfg(not(Py_LIMITED_API))]
     pub fn PyMarshal_WriteLongToFile(value: c_long, file: *mut FILE, version: c_int);
 
+    #[cfg(not(Py_LIMITED_API))]
     pub fn PyMarshal_WriteObjectToFile(object: *mut PyObject, file: *mut FILE, version: c_int);
 
+    #[cfg(not(Py_LIMITED_API))]
     pub fn PyMarshal_ReadLongFromFile(file: *mut FILE) -> c_long;
 
+    #[cfg(not(Py_LIMITED_API))]
     pub fn PyMarshal_ReadShortFromFile(file: *mut FILE) -> c_int;
 
+    #[cfg(not(Py_LIMITED_API))]
     pub fn PyMarshal_ReadObjectFromFile(file: *mut FILE) -> *mut PyObject;
 
+    #[cfg(not(Py_LIMITED_API))]
     pub fn PyMarshal_ReadLastObjectFromFile(file: *mut FILE) -> *mut PyObject;
 }
