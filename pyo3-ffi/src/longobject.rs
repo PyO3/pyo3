3d80a93c5d6baf5a72202af767776b39f50a0cdd
Code-Apprentice <codeguru42@gmail.com>
2026-04-11 18:28:38 -0600

    Fix mismatched parentheses

--- pyo3-ffi/src/longobject.rs
+++ pyo3-ffi/src/longobject.rs
@@ -85,7 +85,7 @@ extern_libpython! {
 #[cfg(not(Py_LIMITED_API))]
 extern_libpython! {
     #[cfg_attr(PyPy, link_name = "_PyPyLong_NumBits")]
-    #[cfg(not(Py_3_13)]
+    #[cfg(not(Py_3_13))]
     #[doc(hidden)]
     pub fn _PyLong_NumBits(obj: *mut PyObject) -> size_t;
 }
