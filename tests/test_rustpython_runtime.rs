0653c94628832fc339971bbf4adb1a3a5c26f1d9
sunnymar <sunnym@gmail.com>
2026-04-13 08:20:11 +0300

    test: mark RustPython worker-thread import repro as blocked upstream

--- tests/test_rustpython_runtime.rs
+++ tests/test_rustpython_runtime.rs
@@ -3,6 +3,7 @@
 use pyo3::prelude::*;
 
 #[test]
+#[ignore = "upstream RustPython bug: spawned-thread imports recurse in importlib (_blocking_on); see RustPython/RustPython#7586"]
 fn worker_thread_can_import_array() {
     let handle = std::thread::spawn(|| {
         Python::attach(|py| {
