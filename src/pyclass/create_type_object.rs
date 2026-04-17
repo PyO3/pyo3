a2fd837fc84fb30b4ea147e0fe9acbe19b28997e
Winston H. <winstxnhdw@gmail.com>
2026-04-17 16:21:28 +0800

    style: apply cargo fmt (#5981)
    * style: apply cargo fmt
    
    * Delete newsfragments/5981.changed.md
    
    * chore: update ui tests
    
    * chore: update ui tests again

--- src/pyclass/create_type_object.rs
+++ src/pyclass/create_type_object.rs
@@ -11,7 +11,7 @@ use crate::{
             assign_sequence_item_from_mapping, get_sequence_item_from_mapping, tp_dealloc,
             tp_dealloc_with_gc, PyClassImpl, PyClassItemsIter, PyObjectOffset,
         },
-        pymethods::{Getter, PyGetterDef, PyMethodDefType, PySetterDef, Setter, _call_clear},
+        pymethods::{_call_clear, Getter, PyGetterDef, PyMethodDefType, PySetterDef, Setter},
         trampoline::trampoline,
     },
     pycell::impl_::PyClassObjectLayout,
