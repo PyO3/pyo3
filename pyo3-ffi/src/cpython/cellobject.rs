78d27bd778593c8e33d3530a8e7d2ba7bcc08130
Charlie Lin <tuug@gmx.us>
2026-04-17 14:19:29 +0000

    fmt

--- pyo3-ffi/src/cpython/cellobject.rs
+++ pyo3-ffi/src/cpython/cellobject.rs
@@ -9,15 +9,15 @@ pub struct PyCellObject {
 }
 
 extern_libpython! {
-  pub fn PyCell_New(o: *mut PyObject) -> *mut PyObject;
-  pub fn PyCell_Get(o: *mut PyObject) -> *mut PyObject;
-  pub fn PyCell_Set(o: *mut PyObject, val: *mut PyObject) -> *mut PyObject;
-  pub static mut PyCell_Type: PyTypeObject;
+    pub fn PyCell_New(o: *mut PyObject) -> *mut PyObject;
+    pub fn PyCell_Get(o: *mut PyObject) -> *mut PyObject;
+    pub fn PyCell_Set(o: *mut PyObject, val: *mut PyObject) -> *mut PyObject;
+    pub static mut PyCell_Type: PyTypeObject;
 }
 
 #[inline]
 pub unsafe fn PyCell_Check(op: *mut PyObject) -> c_int {
-  Py_IS_TYPE(op, &raw mut PyCell_Type)
+    Py_IS_TYPE(op, &raw mut PyCell_Type)
 }
 
 // skipped PyCell_SET
