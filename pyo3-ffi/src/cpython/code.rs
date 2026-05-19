588330727a7e08350917a34b978b9418ae3906c6
Charlie Lin <tuug@gmx.us>
2026-05-19 15:35:55 -0500

    Change visibility of _PyCode_GetExtra and _PyCode_SetExtra

--- pyo3-ffi/src/cpython/code.rs
+++ pyo3-ffi/src/cpython/code.rs
@@ -140,12 +140,12 @@ extern_libpython! {
         names: *mut PyObject,
         lnotab: *mut PyObject,
     ) -> *mut PyObject;
-    pub(crate) fn _PyCode_GetExtra(
+    pub fn _PyCode_GetExtra(
         code: *mut PyObject,
         index: Py_ssize_t,
         extra: *const *mut c_void,
     ) -> c_int;
-    pub(crate) fn _PyCode_SetExtra(
+    pub fn _PyCode_SetExtra(
         code: *mut PyObject,
         index: Py_ssize_t,
         extra: *mut c_void,
