7d2edfd55997574e7ca9ebc1d7e1698119ca3370
person93 <person93.person93@gmail.com>
2026-05-18 21:16:15 -0400

    forbid `std::sync::Once`

--- src/sealed.rs
+++ src/sealed.rs
@@ -65,6 +65,8 @@ impl Sealed for ModuleDef {}
 impl<T: crate::type_object::PyTypeInfo> Sealed for PyNativeTypeInitializer<T> {}
 impl<T: crate::pyclass::PyClass> Sealed for PyClassInitializer<T> {}
 
+#[cfg(wip_feature_std)]
+#[allow(clippy::disallowed_types)]
 impl Sealed for std::sync::Once {}
 #[cfg(wip_feature_std)]
 #[allow(clippy::disallowed_types)]
