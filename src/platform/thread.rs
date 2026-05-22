88fb5c01c85db1f33614ff60128c02858cee3df6
person93 <person93.person93@gmail.com>
2026-05-22 10:35:43 -0400

    use `c_ulong` instead of `NonZero<u64>`

--- src/platform/thread.rs
+++ src/platform/thread.rs
@@ -1,3 +1,4 @@
+use core::ffi::c_ulong;
 use core::num::NonZero;
 
 use pyo3_ffi::PyThread_get_thread_ident;
@@ -5,7 +6,9 @@ use pyo3_ffi::PyThread_get_thread_ident;
 #[must_use]
 pub fn current() -> Thread {
     Thread {
-        id: ThreadId(unsafe { PyThread_get_thread_ident() }),
+        // SAFETY: PyThread_get_thread_ident never returns zero
+        // https://docs.python.org/3/c-api/threads.html#c.PyThread_get_thread_ident
+        id: ThreadId(unsafe { NonZero::new_unchecked(PyThread_get_thread_ident()) }),
     }
 }
 
@@ -20,4 +23,4 @@ impl Thread {
 }
 
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
-pub struct ThreadId(NonZero<u64>);
+pub struct ThreadId(NonZero<c_ulong>);
