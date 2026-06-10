180e71ff9d96fabe5a787fa6c90b32290e48db26
David Hewitt <mail@davidhewitt.dev>
2026-06-10 09:36:16 +0100

    skip detach test on graalpy

--- pytests/tests/test_finalization.py
+++ pytests/tests/test_finalization.py
@@ -1,7 +1,7 @@
 import sysconfig
+from sys import implementation
 
 import pytest
-
 from pyo3_pytests import misc
 
 
@@ -28,5 +28,9 @@ def test_hammer_attaching_in_thread():
     loopy.append(misc.hammer_attaching_in_thread())
 
 
+@pytest.mark.skipif(
+    implementation.name == "graalpy",
+    reason="graalpy aborts instead of unwinding the thread",
+)
 def test_detach_during_finalization():
     loopy.append(misc.detach_during_finalization())
