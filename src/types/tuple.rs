a812a1aa691f376b55b85d316d311a2d45f2821a
chiri <chirizxc@proton.me>
2026-06-12 23:11:56 +0300

    add SAFETY

--- src/types/tuple.rs
+++ src/types/tuple.rs
@@ -510,6 +510,7 @@ impl DoubleEndedIterator for BoundTupleIterator<'_> {
 
 impl ExactSizeIterator for BoundTupleIterator<'_> {
     fn len(&self) -> usize {
+        // SAFETY: `index <= length` is maintained by all iterator methods: `new()`, `next_back()`
         unsafe { std::hint::assert_unchecked(self.index <= self.length) };
         self.length.saturating_sub(self.index)
     }
@@ -609,6 +610,7 @@ impl DoubleEndedIterator for BorrowedTupleIterator<'_, '_> {
 
 impl ExactSizeIterator for BorrowedTupleIterator<'_, '_> {
     fn len(&self) -> usize {
+        // SAFETY: same invariant as BoundTupleIterator
         unsafe { std::hint::assert_unchecked(self.index <= self.length) };
         self.length.saturating_sub(self.index)
     }
