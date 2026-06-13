2be97b4b39bbe9a43ea11d7bbbc4e5b7bb5c18fb
chiri <chirizxc@proton.me>
2026-06-13 10:04:35 +0300

    fix

--- src/conversions/num_bigint.rs
+++ src/conversions/num_bigint.rs
@@ -281,7 +281,7 @@ fn int_to_u32_vec<const SIGNED: bool>(long: &Bound<'_, PyInt>) -> PyResult<Vec<u
         // SAFETY: `n_bytes_unsigned <= isize::MAX` (try_into above) and
         // `n_digits * 4 <= n_bytes_unsigned + 3`
         #[expect(clippy::checked_conversions)]
-        core::hint::assert_unchecked(n_digits * 4 <= isize::MAX as usize)
+        core::hint::assert_unchecked(n_digits * 4 <= isize::MAX as usize);
         ffi::PyLong_AsNativeBytes(
             long.as_ptr().cast(),
             buffer.as_mut_ptr().cast(),
