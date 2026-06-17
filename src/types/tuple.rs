33997b5e274bd79a312c1d3063b82fa932155918
chiri <chirizxc@proton.me>
2026-06-17 18:13:56 +0300

    review (x2)

--- src/types/tuple.rs
+++ src/types/tuple.rs
@@ -510,8 +510,6 @@ impl DoubleEndedIterator for BoundTupleIterator<'_> {
 
 impl ExactSizeIterator for BoundTupleIterator<'_> {
     fn len(&self) -> usize {
-        // SAFETY: `index <= length` is maintained by all iterator methods: `new()`, `next_back()`
-        unsafe { core::hint::assert_unchecked(self.index <= self.length) };
         self.length.saturating_sub(self.index)
     }
 }
@@ -610,8 +608,6 @@ impl DoubleEndedIterator for BorrowedTupleIterator<'_, '_> {
 
 impl ExactSizeIterator for BorrowedTupleIterator<'_, '_> {
     fn len(&self) -> usize {
-        // SAFETY: same invariant as BoundTupleIterator
-        unsafe { core::hint::assert_unchecked(self.index <= self.length) };
         self.length.saturating_sub(self.index)
     }
 }
