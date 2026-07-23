3d2e246badd0e872c1d6cff40fa86523eddb896c
Florentin Labelle <florentin.labelle@datadoghq.com>
2026-07-23 13:15:43 +0200

    Add safe context watcher API

---
+++ src/types/context.rs
@@ -0,0 +1,21 @@
+#![deny(clippy::undocumented_unsafe_blocks)]
+
+use crate::{ffi, PyAny};
+
+/// Represents a Python [`contextvars.Context`][1] object.
+///
+/// Values of this type are accessed via PyO3's smart pointers, e.g. as
+/// [`Py<PyContext>`][crate::Py] or [`Bound<'py, PyContext>`][crate::Bound].
+///
+/// [1]: https://docs.python.org/3/library/contextvars.html#contextvars.Context
+#[repr(transparent)]
+pub struct PyContext(PyAny);
+
+pyobject_native_type_core!(
+    PyContext,
+    pyobject_native_static_type_object!(ffi::PyContext_Type),
+    "contextvars",
+    "Context",
+    #module=Some("contextvars"),
+    #checkfunction=ffi::PyContext_CheckExact
+);
