d53be71954ba9c055b3ce07f580ac865a3e00167
chiri <chirizxc@proton.me>
2026-07-31 21:45:38 +0300

    more test

---
+++ tests/test_pymem_alloc.rs
@@ -0,0 +1,22 @@
+#![cfg(feature = "pymem-raw-alloc")]
+
+use pyo3::pymem_alloc::PyMemRawAllocator;
+
+#[global_allocator]
+static GLOBAL: PyMemRawAllocator = PyMemRawAllocator;
+
+#[test]
+fn allocations_work_through_global_allocator() {
+    let mut vec: Vec<u8> = Vec::with_capacity(1024);
+    vec.extend_from_slice(&[1, 2, 3, 4]);
+    assert_eq!(vec, vec![1, 2, 3, 4]);
+
+    let s = String::from("hello");
+    assert_eq!(s.len(), 4);
+
+    #[repr(align(64))]
+    struct Aligned([u8; 128]);
+    let boxed = Box::new(Aligned([7u8; 128]));
+    assert_eq!(boxed.0[0], 7);
+    assert_eq!((&*boxed as *const Aligned).addr() % 64, 0);
+}
