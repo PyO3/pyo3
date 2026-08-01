d9f21b811fc0194ad8acbc748b08a792dda8ef3c
chiri <chirizxc@proton.me>
2026-08-01 13:06:08 +0300

    fmt

--- tests/test_pymem_alloc.rs
+++ tests/test_pymem_alloc.rs
@@ -14,7 +14,7 @@ fn allocations_work_through_global_allocator() {
 
     #[repr(align(64))]
     struct Aligned([u8; 128]);
-    let boxed = Box::new(Aligned([7u8; 128]));
+    let boxed = Box::new(Aligned([7_u8; 128]));
     assert_eq!(boxed.0[0], 7);
     assert_eq!((&*boxed as *const Aligned).addr() % 64, 0);
 }
