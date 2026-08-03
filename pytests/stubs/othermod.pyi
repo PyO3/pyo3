387a136c8516e31060d7d3cb49e9bd829b24c1aa
Thomas Pellissier-Tanon <thomas.pellissier-tanon@helsing.ai>
2026-08-03 09:23:54 +0200

    Introspection: make __new__ return Self
    It a class is overridden but not __new__, __new__ still return an object instance of the child class and not of the parent class
    
    Uses typing.Self if the module is compiled for 3.11+, typing_extensions instead
    
    # Conflicts:
    #	pytests/stubs/buf_and_str.pyi
    #	pytests/stubs/comparisons.pyi
    #	pytests/stubs/datetime.pyi
    #	pytests/stubs/dict_iter.pyi
    #	pytests/stubs/enums.pyi
    #	pytests/stubs/othermod.pyi
    #	pytests/stubs/pyclasses.pyi

--- pytests/stubs/othermod.pyi
+++ pytests/stubs/othermod.pyi
@@ -1,11 +1,11 @@
-from typing import Final, SupportsIndex, final
+from typing import Final, Self, SupportsIndex, final
 
 USIZE_MAX: Final[int]
 USIZE_MIN: Final[int]
 
 @final
 class ModClass:
-    def __new__(cls, /) -> ModClass: ...
+    def __new__(cls, /) -> Self: ...
     def noop(self, /, x: SupportsIndex) -> int: ...
 
 def double(x: SupportsIndex) -> int: ...
