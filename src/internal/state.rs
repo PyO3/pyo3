322dd735584f0622d7ab85d0ef007e598c1ccd4b
person93 <person93.person93@gmail.com>
2026-08-07 04:00:53 -0400

    remove OnceLock that was wrapping a Mutex

--- src/internal/state.rs
+++ src/internal/state.rs
@@ -11,7 +11,7 @@ use core::sync::atomic::{AtomicBool, Ordering};
 #[cfg_attr(pyo3_disable_reference_pool, allow(unused_imports))]
 use core::{mem, ptr::NonNull};
 #[cfg(not(pyo3_disable_reference_pool))]
-use std::sync::{Mutex, OnceLock};
+use std::sync::Mutex;
 
 std::thread_local! {
     /// This is an internal counter in pyo3 monitoring whether this thread is attached to the interpreter.
@@ -303,19 +303,19 @@ impl ReferencePool {
 }
 
 #[cfg(not(pyo3_disable_reference_pool))]
-static POOL: OnceLock<ReferencePool> = OnceLock::new();
+static POOL: ReferencePool = ReferencePool::new();
 
 #[cfg(not(pyo3_disable_reference_pool))]
 fn get_pool() -> &'static ReferencePool {
-    POOL.get_or_init(ReferencePool::new)
+    &POOL
 }
 
 #[cfg_attr(pyo3_disable_reference_pool, inline(always))]
 #[cfg_attr(pyo3_disable_reference_pool, allow(unused_variables))]
 fn drop_deferred_references(py: Python<'_>) {
     #[cfg(not(pyo3_disable_reference_pool))]
-    if let Some(pool) = POOL.get() {
-        pool.drop_deferred_references(py);
+    {
+        POOL.drop_deferred_references(py);
     }
 }
 
@@ -344,10 +344,10 @@ impl Drop for SuspendAttach {
         unsafe { ffi::PyEval_RestoreThread(self.tstate) };
         // Update counts of `Py<T>` that were dropped while not attached.
         #[cfg(not(pyo3_disable_reference_pool))]
-        if let Some(pool) = POOL.get() {
+        {
             // SAFETY: just re-attached
             let py = unsafe { Python::assume_attached() };
-            pool.drop_deferred_references(py);
+            POOL.drop_deferred_references(py);
         }
     }
 }
