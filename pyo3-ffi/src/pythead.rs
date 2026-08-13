8d28a1212a43e0d25689b1ef7ee94ca0897d2a0d
person93 <person93.person93@gmail.com>
2026-08-13 00:36:53 -0400

    use transparent type for Py_tss_t if not Py_LIMITED_API

--- pyo3-ffi/src/pythead.rs
+++ pyo3-ffi/src/pythead.rs
@@ -1,7 +1,26 @@
 use core::ffi::{c_int, c_void};
 
-// TODO use non-opaque type if not(PY_LIMITED_API)
+#[cfg(Py_LIMITED_API)]
 opaque_struct!(pub Py_tss_t);
+
+#[cfg(not(Py_LIMITED_API))]
+#[repr(C)]
+pub struct Py_tss_t {
+    _is_initialized: c_int,
+    _key: NATIVE_TSS_KEY_T,
+}
+
+#[cfg(not(Py_LIMITED_API))]
+pub const Py_tss_NEEDS_INIT: Py_tss_t = Py_tss_t {
+    _is_initialized: 0,
+    _key: 0,
+};
+
+#[cfg(all(windows, not(Py_LIMITED_API)))]
+type NATIVE_TSS_KEY_T = core::ffi::c_ulong;
+
+#[cfg(all(not(windows), not(Py_LIMITED_API)))]
+type NATIVE_TSS_KEY_T = libc::pthread_key_t;
 
 extern_libpython! {
     pub fn PyThread_tss_alloc() -> *mut Py_tss_t;
