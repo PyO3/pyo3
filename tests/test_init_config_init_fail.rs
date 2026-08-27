6587e060960a85b7749c011883403850af34d81f
person93 <person93.person93@gmail.com>
2026-08-27 18:49:49 -0400

    fix test

--- tests/test_init_config_init_fail.rs
+++ tests/test_init_config_init_fail.rs
@@ -9,5 +9,5 @@ fn test_init_fail() {
     config
         .set_str(c"prefix", c"/path/that/does/not/exit")
         .unwrap();
-    Python::initialize_from_init_config(config).unwrap();
+    Python::initialize_from_init_config(config).unwrap_err();
 }
