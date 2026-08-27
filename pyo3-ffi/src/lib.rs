273dab57c29b427a511d5bdd3075095967a42238
person93 <person93.person93@gmail.com>
2026-08-27 00:32:59 -0400

    remove unfulfilled lint expectation

--- pyo3-ffi/src/lib.rs
+++ pyo3-ffi/src/lib.rs
@@ -485,7 +485,6 @@ pub use self::pyport::*;
 pub use self::pystate::*;
 pub use self::pystrtod::*;
 pub use self::pythonrun::*;
-#[cfg_attr(PyPy, expect(unused_imports, reason = "not implemented for PyPy"))]
 pub use self::pythread::*;
 pub use self::pytypedefs::*;
 pub use self::rangeobject::*;
