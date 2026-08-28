8733d184a8595a05f9fb1a9c76d3b041935aa3ed
person93 <person93.person93@gmail.com>
2026-08-27 21:05:36 -0400

    implement LocalKey using tss api

---
+++ src/platform/thread/thread_local.rs
@@ -0,0 +1,200 @@
+use core::cell::{Cell, UnsafeCell};
+use core::ffi::c_void;
+use core::fmt::{Debug, Display};
+use core::marker::PhantomData;
+use core::ptr::{self, NonNull};
+use core::sync::atomic::{AtomicPtr, AtomicU8, Ordering};
+
+#[cfg(not(any(RustPython, Py_LIMITED_API)))]
+use pyo3_ffi::Py_tss_NEEDS_INIT;
+
+use crate::ffi::{
+    PyThread_tss_alloc, PyThread_tss_create, PyThread_tss_delete, PyThread_tss_free,
+    PyThread_tss_get, PyThread_tss_set,
+};
+use crate::platform::prelude::*;
+
+pub struct LocalKey<T: 'static> {
+    #[cfg(any(RustPython, Py_LIMITED_API))]
+    inner: AtomicPtr<crate::ffi::Py_tss_t>,
+
+    #[cfg(not(any(RustPython, Py_LIMITED_API)))]
+    inner: UnsafeCell<crate::ffi::Py_tss_t>,
+
+    state: AtomicU8,
+    init: fn() -> T,
+}
+
+// SAFETY: the unsafecell is only accessed by python tss functions which are thread safe
+#[cfg(not(any(RustPython, Py_LIMITED_API)))]
+unsafe impl<T: 'static> Sync for LocalKey<T> {}
+
+const LOCAL_KEY_UNINIT: u8 = 0;
+const LOCAL_KEY_CREATED: u8 = 1;
+const LOCAL_KEY_DESTROYED: u8 = 2;
+
+impl<T: 'static> Debug for LocalKey<T> {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        f.debug_struct("LocalKey").finish_non_exhaustive()
+    }
+}
+
+#[derive(Clone, Copy, Eq, PartialEq, Debug)]
+pub struct AccessError;
+
+impl Display for AccessError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        Display::fmt("already destroyed", f)
+    }
+}
+
+impl core::error::Error for AccessError {}
+
+// This ensures the panicking code is outlined from `with` for `LocalKey`.
+#[cfg_attr(not(panic = "immediate-abort"), inline(never))]
+#[track_caller]
+#[cold]
+fn panic_access_error(err: AccessError) -> ! {
+    panic!("cannot access a Thread Local Storage value during or after destruction: {err:?}")
+}
+
+impl<T: 'static> LocalKey<T> {
+    pub const unsafe fn new(init: fn() -> T) -> LocalKey<T> {
+        cfg_select! {
+            any(RustPython, Py_LIMITED_API) => {
+                LocalKey {
+                    inner: AtomicPtr::new(core::ptr::null_mut()),
+                    state: AtomicU8::new(LOCAL_KEY_UNALLOCATED),
+                    init,
+                }
+            },
+
+            _ => {
+                LocalKey {
+                    inner: UnsafeCell::new(Py_tss_NEEDS_INIT),
+                    state: AtomicU8::new(LOCAL_KEY_CREATED),
+                    init
+                }
+            }
+        }
+    }
+
+    #[track_caller]
+    #[inline]
+    pub fn with<F, R>(&'static self, f: F) -> R
+    where
+        F: FnOnce(&T) -> R,
+    {
+        match self.try_with(f) {
+            Ok(r) => r,
+            Err(err) => panic_access_error(err),
+        }
+    }
+
+    #[inline]
+    pub fn try_with<F, R>(&'static self, f: F) -> Result<R, AccessError>
+    where
+        F: FnOnce(&T) -> R,
+    {
+        match self.state.load(Ordering::SeqCst) {
+            LOCAL_KEY_UNINIT => self.initialize(),
+            LOCAL_KEY_CREATED => (),
+            LOCAL_KEY_DESTROYED => return Err(AccessError),
+            _ => unreachable!(),
+        }
+        // SAFETY: the match statement above ensures we only reach this point if tss is created
+        let val = unsafe { self.get_val() };
+        Ok(f(val))
+    }
+
+    #[cfg(any(RustPython, Py_LIMITED_API))]
+    fn initialize(&'static self) {
+        // SAFETY: no requirements
+        let inner = unsafe { PyThread_tss_alloc() };
+        let inner = NonNull::new(inner).unwrap();
+        // SAFETY: ptr obtained by calling PyThread_tss_alloc
+        let result = unsafe { PyThread_tss_create(inner.as_ptr()) };
+        assert_eq!(result, 0, "failed to created thread specific storage");
+
+        match self.inner.compare_exchange(
+            ptr::null_mut(),
+            inner.as_ptr(),
+            Ordering::SeqCst,
+            Ordering::SeqCst,
+        ) {
+            Ok(_) => self.state.store(LOCAL_KEY_CREATED, Ordering::SeqCst),
+            Err(redundant_tss) => {
+                // SAFETY: inner is returned by PyThread_tss_alloc and is not used after this call
+                unsafe { PyThread_tss_free(redundant_tss) };
+            }
+        }
+    }
+
+    #[cfg(not(any(RustPython, Py_LIMITED_API)))]
+    fn initialize(&'static self) {
+        // SAFETY: inner is initialized with Py_tss_NEEDS_INIT
+        let result = unsafe { PyThread_tss_create(self.inner.get()) };
+        assert_eq!(result, 0, "failed to created thread specific storage");
+        self.state.store(LOCAL_KEY_CREATED, Ordering::SeqCst);
+    }
+
+    /// # Safety
+    /// Can only be called if tss is created
+    unsafe fn get_val<'a>(&'static self) -> &'a T {
+        let inner = cfg_select! {
+            any(RustPython, Py_LIMITED_API) => self.inner.load(Ordering::SeqCst),
+            _ => self.inner.get(),
+        };
+        // SAFETY: inner is a valid tss key (upheld by caller)
+        let val: *mut T = unsafe { PyThread_tss_get(inner) }.cast();
+        match NonNull::new(val) {
+            // SAFETY: no mut ref is ever created from this pointer
+            Some(val) => unsafe { val.as_ref() },
+            None => {
+                let val = Box::new((self.init)());
+                let val = Box::into_raw(val);
+                // SAFETY: inner is a valid tss key
+                let result = unsafe { PyThread_tss_set(inner, val.cast()) };
+                assert_eq!(result, 0, "failed to set thread specific value");
+                // SAFETY: val was just allocated above
+                unsafe { NonNull::new_unchecked(val).as_ref() }
+            }
+        }
+    }
+}
+
+impl<T: 'static> Drop for LocalKey<T> {
+    fn drop(&mut self) {
+        self.state.store(LOCAL_KEY_DESTROYED, Ordering::SeqCst);
+        cfg_select! {
+            any(RustPython, Py_LIMITED_API) => {
+                // SAFETY: inner is returned by PyThread_tss_alloc and is not used after this call
+                unsafe { PyThread_tss_free(self.inner.load(Ordering::SeqCst)) };
+            },
+            _ => {
+                // SAFETY: inner is not used again after this call
+                unsafe { PyThread_tss_delete(self.inner.get()) };
+            },
+        }
+    }
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! thread_local {
+    ($($(#[$attr:meta])* $vis:vis static $name:ident : $ty:ty = $(const)? $init:expr;)+) => {
+        $(
+            $(#[$attr])*
+            #[allow(unused_braces)]
+            // SAFETY: correctly intializes a LocalKey
+            $vis static $name: $crate::platform::thread::LocalKey<$ty> = unsafe {
+                $crate::platform::thread::LocalKey::new({
+                    fn init() -> $ty {
+                        $init
+                    }
+                    init
+                })
+            };
+        )+
+    };
+}
