0769c4f47378c2169542aff0e6235c6e7fa2e4d3
Jonas Dedden <mail@jonas-dedden.de>
2026-08-28 11:43:39 +0200

    experimental-inspect: give slot-wrapper trailing arguments their `None` default

--- pyo3-macros-backend/src/pyfunction/signature.rs
+++ pyo3-macros-backend/src/pyfunction/signature.rs
@@ -10,6 +10,7 @@ use quote::ToTokens;
 use syn::{
     ext::IdentExt,
     parse::{Parse, ParseStream},
+    parse_quote,
     punctuated::Punctuated,
     spanned::Spanned,
     Expr, Token,
@@ -582,6 +583,25 @@ impl<'a> FunctionSignature<'a> {
             arguments,
             python_signature,
             attribute: None,
+        }
+    }
+
+    /// Gives the last `count` positional parameters a `None` default, matching a CPython slot
+    /// wrapper which substitutes `None` for the trailing arguments the caller may omit.
+    pub fn default_trailing_parameters_to_none(&mut self, count: usize) {
+        let regular = self.arguments.iter_mut().filter_map(|arg| match arg {
+            FnArg::Regular(arg) => Some(arg),
+            _ => None,
+        });
+        let mut defaulted = 0;
+        for arg in regular.rev().take(count) {
+            arg.default_value = Some(Box::new(parse_quote!(None)));
+            defaulted += 1;
+        }
+        for _ in 0..defaulted {
+            self.python_signature
+                .default_positional_parameters
+                .push(parse_quote!(None));
         }
     }
 
