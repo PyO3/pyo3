0763c4e5094092d4bea7b5604507fabc752e2601
person93 <person93.person93@gmail.com>
2026-08-29 00:52:42 -0400

    feature-gate more std things

--- src/conversions/std/mod.rs
+++ src/conversions/std/mod.rs
@@ -5,10 +5,13 @@ mod ipaddr;
 mod map;
 pub(crate) mod num;
 mod option;
+#[cfg(wip_feature_std)]
 mod osstr;
+#[cfg(wip_feature_std)]
 mod path;
 mod set;
 mod slice;
 mod string;
+#[cfg(wip_feature_std)]
 mod time;
 mod vec;
