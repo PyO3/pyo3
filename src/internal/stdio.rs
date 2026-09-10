9e520be93fa2a31b2cbe795abc1be2d22c9bdb57
person93 <person93.person93@gmail.com>
2026-09-10 13:15:27 -0400

    remove unused function for writing to python stderr

--- src/internal/stdio.rs
+++
@@ -1,9 +0,0 @@
-use core::ffi::CStr;
-
-use crate::ffi::PySys_WriteStderr;
-use crate::prelude::Python;
-
-pub(crate) fn write_py_stderr(_py: Python<'_>, message: &CStr) {
-    // SAFETY: message is a valid c string
-    unsafe { PySys_WriteStderr(message.as_ptr()) };
-}
