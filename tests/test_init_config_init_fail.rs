43c058452252c0f2f3e477ff663595ca3da7077e
person93 <person93.person93@gmail.com>
2026-09-10 14:04:06 -0400

    Update tests/test_init_config_init_fail.rs
    Co-authored-by: David Hewitt <mail@davidhewitt.dev>

--- tests/test_init_config_init_fail.rs
+++ tests/test_init_config_init_fail.rs
@@ -4,11 +4,9 @@ use pyo3::init_config::PyInitConfig;
 use pyo3::prelude::Python;
 
 #[test]
-#[ignore = "this doesn't seem to be a reliable way to make python fail to initialize"]
 fn test_init_fail() {
     let mut config = PyInitConfig::default();
-    config
-        .set_str(c"prefix", c"/path/that/does/not/exit")
-        .unwrap();
+    // invalid allocator value should cause Python to fail to initialize reliably
+    config.set_int(c"allocator", 999).unwrap();
     Python::initialize_from_init_config(config).unwrap_err();
 }
