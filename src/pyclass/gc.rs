ea23291737d5d47067e905adbece4005775d9997
Bas Schoenmaeckers <b.schoenmaeckers@zandersgroup.com>
2026-09-14 16:40:20 +0200

    Revert "Implement PyGcTraversable for Weak<T>"
    This reverts commit 7a7af89ad9815a0f595778d7be0f0bd0b5ce0e3b.

--- src/pyclass/gc.rs
+++ src/pyclass/gc.rs
@@ -5,7 +5,7 @@ use alloc::{
     collections::{BTreeMap, BTreeSet, BinaryHeap, LinkedList, VecDeque},
     ffi::CString,
     string::String,
-    sync::{Arc, Weak},
+    sync::Arc,
     vec::Vec,
 };
 use core::{
@@ -440,19 +440,6 @@ unsafe impl<T: PyGcTraversable> PyGcTraversable for Arc<T> {
             }
         }
     }
-}
-
-// SAFETY: `Weak<T>` is non-owning and does not keep Python reference cycles alive.
-unsafe impl<T> PyGcTraversable for Weak<T> {
-    const MAY_CONTAIN_CYCLES: bool = false;
-
-    #[inline]
-    fn traverse(&self, _visit: PyVisit<'_>) -> Result<(), PyTraverseError> {
-        Ok(())
-    }
-
-    #[inline]
-    fn clear(&mut self) {}
 }
 
 // SAFETY: `Result<T, E>` owns either `T` or `E`; delegating to active variant is sound.
