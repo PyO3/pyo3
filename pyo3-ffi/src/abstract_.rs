1febd5783ffb9ca3291bc5d1da582641a5c36ca1
David Hewitt <mail@davidhewitt.dev>
2026-09-15 21:59:13 +0100

    fix `PyVectorcall_Call` cfg

--- pyo3-ffi/src/abstract_.rs
+++ pyo3-ffi/src/abstract_.rs
@@ -75,7 +75,7 @@ extern_libpython! {
     #[cfg_attr(PyPy, link_name = "PyPyVectorcall_NARGS")]
     pub fn PyVectorcall_NARGS(nargsf: size_t) -> Py_ssize_t;
 
-    #[cfg(Py_3_12)]
+    #[cfg(any(Py_3_12, not(Py_LIMITED_API)))]
     #[cfg_attr(PyPy, link_name = "PyPyVectorcall_Call")]
     pub fn PyVectorcall_Call(
         callable: *mut PyObject,
