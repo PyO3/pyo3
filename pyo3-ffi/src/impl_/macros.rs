dd54cd6e129439ab2c5d98ff11f06f1e0ea85cd6
David Hewitt <mail@davidhewitt.dev>
2026-09-24 08:03:04 +0100

    add future libpypy3.13-c dll

--- pyo3-ffi/src/impl_/macros.rs
+++ pyo3-ffi/src/impl_/macros.rs
@@ -289,6 +289,7 @@ macro_rules! extern_libpython {
             // PyPy (DLL is libpypy3.X-c.dll, not pythonXY.dll)
             "libpypy3.11-c",
             "libpypy3.12-c",
+            "libpypy3.13-c",
         );
     };
     // Internal: generate cfg_attr for each DLL name. One of these will be selected
